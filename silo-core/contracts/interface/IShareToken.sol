84fd908e814f2594889ec0eb2508e0b725a86ad6
shadowyedd <shadowyedd@protonmail.com>
2023-06-15 20:31:23 +0200

    static solidity version

--- silo-core/contracts/interface/IShareToken.sol
+++ silo-core/contracts/interface/IShareToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity >=0.8.17;
+pragma solidity 0.8.19;
 
 import "openzeppelin-contracts-upgradeable/contracts/token/ERC20/extensions/IERC20MetadataUpgradeable.sol";
 import "../interface/ISilo.sol";
--- silo-core/contracts/interface/ISiloOracle.sol
+++ silo-core/contracts/interface/ISiloOracle.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity >=0.8.17;
+pragma solidity 0.8.19;
 
 interface ISiloOracle {
     function quote(uint256 _baseAmount, address _baseToken) external returns (uint256 quoteAmount);
