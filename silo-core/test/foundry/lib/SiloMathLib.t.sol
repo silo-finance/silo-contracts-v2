7e8beff041ee1c6eccbf89aab382b20afea3288a
shadowyedd <shadowyedd@protonmail.com>
2023-09-28 18:40:31 +0200

    Test cases

--- silo-core/test/foundry/lib/SiloMathLib.t.sol
+++ silo-core/test/foundry/lib/SiloMathLib.t.sol
@@ -10,53 +10,50 @@ contract SiloMathLibTest is Test {
     address public asset = address(10002);
     address public model = address(10003);
 
-    // TODO remove fuzz
-    function test_liquidity(uint256 _collateralAssets, uint256 _debtAssets) public {
-        if (_debtAssets >= _collateralAssets) {
-            assertEq(SiloMathLib.liquidity(_collateralAssets, _debtAssets), 0);
-        } else {
-            assertEq(SiloMathLib.liquidity(_collateralAssets, _debtAssets), _collateralAssets - _debtAssets);
-        }
+    /*
+    forge test -vv --mt test_liquidity
+    */
+    function test_liquidity() public {
+        assertEq(SiloMathLib.liquidity(0, 0), 0);
+        assertEq(SiloMathLib.liquidity(100, 10), 90);
+        assertEq(SiloMathLib.liquidity(1e18, 1), 999999999999999999);
+        assertEq(SiloMathLib.liquidity(1e18, 0.1e18), 0.9e18);
+        assertEq(SiloMathLib.liquidity(25000e18, 7999e18), 17001e18);
+        assertEq(SiloMathLib.liquidity(25000e18, 30000e18), 0);
     }
 
-    // TODO remove fuzz
+    /*
+    forge test -vv --mt test_calculateUtilization
+    */
     function test_calculateUtilization(uint256 _collateralAssets, uint256 _debtAssets) public {
         uint256 dp = 1e18;
-
-        vm.assume(_collateralAssets > 0);
-        vm.assume(_debtAssets < type(uint128).max);
-        uint256 u = _debtAssets * dp / _collateralAssets;
-        vm.assume(u <= dp);
-
-        assertEq(SiloMathLib.calculateUtilization(dp, _collateralAssets, _debtAssets), u);
 
         assertEq(SiloMathLib.calculateUtilization(dp, 1e18, 0.9e18), 0.9e18);
         assertEq(SiloMathLib.calculateUtilization(dp, 1e18, 0.1e18), 0.1e18);
         assertEq(SiloMathLib.calculateUtilization(dp, 10e18, 1e18), 0.1e18);
         assertEq(SiloMathLib.calculateUtilization(dp, 100e18, 25e18), 0.25e18);
         assertEq(SiloMathLib.calculateUtilization(dp, 100e18, 49e18), 0.49e18);
+        assertEq(SiloMathLib.calculateUtilization(1e4, 100e18, 49e18), 0.49e4);
+
+        assertEq(SiloMathLib.calculateUtilization(1e18, 0, _debtAssets), 0);
+        assertEq(SiloMathLib.calculateUtilization(1e18, _collateralAssets, 0), 0);
+        assertEq(SiloMathLib.calculateUtilization(0, _collateralAssets, _debtAssets), 0);
     }
 
-    // TODO remove fuzz
+    /*
+    forge test -vv --mt test_calculateUtilizationWithMax
+    */
     function test_calculateUtilizationWithMax(uint256 _dp, uint256 _collateralAssets, uint256 _debtAssets) public {
         vm.assume(_debtAssets < type(uint128).max);
         vm.assume(_dp < type(uint128).max);
 
-        uint256 standardDp = 1e18;
-
-        assertEq(SiloMathLib.calculateUtilization(standardDp, 0, _debtAssets), 0);
-        assertEq(SiloMathLib.calculateUtilization(standardDp, _collateralAssets, 0), 0);
-        assertEq(SiloMathLib.calculateUtilization(0, _collateralAssets, _debtAssets), 0);
-
-        uint256 u = SiloMathLib.calculateUtilization(_dp, _collateralAssets, _debtAssets);
-        assertTrue(u <= _dp);
+        assertTrue(SiloMathLib.calculateUtilization(_dp, _collateralAssets, _debtAssets) <= _dp);
     }
 
     /*
     forge test -vv --mt test_convertToAssets
     */
     function test_convertToAssets() public {
-        uint256 offset = SiloMathLib._DECIMALS_OFFSET_POW;
         uint256 shares;
         uint256 totalAssets;
         uint256 totalShares;
@@ -69,9 +66,19 @@ contract SiloMathLibTest is Test {
             "all zeros"
         );
 
+        shares = 10;
+
+        assertEq(
+            SiloMathLib.convertToAssets(
+                shares, totalAssets, totalShares, MathUpgradeable.Rounding.Down, ISilo.AssetType.Collateral
+            ),
+            10,
+            "first mint"
+        );
+
         shares = 0;
         totalAssets = 1000;
-        totalShares = 1000 * offset;
+        totalShares = 1000;
 
         assertEq(
             SiloMathLib.convertToAssets(
@@ -81,6 +88,18 @@ contract SiloMathLibTest is Test {
             "0 shares => 0 assets"
         );
 
+        shares = 333;
+        totalAssets = 1000;
+        totalShares = 999;
+
+        assertEq(
+            SiloMathLib.convertToAssets(
+                shares, totalAssets, totalShares, MathUpgradeable.Rounding.Down, ISilo.AssetType.Collateral
+            ),
+            333,
+            "(1000/999) 333 shares down => 333 assets"
+        );
+
         shares = 1;
         totalAssets = 1;
         totalShares = 1;
@@ -88,8 +107,8 @@ contract SiloMathLibTest is Test {
             SiloMathLib.convertToAssets(
                 shares, totalAssets, totalShares, MathUpgradeable.Rounding.Down, ISilo.AssetType.Collateral
             ),
-            0,
-            "(1/1), 1 share down => 0 assets"
+            1,
+            "(1/1), 1 share down => 1 assets"
         );
         assertEq(
             SiloMathLib.convertToAssets(
@@ -100,12 +119,12 @@ contract SiloMathLibTest is Test {
         );
 
         totalAssets = 10;
-        totalShares = 10 * offset;
+        totalShares = 10;
         assertEq(
             SiloMathLib.convertToAssets(
                 1, totalAssets, totalShares, MathUpgradeable.Rounding.Down, ISilo.AssetType.Collateral
             ),
-            0,
+            1,
             "(10/10), 0.01 share down => 0 assets"
         );
         assertEq(
@@ -119,15 +138,320 @@ contract SiloMathLibTest is Test {
             SiloMathLib.convertToAssets(
                 100, totalAssets, totalShares, MathUpgradeable.Rounding.Up, ISilo.AssetType.Collateral
             ),
-            1,
-            "(10/10), 1.0 share Up => 1 assets"
+            100,
+            "(10/10), 100 share Up => 100 assets"
         );
         assertEq(
             SiloMathLib.convertToAssets(
                 1000, totalAssets, totalShares, MathUpgradeable.Rounding.Up, ISilo.AssetType.Collateral
             ),
+            1000,
+            "(10/10), 1000 share Up => 1000 assets"
+        );
+    }
+
+    /*
+    forge test -vv --mt test_convertToAssetsForDebt
+    */
+    function test_convertToAssetsForDebt() public {
+        uint256 shares;
+        uint256 totalAssets;
+        uint256 totalShares;
+
+        assertEq(
+            SiloMathLib.convertToAssets(
+                shares, totalAssets, totalShares, MathUpgradeable.Rounding.Down, ISilo.AssetType.Debt
+            ),
+            0,
+            "all zeros"
+        );
+
+        shares = 100;
+
+        assertEq(
+            SiloMathLib.convertToAssets(
+                shares, totalAssets, totalShares, MathUpgradeable.Rounding.Down, ISilo.AssetType.Debt
+            ),
+            100,
+            "first mint"
+        );
+
+        shares = 0;
+        totalAssets = 1000;
+        totalShares = 1000;
+
+        assertEq(
+            SiloMathLib.convertToAssets(
+                shares, totalAssets, totalShares, MathUpgradeable.Rounding.Down, ISilo.AssetType.Debt
+            ),
+            0,
+            "0 shares => 0 assets"
+        );
+
+        shares = 333;
+        totalAssets = 1000;
+        totalShares = 999;
+
+        assertEq(
+            SiloMathLib.convertToAssets(
+                shares, totalAssets, totalShares, MathUpgradeable.Rounding.Up, ISilo.AssetType.Debt
+            ),
+            334,
+            "(1000/999) 333 shares up => 334 assets"
+        );
+
+        shares = 1;
+        totalAssets = 1;
+        totalShares = 1;
+        assertEq(
+            SiloMathLib.convertToAssets(
+                shares, totalAssets, totalShares, MathUpgradeable.Rounding.Down, ISilo.AssetType.Debt
+            ),
+            1,
+            "(1/1), 1 share down => 1 assets"
+        );
+        assertEq(
+            SiloMathLib.convertToAssets(
+                shares, totalAssets, totalShares, MathUpgradeable.Rounding.Up, ISilo.AssetType.Debt
+            ),
+            1,
+            "(1/1), 1 share Up => 1 assets"
+        );
+
+        totalAssets = 10;
+        totalShares = 10;
+        assertEq(
+            SiloMathLib.convertToAssets(
+                1, totalAssets, totalShares, MathUpgradeable.Rounding.Down, ISilo.AssetType.Debt
+            ),
+            1,
+            "(10/10), 0.01 share down => 1 assets"
+        );
+        assertEq(
+            SiloMathLib.convertToAssets(
+                1, totalAssets, totalShares, MathUpgradeable.Rounding.Up, ISilo.AssetType.Debt
+            ),
+            1,
+            "(10/10), 0.01 share Up => 1 assets"
+        );
+
+        totalAssets = 1000;
+        totalShares = 1000;
+        assertEq(
+            SiloMathLib.convertToAssets(
+                1000, totalAssets, totalShares, MathUpgradeable.Rounding.Down, ISilo.AssetType.Debt
+            ),
+            1000,
+            "(1000/1000), 1000 share Down => 1000 assets"
+        );
+        assertEq(
+            SiloMathLib.convertToAssets(
+                1000, totalAssets, totalShares, MathUpgradeable.Rounding.Up, ISilo.AssetType.Debt
+            ),
+            1000,
+            "(10/10), 1000 share Up => 1000 assets"
+        );
+    }
+
+    /*
+    forge test -vv --mt test_convertToShares
+    */
+    function test_convertToShares() public {
+        uint256 assets;
+        uint256 totalAssets;
+        uint256 totalShares;
+
+        assertEq(
+            SiloMathLib.convertToShares(
+                assets, totalAssets, totalShares, MathUpgradeable.Rounding.Down, ISilo.AssetType.Collateral
+            ),
+            0,
+            "all zeros"
+        );
+
+        assets = 10;
+
+        assertEq(
+            SiloMathLib.convertToShares(
+                assets, totalAssets, totalShares, MathUpgradeable.Rounding.Down, ISilo.AssetType.Collateral
+            ),
             10,
-            "(10/10), 10.00 share Up => 10 assets"
+            "first mint"
+        );
+
+        assets = 333;
+        totalAssets = 999;
+        totalShares = 1000;
+
+        assertEq(
+            SiloMathLib.convertToShares(
+                assets, totalAssets, totalShares, MathUpgradeable.Rounding.Down, ISilo.AssetType.Collateral
+            ),
+            333,
+            "(999/1000) 333 assets down => 333 assets"
+        );
+
+
+        assets = 0;
+        totalAssets = 1000;
+        totalShares = 1000;
+
+        assertEq(
+            SiloMathLib.convertToShares(
+                assets, totalAssets, totalShares, MathUpgradeable.Rounding.Down, ISilo.AssetType.Collateral
+            ),
+            0,
+            "0 shares => 0 assets"
+        );
+
+        assets = 1;
+        totalAssets = 1;
+        totalShares = 1;
+        assertEq(
+            SiloMathLib.convertToShares(
+                assets, totalAssets, totalShares, MathUpgradeable.Rounding.Down, ISilo.AssetType.Collateral
+            ),
+            1,
+            "(1/1), 1 share down => 1 assets"
+        );
+        assertEq(
+            SiloMathLib.convertToShares(
+                assets, totalAssets, totalShares, MathUpgradeable.Rounding.Up, ISilo.AssetType.Collateral
+            ),
+            1,
+            "(1/1), 1 share Up => 1 assets"
+        );
+
+        totalAssets = 10;
+        totalShares = 10;
+        assertEq(
+            SiloMathLib.convertToShares(
+                1, totalAssets, totalShares, MathUpgradeable.Rounding.Down, ISilo.AssetType.Collateral
+            ),
+            1,
+            "(10/10), 0.01 share down => 0 assets"
+        );
+        assertEq(
+            SiloMathLib.convertToShares(
+                1, totalAssets, totalShares, MathUpgradeable.Rounding.Up, ISilo.AssetType.Collateral
+            ),
+            1,
+            "(10/10), 0.01 share Up => 1 assets"
+        );
+        assertEq(
+            SiloMathLib.convertToShares(
+                100, totalAssets, totalShares, MathUpgradeable.Rounding.Up, ISilo.AssetType.Collateral
+            ),
+            100,
+            "(10/10), 100 share Up => 100 assets"
+        );
+        assertEq(
+            SiloMathLib.convertToShares(
+                1000, totalAssets, totalShares, MathUpgradeable.Rounding.Up, ISilo.AssetType.Collateral
+            ),
+            1000,
+            "(10/10), 1000 share Up => 1000 assets"
+        );
+    }
+
+    /*
+    forge test -vv --mt test_convertToSharesForDebt
+    */
+    function test_convertToSharesForDebt() public {
+        uint256 assets;
+        uint256 totalAssets;
+        uint256 totalShares;
+
+        assertEq(
+            SiloMathLib.convertToShares(
+                assets, totalAssets, totalShares, MathUpgradeable.Rounding.Down, ISilo.AssetType.Debt
+            ),
+            0,
+            "all zeros"
+        );
+
+        assets = 10;
+
+        assertEq(
+            SiloMathLib.convertToShares(
+                assets, totalAssets, totalShares, MathUpgradeable.Rounding.Down, ISilo.AssetType.Debt
+            ),
+            10,
+            "first mint"
+        );
+
+        assets = 333;
+        totalAssets = 999;
+        totalShares = 1000;
+
+        assertEq(
+            SiloMathLib.convertToShares(
+                assets, totalAssets, totalShares, MathUpgradeable.Rounding.Up, ISilo.AssetType.Debt
+            ),
+            334,
+            "(999/1000) 333 assets up => 334 shares"
+        );
+
+
+        assets = 0;
+        totalAssets = 1000;
+        totalShares = 1000;
+
+        assertEq(
+            SiloMathLib.convertToShares(
+                assets, totalAssets, totalShares, MathUpgradeable.Rounding.Down, ISilo.AssetType.Debt
+            ),
+            0,
+            "0 shares => 0 assets"
+        );
+
+        assets = 1;
+        totalAssets = 1;
+        totalShares = 1;
+        assertEq(
+            SiloMathLib.convertToShares(
+                assets, totalAssets, totalShares, MathUpgradeable.Rounding.Down, ISilo.AssetType.Debt
+            ),
+            1,
+            "(1/1), 1 share down => 1 assets"
+        );
+        assertEq(
+            SiloMathLib.convertToShares(
+                assets, totalAssets, totalShares, MathUpgradeable.Rounding.Up, ISilo.AssetType.Debt
+            ),
+            1,
+            "(1/1), 1 share Up => 1 assets"
+        );
+
+        totalAssets = 10;
+        totalShares = 10;
+        assertEq(
+            SiloMathLib.convertToShares(
+                1, totalAssets, totalShares, MathUpgradeable.Rounding.Down, ISilo.AssetType.Debt
+            ),
+            1,
+            "(10/10), 0.01 share down => 0 assets"
+        );
+        assertEq(
+            SiloMathLib.convertToShares(
+                1, totalAssets, totalShares, MathUpgradeable.Rounding.Up, ISilo.AssetType.Debt
+            ),
+            1,
+            "(10/10), 0.01 share Up => 1 assets"
+        );
+        assertEq(
+            SiloMathLib.convertToShares(
+                100, totalAssets, totalShares, MathUpgradeable.Rounding.Up, ISilo.AssetType.Debt
+            ),
+            100,
+            "(10/10), 100 share Up => 100 assets"
+        );
+        assertEq(
+            SiloMathLib.convertToShares(
+                1000, totalAssets, totalShares, MathUpgradeable.Rounding.Up, ISilo.AssetType.Debt
+            ),
+            1000,
+            "(10/10), 1000 share Up => 1000 assets"
         );
     }
 
