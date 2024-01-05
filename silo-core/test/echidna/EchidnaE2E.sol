a6af4b81b18b566bad8d113b88ed1e656c28f5cc
Neo Racer <0xNeoRacer@protonmail.com>
2024-01-05 10:34:28 +0100

    I was able to succesfully run E2E echidna tests

--- silo-core/test/echidna/EchidnaE2E.sol
+++ silo-core/test/echidna/EchidnaE2E.sol
@@ -14,7 +14,12 @@ import {Strings} from "openzeppelin-contracts/utils/Strings.sol";
 // Note: In order to run this campaign all library functions marked as `public` or `external`
 // Need to be changed to be `internal`. This includes all library contracts in contracts/lib/
 
-// Command to run: echidna ./silo-core/contracts/echidna/EchidnaE2E.sol --contract EchidnaE2E --config ./silo-core/contracts/echidna/e2e-internal.yaml --workers 10
+// Command to run:
+/*
+// SOLC_VERSION=0.8.21 echidna silo-core/test/echidna/EchidnaDemo.e.sol
+
+SOLC_VERSION=0.8.21 echidna silo-core/test/echidna/EchidnaE2E.sol --contract EchidnaE2E --config silo-core/test/echidna/e2e-internal.yaml --workers 10
+*/
 contract EchidnaE2E is Deployers, PropertiesAsserts {
     using Strings for uint256;
     ISiloConfig siloConfig;
