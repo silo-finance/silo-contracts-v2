deea8e6a0f550b2bbe6360184e3bb96a77bde6ce
Or Pistiner <or@certora.com>
2024-02-05 00:08:05 +0200

    Removed old sol files

--- certora/harness/Silo1.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: BUSL-1.1
-import {Silo, ISiloFactory} from "../../silo-core/contracts/Silo.sol";
-contract Silo1 is Silo {
-    constructor(ISiloFactory _siloFactory) Silo(_siloFactory)  {}
-
-}
