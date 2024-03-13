6eedc512af798f19cb093fedab9680689674173a
Neo Racer <0xNeoRacer@protonmail.com>
2024-03-13 16:31:48 +0100

    .

--- silo-core/test/foundry/lib/SiloLendingLib/BorrowPossible.t.sol
+++ silo-core/test/foundry/lib/SiloLendingLib/BorrowPossible.t.sol
@@ -32,15 +32,39 @@ contract BorrowPossibleTest is Test {
     }
 
     /*
-    forge test -vv --mt test_borrowPossible_borrowable_notPossibleWithCollateral
+    forge test -vv --mt test_borrowPossible_notPossibleWithDebt
     */
-    function test_borrowPossible_borrowable_notPossibleWithCollateral() public {
+    function test_borrowPossible_notPossibleWithDebt() public {
         TokenMock protectedShareToken = new TokenMock(address(0x111));
         TokenMock collateralShareToken = new TokenMock(address(0x222));
         TokenMock debtShareToken = new TokenMock(address(0x333));
         address borrower = address(0x333);
 
         protectedShareToken.balanceOfMock(borrower, 0);
+        collateralShareToken.balanceOfMock(borrower, 2);
+        debtShareToken.balanceOfMock(borrower, 1);
+
+        (
+            bool possible, uint256 protectedSharesToWithdraw, uint256 collateralSharesToWithdraw
+        ) = SiloLendingLib.borrowPossible(
+            protectedShareToken.ADDRESS(), collateralShareToken.ADDRESS(), debtShareToken.ADDRESS(), borrower
+        );
+
+        assertFalse(possible, "borrow NOT possible when debt");
+        assertEq(protectedSharesToWithdraw, 0);
+        assertEq(collateralSharesToWithdraw, 2);
+    }
+
+    /*
+    forge test -vv --mt test_borrowPossible_possibleWithWithdraw
+    */
+    function test_borrowPossible_possibleWithWithdraw() public {
+        TokenMock protectedShareToken = new TokenMock(address(0x111));
+        TokenMock collateralShareToken = new TokenMock(address(0x222));
+        TokenMock debtShareToken = new TokenMock(address(0x333));
+        address borrower = address(0x333);
+
+        protectedShareToken.balanceOfMock(borrower, 3);
         collateralShareToken.balanceOfMock(borrower, 2);
         debtShareToken.balanceOfMock(borrower, 0);
 
@@ -51,29 +75,7 @@ contract BorrowPossibleTest is Test {
         );
 
         assertTrue(possible, "borrow possible (when no debt) conditionally, with withdraw");
-        assertEq(protectedSharesToWithdraw, 0);
+        assertEq(protectedSharesToWithdraw, 3);
         assertEq(collateralSharesToWithdraw, 2);
-    }
-
-    /*
-    forge test -vv --mt test_borrowPossible_borrowable_notPossibleWithProtected
-    */
-    function test_borrowPossible_borrowable_notPossibleWithProtected() public {
-        TokenMock protectedShareToken = new TokenMock(address(0x111));
-        TokenMock collateralShareToken = new TokenMock(address(0x222));
-        TokenMock debtShareToken = new TokenMock(address(0x333));
-        address borrower = address(0x333);
-
-        protectedShareToken.balanceOfMock(borrower, 1);
-
-        (
-            bool possible, uint256 protectedSharesToWithdraw, uint256 collateralSharesToWithdraw
-        ) = SiloLendingLib.borrowPossible(
-            protectedShareToken.ADDRESS(), collateralShareToken.ADDRESS(), debtShareToken.ADDRESS(), borrower
-        );
-
-        assertFalse(possible, "borrow NOT possible when borrowPossible=true and no collateral in this token");
-        assertEq(protectedSharesToWithdraw, 0);
-        assertEq(collateralSharesToWithdraw, 0);
     }
 }
