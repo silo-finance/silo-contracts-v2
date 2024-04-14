eb2dbf129ca2dffbd360f285d65d08df143b51e7
Sergey Maksimov <sergey@silo.finance>
2024-04-14 22:58:15 +0300

    work in progress

--- silo-core/contracts/interfaces/IDynamicKinkModelV1.sol
+++ silo-core/contracts/interfaces/IDynamicKinkModelV1.sol
@@ -1,63 +1,43 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0;
 
-import {IInterestRateModelV2Config} from "./IInterestRateModelV2Config.sol";
-
-// solhint-disable var-name-mixedcase
-
-interface IDynamicKinkModelV1 {
+// import {IInterestRateModel} from "./IInterestRateModel.sol";
+// interface IDynamicKinkModelV1 is IInterestRateModel {
+    interface IDynamicKinkModelV1 {
     struct Config {
-        // uopt ∈ (0, 1) – optimal utilization;
-        int256 uopt;
-        // ucrit ∈ (uopt, 1) – threshold of large utilization;
-        int256 ucrit;
-        // ulow ∈ (0, uopt) – threshold of low utilization
+        // ulow ∈ [0, 1) – threshold of low utilization;
         int256 ulow;
-        // rmin ≥ 0 minimal per-second interest rate
-        int256 rmin;
-        // kmin ≥ 0 minimal slope of central segment of the kink
-        int256 kmin;
-        // kmax ≥ kmin max slope of central segment of the kink
-        int256 kmax;
-        // alpha ≥ 0 factor for the slope for the critical segment of the kink
-        int256 alpha;
-        // cplus ≥ 0 coefficent of growth of the slope k
-        int256 cplus;
-        // cminus ≥ 0 coefficent of decrease of the slope k
-        int256 cminus;
-        // c0 ≥ 0 minimal rate of decrease of the slope k
-        int256 c0;
-    }
-
-    struct ConfigWithState {
-         // uopt ∈ (0, 1) – optimal utilization;
-        int256 uopt;
-        // ucrit ∈ (uopt, 1) – threshold of large utilization;
+        // u1 ∈ [0, 1) – lower bound of optimal utilization range;
+        int256 u1;
+        // u2 ∈ [u1, 1] – upper bound of optimal utilization range;
+        int256 u2;
+        // ucrit ∈ [ulow, 1] – threshold of critical utilization;
         int256 ucrit;
-        // ulow ∈ (0, uopt) – threshold of low utilization
-        int256 ulow;
-        // rmin ≥ 0 minimal per-second interest rate
+        // rmin ⩾ 0 – minimal per-second interest rate;
         int256 rmin;
-        // kmin ≥ 0 minimal slope of central segment of the kink
+        // kmin ⩾ 0 – minimal slope k of central segment of the kink;
         int256 kmin;
-        // kmax ≥ kmin max slope of central segment of the kink
+        // kmax ⩾ kmin – maximal slope k of central segment of the kink;
         int256 kmax;
-        // alpha ≥ 0 factor for the slope for the critical segment of the kink
+        // dmax – maximal growth rate of the slope k;
+        int256 dmax;
+        // α ⩾ 0 – factor for the slope for the critical segment of the kink;
         int256 alpha;
-        // cplus ≥ 0 coefficent of growth of the slope k
-        int256 cplus;
-        // cminus ≥ 0 coefficent of decrease of the slope k
+        // c− ⩾ 0 – coefficient of decrease of the slope k;
         int256 cminus;
-        // c0 ≥ 0 minimal rate of decrease of the slope k
-        int256 c0;
-        // state of the slope after latest interest rate accrual
-        int256 k;
+        // c+ ⩾ 0 – growth coefficient of the slope k;
+        int256 cplus;
+        // c1 ⩾ 0 – minimal rate of decrease of the slope k;
+        int256 c1;
+        // c2 ⩾ 0 – minimal growth rate of the slope k.
+        int256 c2;
     }
 
     struct Setup {
+        // constant config
+        Config config;
         // state of the slope after latest interest rate accrual
         int256 k;
-        Config config;
     }
     /* solhint-enable */
 
@@ -77,71 +57,41 @@ interface IDynamicKinkModelV1 {
     error InvalidC0();
     error InvalidK();
 
-    /// @dev Get config for given asset in a Silo. If dedicated config is not set, default one will be returned.
-    /// @param _silo Silo address for which config should be set
-    /// @return Config struct for asset in Silo
-    function getConfig(address _silo) external view returns (ConfigWithState memory);
+    // @ natspec todo
+    /// dev Get config for given asset in a Silo. If dedicated config is not set, default one will be returned.
+    /// param _silo Silo address for which config should be set
+    /// return Config struct for asset in Silo
+    // function getSetup(address _silo) external view returns (Setup memory);
 
-    /// @notice get the flag to detect rcomp restriction (zero current interest) due to overflow
-    /// overflow boolean flag to detect rcomp restriction
-    function overflowDetected(address _silo, uint256 _blockTimestamp)
-        external
-        view
-        returns (bool overflow);
+    /// dev pure function that calculates current annual interest rate
+    /// param _c configuration object, IInterestRateModel.ConfigWithState
+    /// param _totalBorrowAmount current total borrows for asset
+    /// param _totalDeposits current total deposits for asset
+    /// param _interestRateTimestamp timestamp of last interest rate update
+    /// param _blockTimestamp current block timestamp
+    /// return rcur current annual interest rate (1e18 == 100%)
+    // function calculateCurrentInterestRate(
+    //     Setup calldata _setup,
+    //     uint256 _totalDeposits,
+    //     uint256 _totalBorrowAmount,
+    //     uint256 _interestRateTimestamp,
+    //     uint256 _blockTimestamp
+    // ) external pure returns (uint256 rcur);
 
-    /// @dev pure function that calculates current annual interest rate
-    /// @param _c configuration object, IInterestRateModel.ConfigWithState
-    /// @param _totalBorrowAmount current total borrows for asset
-    /// @param _totalDeposits current total deposits for asset
-    /// @param _interestRateTimestamp timestamp of last interest rate update
-    /// @param _blockTimestamp current block timestamp
-    /// @return rcur current annual interest rate (1e18 == 100%)
-    function calculateCurrentInterestRate(
-        ConfigWithState calldata _c,
-        uint256 _totalDeposits,
-        uint256 _totalBorrowAmount,
-        uint256 _interestRateTimestamp,
-        uint256 _blockTimestamp
-    ) external pure returns (uint256 rcur);
-
-    /// @dev pure function that calculates interest rate based on raw input data
-    /// @param _c configuration object, IInterestRateModel.ConfigWithState
-    /// @param _totalBorrowAmount current total borrows for asset
-    /// @param _totalDeposits current total deposits for asset
-    /// @param _interestRateTimestamp timestamp of last interest rate update
-    /// @param _blockTimestamp current block timestamp
-    /// @return rcomp compounded interest rate from last update until now (1e18 == 100%)
-    /// @return ri current integral part of the rate
-    /// @return Tcrit time during which the utilization exceeds the critical value
-    /// @return overflow boolean flag to detect rcomp restriction
-    function calculateCompoundInterestRateWithOverflowDetection(
-        ConfigWithState memory _c,
-        uint256 _totalDeposits,
-        uint256 _totalBorrowAmount,
-        uint256 _interestRateTimestamp,
-        uint256 _blockTimestamp
-    )
-        external
-        pure
-        returns (
-            uint256 rcomp,
-            int256 k
-        );
-
-    /// @dev pure function that calculates interest rate based on raw input data
-    /// @param _c configuration object, IInterestRateModel.ConfigWithState
-    /// @param _totalBorrowAmount current total borrows for asset
-    /// @param _totalDeposits current total deposits for asset
-    /// @param _interestRateTimestamp timestamp of last interest rate update
-    /// @param _blockTimestamp current block timestamp
-    /// @return rcomp compounded interest rate from last update until now (1e18 == 100%)
-    /// @return ri current integral part of the rate
-    /// @return Tcrit time during which the utilization exceeds the critical value
-    function calculateCompoundInterestRate(
-        ConfigWithState memory _c,
-        uint256 _totalDeposits,
-        uint256 _totalBorrowAmount,
-        uint256 _interestRateTimestamp,
-        uint256 _blockTimestamp
-    ) external pure returns (uint256 rcomp, int256 k);
+    /// dev pure function that calculates interest rate based on raw input data
+    /// param _c configuration object, IInterestRateModel.ConfigWithState
+    /// param _totalBorrowAmount current total borrows for asset
+    /// param _totalDeposits current total deposits for asset
+    /// param _interestRateTimestamp timestamp of last interest rate update
+    /// param _blockTimestamp current block timestamp
+    /// return rcomp compounded interest rate from last update until now (1e18 == 100%)
+    /// return ri current integral part of the rate
+    /// return Tcrit time during which the utilization exceeds the critical value
+    // function calculateCompoundInterestRate(
+    //     Setup calldata _setup,
+    //     uint256 _totalDeposits,
+    //     uint256 _totalBorrowAmount,
+    //     uint256 _interestRateTimestamp,
+    //     uint256 _blockTimestamp
+    // ) external pure returns (uint256 rcomp, int256 k);
 }
