d42ef6b5069a30185f9d8d28e6edcfffaafbed2e
Sergey Maksimov <sergey@silo.finance>
2024-04-14 22:58:15 +0300

    work in progress

--- silo-core/contracts/interestRateModel/DynamicKinkModelV1.sol
+++ silo-core/contracts/interestRateModel/DynamicKinkModelV1.sol
@@ -13,35 +13,29 @@ import {IDynamicKinkModelV1} from "../interfaces/IDynamicKinkModelV1.sol";
 
 /// @title DynamicKinkModelV1
 /// @custom:security-contact security@silo.finance
-contract DynamicKinkModelV1 is IInterestRateModel, IDynamicKinkModelV1 {
+contract DynamicKinkModelV1 is IDynamicKinkModelV1 {
     using PRBMathSD59x18 for int256;
     using SafeCast for int256;
     using SafeCast for uint256;
 
     error InvalidTimestamp();
 
-    /// @dev maximum value of current interest rate the model will return
-    uint256 internal immutable R_CURRENT_MAX;
-
-    /// @dev maximum value of compound interest per second the model will return
-    uint256 internal immutable R_COMPOUND_MAX_PER_SECOND;
-
     /// @dev DP is 18 decimal points used for integer calculations
-    uint256 internal constant _DP = 1e18;
-
-    uint256 public K; // TODO
+    int256 internal constant _DP = 1e18;
 
     /// @dev maximum value of X. If x > X_MAX => exp(x) > (2**16) * 1e18. X_MAX = ln((2**16) * 1e18 + 1)
     int256 public constant X_MAX = 88722839111672999628;
 
-    constructor() {
-        R_CURRENT_MAX = 1e20; // this is 10,000% APR in the 18-decimals format
-        R_COMPOUND_MAX_PER_SECOND = R_CURRENT_MAX / (365 * 24 * 3600); // this is per-second rate
-    }
+    /// @dev each Silo setup is stored separately in mapping, that's why we do not need to clone IRM
+    /// at the same time this is safety feature because we will write to this mapping based on msg.sender
+    /// silo => setup
+    mapping (address => Setup) public getSetup;
 
     /// @dev optional method that can connect silo to it's model state on silo initialization
     /// can be empty by must implement interface.
-    function connect(address _configAddress) external;
+    function connect(address _configAddress) external {
+
+    }
 
     /// @dev get compound interest rate and update model storage for current block.timestamp
     /// @param _collateralAssets total silo collateral assets
@@ -60,54 +54,55 @@ contract DynamicKinkModelV1 is IInterestRateModel, IDynamicKinkModelV1 {
     /// @param _u utilization at time t0
     /// @return rcomp compounded interest rate
     /// @return k updated state of the slope k at time t1
-    function compoundInterestRate(ConfigWithState memory _c, uint256 _t0, uint256 _t1, uint256 _u)
+    function compoundInterestRate(Setup memory _setup, int256 _t0, int256 _t1, int256 _u)
         public
         pure
-        returns (uint256 rcomp, uint256 k)
+        returns (int256 rcomp, int256 k)
     {
         if (_t1 <= _t0) revert InvalidTimestamp();
 
-        k = _c.k;
-        uint256 T = _t1 - _t0;
+        k = _setup.k;
+        int256 T = _t1 - _t0;
         int256 slope; // rate of change for k
 
-        if (_u >= _c.uopt) {
-            slope = _c.cplus * (_u - _c.uopt) / _DP;
+        // todo uopt -> u1
+        if (_u >= _setup.config.u1) {
+            slope = _setup.config.cplus * (_u - _setup.config.u1) / _DP;
         } else {
-            slope = - _c.c0 + _c.cminus * (_u - _c.uopt) / _DP;
+            slope = - _setup.config.c1 + _setup.config.cminus * (_u - _setup.config.u1) / _DP;
         }
 
-        int256 k1 = _c.k + slope * T; // slope of the kink at t1 ignoring lower and upper bounds
+        int256 k1 = k + slope * T; // slope of the kink at t1 ignoring lower and upper bounds
 
-        uint256 x; // an integral of k
+        int256 x; // an integral of k
 
-        if (k1 > _c.kmax) {
-            x = _c.kmax * T - (_c.kmax - _c.k)**2 / (2 * slope);
-            k = _c.kmax;
-        } else if (k1 < _c.kmin) {
-            x = _c.kmin * T + (k - _c.kmin)**2 / (2 * slope);
-            k = _c.kmin;
+        if (k1 > _setup.config.kmax) {
+            x = _setup.config.kmax * T - (_setup.config.kmax - k)**2 / (2 * slope);
+            k = _setup.config.kmax;
+        } else if (k1 < _setup.config.kmin) {
+            x = _setup.config.kmin * T + (k - _setup.config.kmin)**2 / (2 * slope);
+            k = _setup.config.kmin;
         } else {
             x = (k + k1) * T / 2;
             k = k1;
         }
 
-        uint256 f; // factor for the slope in kink
-        if (_u >= _c.ulow) {
-            f = _u - _c.ulow;
+        int256 f; // factor for the slope in kink
+        if (_u >= _setup.config.ulow) {
+            f = _u - _setup.config.ulow;
 
-            if (_u >= _c.ucrit) {
-                f = f + _c.alpha * (_u - _c.ucrit) / _DP;
+            if (_u >= _setup.config.ucrit) {
+                f = f + _setup.config.alpha * (_u - _setup.config.ucrit) / _DP;
             }
         }
 
-        x = _c.rmin * T + f * x / _DP;
+        x = _setup.config.rmin * T + f * x / _DP;
 
-        if (x > X_MAX) {
-            rcomp = R_COMPOUND_MAX_PER_SECOND * T;
-        } else {
-            rcomp = x.exp() - _DP;
-        }
+        // if (x > X_MAX) {
+        //     rcomp = R_COMPOUND_MAX_PER_SECOND * T;
+        // } else {
+        //     rcomp = x.exp() - _DP;
+        // }
     }
 
     function currentInterestRate(uint256 _t0, uint256 _t1, uint256 _k, uint256 _u)
@@ -123,25 +118,21 @@ contract DynamicKinkModelV1 is IInterestRateModel, IDynamicKinkModelV1 {
     /// @param _silo address of Silo for which interest rate should be calculated
     /// @param _blockTimestamp current block timestamp
     /// @return rcomp compounded interest rate from last update until now (1e18 == 100%)
-    function getCompoundInterestRate(address _silo, uint256 _blockTimestamp) external view returns (uint256 rcomp);
+    function getCompoundInterestRate(address _silo, uint256 _blockTimestamp) external view returns (uint256 rcomp) {
+
+    }
 
     /// @dev get current annual interest rate
     /// @param _silo address of Silo for which interest rate should be calculated
     /// @param _blockTimestamp current block timestamp
     /// @return rcur current annual interest rate (1e18 == 100%)
-    function getCurrentInterestRate(address _silo, uint256 _blockTimestamp) external view returns (uint256 rcur);
+    function getCurrentInterestRate(address _silo, uint256 _blockTimestamp) external view returns (uint256 rcur) {
 
-    /// @dev returns decimal points used by model
-    function decimals() external view returns (uint256);
-
-    /// @notice limit for compounding interest
-    function getRcompMax(uint256 _t) public pure virtual returns (uint256 rcompMax) {
-        rcompMax = R_COMPOUND_MAX_PER_SECOND * _t;
     }
 
-    /// @notice limit for current interest
-    function getRcurrentMax(uint256 _rcur) public pure virtual returns (uint256 rcurrMax) {
-        rcurrMax = R_CURRENT_MAX;
+    /// @dev returns decimal points used by model
+    function decimals() external view returns (uint256) {
+        return 0;
     }
 }
 
