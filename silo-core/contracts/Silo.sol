4be9408638002530ae4a2e949b37d501673ab1af
Neo Racer <0xNeoRacer@protonmail.com>
2024-04-16 12:41:24 +0200

    _callTransitionCollateralWithdraw

--- silo-core/contracts/Silo.sol
+++ silo-core/contracts/Silo.sol
@@ -395,14 +395,13 @@ contract Silo is Initializable, SiloERC4626 {
                 ? (configData.collateralShareToken, _getRawLiquidity())
                 : (configData.protectedShareToken, total[AssetType.Protected].assets);
 
-            (assets, _shares) = SiloERC4626Lib.transitionCollateralWithdraw(
+            (assets, _shares) = _callTransitionCollateralWithdraw(
                 shareTokenFrom,
                 _shares,
                 _owner,
                 msg.sender,
                 _withdrawType,
-                liquidity,
-                total[_withdrawType]
+                liquidity
             );
         }
 
@@ -996,6 +995,7 @@ contract Silo is Initializable, SiloERC4626 {
         returns (uint256 assets, uint256 shares)
     {
         ISiloConfig siloConfigCached = config;
+
         (,ISiloConfig.ConfigData memory debtConfig,, IHookReceiver hookReceiverAfter) = siloConfigCached.startAction(
             address(this),
             _borrower,
@@ -1208,6 +1208,25 @@ contract Silo is Initializable, SiloERC4626 {
             _spender,
             total[AssetType.Debt],
             total[AssetType.Collateral].assets
+        );
+    }
+
+    function _callTransitionCollateralWithdraw(
+        address _shareToken,
+        uint256 _shares,
+        address _owner,
+        address _spender,
+        ISilo.AssetType _assetType,
+        uint256 _liquidity
+    ) internal returns (uint256 assets, uint256 shares) {
+        (assets, _shares) = SiloERC4626Lib.transitionCollateralWithdraw(
+            _shareToken,
+            _shares,
+            _owner,
+            _spender,
+            _assetType,
+            _liquidity,
+            total[_assetType]
         );
     }
 }
