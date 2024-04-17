18122fb005d18dab7601f1afd22ea58d56365a01
Otakar <Otakar@certora.com>
2024-04-17 08:29:38 +0200

    Added hook to call to accrueInterest

--- certora/harness/silo0/Silo0.sol
+++ certora/harness/silo0/Silo0.sol
@@ -1,9 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.21;
 
+import {ISiloConfig} from "silo-core/contracts/interfaces/ISiloConfig.sol";
 import {SiloHarness} from "../SiloHarness.sol";
 import {ISiloFactory} from "silo-core/contracts/interfaces/ISiloFactory.sol";
 
 contract Silo0 is SiloHarness {
     constructor(ISiloFactory _siloFactory) SiloHarness(_siloFactory) {}
+
+    function _accrueInterest_orig() external
+        returns (uint256 accruedInterest, ISiloConfig.ConfigData memory configData)
+    {
+        configData = config.getConfig(address(this));
+
+        accruedInterest = _callAccrueInterestForAsset(
+            configData.interestRateModel, configData.daoFee, configData.deployerFee, address(0)
+        );
+    }
+
 }
