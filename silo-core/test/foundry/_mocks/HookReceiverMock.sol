d311d7079c646cb3b4c9e2ec469b04c95bf4a81d
IhorSF <ihor@silo.finance>
2024-04-30 12:16:00 +0400

    Mock call

--- silo-core/test/foundry/_mocks/HookReceiverMock.sol
+++ silo-core/test/foundry/_mocks/HookReceiverMock.sol
@@ -9,25 +9,15 @@ import {IHookReceiver} from "silo-core/contracts/utils/hook-receivers/interfaces
 contract HookReceiverMock is CommonBase, StdCheats {
     address public immutable ADDRESS;
 
-    uint24 public hooksBefore;
-    uint24 public hooksAfter;
-
     constructor(address _hook) {
         ADDRESS = _hook == address(0) ? makeAddr("HookReceiverMock") : _hook;
     }
 
     function hookReceiverConfigMock(uint24 _hooksBefore, uint24 _hooksAfter) public {
-        hooksBefore = _hooksBefore;
-        hooksAfter = _hooksAfter;
-        // vm.mockCall(
-        //     ADDRESS,
-        //     abi.encodeWithSelector(IHookReceiver.hookReceiverConfig.selector),
-        //     abi.encode(hooksBefore, hooksAfter)
-        // );
-    }
+        bytes memory data = abi.encodeWithSelector(IHookReceiver.hookReceiverConfig.selector);
 
-    function hookReceiverConfig() public view returns (uint24, uint24) {
-        return (hooksBefore, hooksAfter);
+        vm.mockCall(ADDRESS, data, abi.encode(_hooksBefore, _hooksAfter));
+        vm.expectCall(ADDRESS, data);
     }
 
     // TODO
