b25d9815d8466d26af197ff48f8d26f93b96dc64
Neo Racer <0xNeoRacer@protonmail.com>
2024-05-02 10:05:12 +0200

    .

--- silo-core/contracts/lib/AssetTypes.sol
+++ silo-core/contracts/lib/AssetTypes.sol
@@ -3,12 +3,12 @@ pragma solidity 0.8.21;
 
 // solhint-disable private-vars-leading-underscore
 library AssetTypes {
-    /// @dev must match value of ISilo.AssetType.Protected
+    /// @dev must match value of AssetTypes.Protected
     uint256 internal constant Protected = 0;
 
-    /// @dev must match value of ISilo.AssetType.Collateral
+    /// @dev must match value of AssetTypes.Collateral
     uint256 internal constant Collateral = 1;
 
-    /// @dev must match value of ISilo.AssetType.Debt
+    /// @dev must match value of AssetTypes.Debt
     uint256 internal constant Debt = 2;
 }
