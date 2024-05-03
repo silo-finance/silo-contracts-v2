cddb7bfdc5805622731de8c237fc32e284754b65
IhorSF <ihor@silo.finance>
2024-05-03 10:05:43 +0400

    silo-core: GaugeHookReceiver & tests

--- silo-core/contracts/utils/hook-receivers/gauge/GaugeHookReceiver.sol
+++ silo-core/contracts/utils/hook-receivers/gauge/GaugeHookReceiver.sol
@@ -7,47 +7,82 @@ import {Initializable} from "openzeppelin5/proxy/utils/Initializable.sol";
 import {ISilo} from "silo-core/contracts/interfaces/ISilo.sol";
 import {IShareToken} from "silo-core/contracts/interfaces/IShareToken.sol";
 import {ISiloConfig} from "silo-core/contracts/interfaces/ISiloConfig.sol";
+import {Hook} from "silo-core/contracts/lib/Hook.sol";
+import {HookActionDataDecoder} from "../lib/HookActionDataDecoder.sol";
 import {IGaugeLike as IGauge} from "./interfaces/IGaugeLike.sol";
 import {IGaugeHookReceiver, IHookReceiver} from "./interfaces/IGaugeHookReceiver.sol";
 import {SiloHookReceiver} from "../_common/SiloHookReceiver.sol";
-import {Hook} from "../../../lib/Hook.sol";
 
 /// @notice Silo share token hook receiver for the gauge.
 /// It notifies the gauge (if configured) about any balance update in the Silo share token.
 contract GaugeHookReceiver is IGaugeHookReceiver, SiloHookReceiver, Ownable2Step, Initializable {
     using Hook for uint256;
+    using Hook for uint24;
+
+    uint256 internal constant HOOKS_BEFORE_NOT_CONFIGURED = 0;
 
     IGauge public gauge;
     IShareToken public shareToken;
+    ISiloConfig public siloConfig;
+
+    mapping(IShareToken => IGauge) public configuredGauges;
 
     constructor() Ownable(msg.sender) {
         _disableInitializers();
+        _transferOwnership(address(0));
     }
 
     /// @notice Initialize a hook receiver
     /// @param _owner Owner of the hook receiver (DAO)
-    function initialize(address _owner, ISiloConfig /* siloConfig */) external virtual initializer {
+    function initialize(address _owner, ISiloConfig _siloConfig) external virtual initializer {
         if (_owner == address(0)) revert OwnerIsZeroAddress();
+        if (address(_siloConfig) == address(0)) revert EmptySiloConfig();
 
+        siloConfig = _siloConfig;
         _transferOwnership(_owner);
     }
 
     /// @inheritdoc IGaugeHookReceiver
     function setGauge(IGauge _gauge) external virtual onlyOwner {
-        if (address(gauge) != address(0) && !gauge.is_killed()) revert CantUpdateActiveGauge();
-        if (_gauge.share_token() != address(shareToken)) revert WrongGaugeShareToken();
+        IShareToken _shareToken = IShareToken(_gauge.share_token());
+        IGauge configuredGauge = configuredGauges[_shareToken];
 
-        gauge = _gauge;
+        if (address(configuredGauge) == address(0) && address(_gauge) == address(0)) revert GaugeIsNotConfigured();
+        if (address(configuredGauge) != address(0) && !configuredGauge.is_killed()) revert CantUpdateActiveGauge();
 
-        emit GaugeConfigured(address(gauge));
+        address silo = _shareToken.silo();
+
+        uint24 tokenType = _getTokenType(silo, address(_shareToken));
+        uint24 hooksAfter = _getHooksAfter(silo);
+
+        if (configuredGauge == address(0)) {
+            // If gauge was not configured before we add action to the hooksAfter
+            uint24 action = tokenType | Hook.SHARE_TOKEN_TRANSFER;
+            hooksAfter = hooksAfter.addAction(action);
+        } else if (address(_gauge) == address(0)) {
+            // if provided gauge is zero address we remove action from the hooksAfter
+            hooksAfter = hooksAfter.removeAction(tokenType);
+        }
+
+        _setHookConfig(silo, HOOKS_BEFORE_NOT_CONFIGURED, hooksAfter);
+
+        configuredGauges[_shareToken] = _gauge;
+
+        emit GaugeConfigured(address(gauge), address(_shareToken));
     }
 
-    function beforeAction(address /* _silo */, uint256 /* _action */, bytes calldata /* _input */) external {
-        // TODO
+    function beforeAction(address, uint256, bytes calldata) external {
+        // Do not expect any actions.
+        revert RequestNotSupported();
     }
 
-    function afterAction(address /* _silo */, uint256 _action, bytes calldata _inputAndOutput) external {
-        if (_action.matchAction(Hook.SHARE_TOKEN_TRANSFER)) return;
+    function afterAction(address _silo, uint256 _action, bytes calldata _inputAndOutput) external {
+        IGauge theGauge = configuredGauges[msg.sender];
+
+        if (theGauge == IGauge(address(0))) revert GaugeIsNotConfigured();
+
+        if (theGauge.is_killed()) return; // Do not revert if gauge is killed. Ignore the action.
+        if (_getHooksAfter(silo).matchAction(_action)) return; // Should not happen, but just in case
 
         (
             address sender,
@@ -56,13 +91,7 @@ contract GaugeHookReceiver is IGaugeHookReceiver, SiloHookReceiver, Ownable2Step
             uint256 senderBalance,
             uint256 recipientBalance,
             uint256 totalSupply
-        ) = abi.decode(_inputAndOutput, (address, address, uint256, uint256, uint256, uint256));
-
-        if (msg.sender != address(shareToken)) revert Unauthorized();
-
-        IGauge theGauge = gauge;
-
-        if (address(theGauge) == address(0) || theGauge.is_killed()) return;
+        ) = _encodedParams.afterTokenTransferDecode();
 
         theGauge.afterTokenTransfer(
             sender,
@@ -71,5 +100,19 @@ contract GaugeHookReceiver is IGaugeHookReceiver, SiloHookReceiver, Ownable2Step
             recipientBalance,
             totalSupply
         );
+    }
+
+    function _getTokenType(address _silo, address _shareToken) internal view returns (uint24) {
+        (
+            address protectedShareToken,
+            address collateralShareToken,
+            address debtShareToken
+        ) = siloConfig.shareTokens(_silo);
+
+        if (_shareToken == collateralShareToken) return uint24(Hook.SHARE_TOKEN_COLLATERAL);
+        if (_shareToken == protectedShareToken) return uint24(Hook.SHARE_TOKEN_PROTECTED);
+        if (_shareToken == debtShareToken) return uint24(Hook.SHARE_TOKEN_DEBT);
+
+        revert InvalidShareToken();
     }
 }
--- silo-core/contracts/utils/hook-receivers/gauge/interfaces/IGaugeHookReceiver.sol
+++ silo-core/contracts/utils/hook-receivers/gauge/interfaces/IGaugeHookReceiver.sol
@@ -9,10 +9,9 @@ import {IGaugeLike as IGauge} from "./IGaugeLike.sol";
 interface IGaugeHookReceiver is IHookReceiver {
     /// @dev Emit when the new gauge is configured
     /// @param gauge Gauge for which hook receiver will send notification about the share token balance updates.
-    event GaugeConfigured(address gauge);
+    /// @param shareToken Share token.
+    event GaugeConfigured(address gauge, address shareToken);
 
-    /// @dev Revert if other than `shareToken` address call `afterTokenTransfer` fn
-    error Unauthorized();
     /// @dev Revert on an attempt to inialize with a zero `_owner` address
     error OwnerIsZeroAddress();
     /// @dev Revert on an attempt to initialize with an invalid `_shareToken` address
@@ -24,12 +23,23 @@ interface IGaugeHookReceiver is IHookReceiver {
     error CantUpdateActiveGauge();
     /// @dev Revert if the gauge hook receiver already has a configured gauge
     error AlreadyConfigured();
+    /// @dev Revert on an attempt to update a gauge with a zero address
+    error EmptyGaugeAddress();
+    /// @dev Too more gauges were passed to the hook receiver
+    error TooManyGauges();
+    /// @dev Revert if provided silo configuration during initialization is empty
+    error EmptySiloConfig();
+    /// @dev Revert if the hook received `beforeAction` notification
+    error RequestNotSupported();
+    /// @dev Revert on an attempt to remove not configured gauge
+    error GaugeIsNotConfigured();
 
     /// @notice Configuration of the gauge
     /// for which the hook receiver should send notifications about the share token balance updates.
     /// The `_gauge` can be updated by an owner (DAO)
-    /// @param _gauge Gauge that should receive notifications
-    function setGauge(IGauge _gauge) external;
+    /// @dev Overrides existing configuration
+    /// @param _gauges Array of gauges for which hook receiver will send notification.
+    function setGauges(IGauge[] calldata _gauges) external;
 
     function gauge() external view returns (IGauge);
     function shareToken() external view returns (IShareToken);
