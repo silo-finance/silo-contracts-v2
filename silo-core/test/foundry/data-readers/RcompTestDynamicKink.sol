4e471d2eaeebcdb8528c0d04889d54e277a2bb51
Sergey Maksimov <sergey@silo.finance>
2024-05-06 11:55:26 +0300

    update test cases

--- silo-core/test/foundry/data-readers/RcompTestDynamicKink.sol
+++ silo-core/test/foundry/data-readers/RcompTestDynamicKink.sol
@@ -60,7 +60,7 @@ contract RcompTestDynamicKink is Test {
 
     function _readDataFromJsonRcomp() internal returns (RcompData[] memory data) {
         string memory root = vm.projectRoot();
-        string memory path = string.concat(root, "/silo-core/test/foundry/data/RcompDynamicKinkv7.json");
+        string memory path = string.concat(root, "/silo-core/test/foundry/data/RcompDynamicKinkv8.json");
         string memory json = vm.readFile(path);
 
         data = abi.decode(vm.parseJson(json, string(abi.encodePacked("."))), (RcompData[]));
