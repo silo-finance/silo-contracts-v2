28e0d99d250136293990c05042cc80413a21a832
Neo Racer <0xNeoRacer@protonmail.com>
2024-05-15 14:36:08 +0200

    debug

--- silo-core/contracts/lib/SiloLendingLib.sol
+++ silo-core/contracts/lib/SiloLendingLib.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.20;
 
+import {console} from "forge-std/console.sol";
+
 import {SafeERC20} from "openzeppelin5/token/ERC20/utils/SafeERC20.sol";
 import {IERC20} from "openzeppelin5/token/ERC20/IERC20.sol";
 import {IERC20Metadata} from "openzeppelin5/token/ERC20/extensions/IERC20Metadata.sol";
@@ -167,6 +169,9 @@ library SiloLendingLib {
         if (_token != address(0) &&
             borrowedAssets > SiloMathLib.liquidity(_args.totalCollateralAssets, totalDebtAssets)
         ) {
+            console.log("_args.assets", _args.assets);
+            console.log("_args.shares", _args.shares);
+            console.log("liquidity", SiloMathLib.liquidity(_args.totalCollateralAssets, totalDebtAssets));
             revert ISilo.NotEnoughLiquidity();
         }
 
