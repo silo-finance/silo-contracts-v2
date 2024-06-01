90821c3f756addbc0d6f66f64e78466714201f9b
IhorSF <ihor@silo.finance>
2024-06-01 17:29:54 +0400

    silo-core: liquidation hooks tests and docs

--- silo-core/test/foundry/Silo/hooks/SiloHooksActions.sol
+++ silo-core/test/foundry/Silo/hooks/SiloHooksActions.sol
@@ -448,6 +448,40 @@ contract SiloHooksActionsTest is SiloLittleHelper, Test, HookMock {
         _siloLeverageSameAssetAllHooks(PROTECTED);
     }
 
+    /// FOUNDRY_PROFILE=core-test forge test -vvv --ffi --mt testLiqudationAllHooks
+    function testLiqudationAllHooks() public {
+        uint256 beforeActions = Hook.LIQUIDATION;
+
+        uint256 afterAction = beforeActions
+            .addAction(Hook.shareTokenTransfer(Hook.PROTECTED_TOKEN)) // as we have protected deposit
+            .addAction(Hook.shareTokenTransfer(Hook.DEBT_TOKEN));
+
+        HookMock hookReceiverMock = new HookMock(beforeActions, afterAction, beforeActions, afterAction);
+        deploySiloWithHook(address(hookReceiverMock));
+
+        _depositForBorrowNotSameAsset();
+
+        uint256 borrowAmount = 70e18;
+
+        vm.prank(_borrower);
+        silo0.borrow(borrowAmount, _borrower, _borrower, _NOT_SAME_ASSET);
+
+        // liquidation
+        vm.warp(block.timestamp + 70 days);
+
+        uint256 collateralToLiquidate;
+        uint256 debtToRepay;
+
+        (collateralToLiquidate, debtToRepay) = partialLiquidation.maxLiquidation(address(silo0), _borrower);
+
+        assertGt(collateralToLiquidate, 0, "expect collateralToLiquidate");
+
+        token0.mint(address(this), debtToRepay);
+        token0.approve(address(silo0), debtToRepay);
+
+        _liquidationAllHooks(_borrower, debtToRepay, borrowAmount);
+    }
+
     function _siloDepositWithHook(
         ISilo _silo,
         MintableToken _token,
@@ -631,7 +665,7 @@ contract SiloHooksActionsTest is SiloLittleHelper, Test, HookMock {
             _amount,
             0, // no balance for the sender
             0, // no balance
-            0, // mo total supply
+            0, // no total supply
             _collateralType
         );
 
@@ -831,7 +865,7 @@ contract SiloHooksActionsTest is SiloLittleHelper, Test, HookMock {
             _amount,
             0, // no balance for the sender
             0, // no balance
-            0, // mo total supply
+            0, // no total supply
             _withdrawType == COLLATERAL ? COLLATERAL : PROTECTED
         );
 
@@ -931,6 +965,67 @@ contract SiloHooksActionsTest is SiloLittleHelper, Test, HookMock {
         );
     }
 
+    function _liquidationAllHooks(address _borrowerAddr, uint256 _debtToRepay, uint256 _borrowAmount) internal {
+        bool receiveSToken = false;
+        uint256 expectedWithdrawCollateral = 100000000000000000000;
+        uint256 expectedRepayDebtAssets = 25600000000000000000000;
+
+        vm.expectEmit(true, true, true, true);
+        
+        emit LiquidationBeforeHA(
+            address(silo0),
+            address(silo0),
+            address(token1),
+            address(token0),
+            _borrowerAddr,
+            _debtToRepay,
+            receiveSToken
+        );
+
+        vm.expectEmit(true, true, true, true);
+
+        emit DebtShareTokenAfterHA(
+            address(silo0),
+            _borrowerAddr,
+            address(0), // because we burn debt share tokens
+            _borrowAmount,
+            0, // no balance for the sender
+            0, // balance
+            0 // total supply
+        );
+
+        vm.expectEmit(true, true, true, true);
+
+        emit ShareTokenAfterHA(
+            address(silo1),
+            _borrowerAddr,
+            address(0), // because we burn tokens
+            expectedWithdrawCollateral,
+            0, // no balance for the sender
+            0, // no balance
+            0, // no total supply
+            PROTECTED
+        );
+
+        vm.expectEmit(true, true, true, true);
+
+        emit LiquidationAfterHA(
+            address(silo0),
+            address(silo0),
+            address(token1),
+            address(token0),
+            _borrowerAddr,
+            _debtToRepay,
+            receiveSToken,
+            expectedWithdrawCollateral,
+            expectedRepayDebtAssets
+        );
+
+        partialLiquidation.liquidationCall(
+            address(silo0), address(token1), address(token0), _borrower, _debtToRepay, receiveSToken
+        );
+    }
+
     function _depositForBorrowNotSameAsset() internal {
         uint256 depositAmount = 100e18;
         uint256 collateralAmount = 100e18;
@@ -948,12 +1043,17 @@ contract SiloHooksActionsTest is SiloLittleHelper, Test, HookMock {
         configOverride.hookReceiverImplementation = _hookReceiver;
         configOverride.configName = SiloConfigsNames.LOCAL_DEPLOYER;
 
-        (_siloConfig, silo0, silo1,,,) = _siloFixture.deploy_local(configOverride);
+        (_siloConfig, silo0, silo1,,, partialLiquidation) = _siloFixture.deploy_local(configOverride);
 
         __init(MintableToken(configOverride.token0), MintableToken(configOverride.token1), silo0, silo1);
 
         ISiloConfig.ConfigData memory configData = _siloConfig.getConfig(address(silo0));
 
         _siloHookReceiver = HookMock(configData.hookReceiver);
+
+        vm.label(address(silo0), "silo0");
+        vm.label(address(silo1), "silo1");
+        vm.label(address(configOverride.token0), "token0");
+        vm.label(address(configOverride.token1), "token1");
     }
 }
