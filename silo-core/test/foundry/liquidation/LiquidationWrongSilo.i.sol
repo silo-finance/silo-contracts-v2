7dc5a7e14ef65959af41567d35ee73490dd5483d
IhorSF <ihor@silo.finance>
2024-06-03 09:31:55 +0400

    imports

--- silo-core/test/foundry/liquidation/LiquidationWrongSilo.i.sol
+++ silo-core/test/foundry/liquidation/LiquidationWrongSilo.i.sol
@@ -1,21 +1,13 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.20;
 
-import "forge-std/Test.sol";
-
-import {IERC20} from "openzeppelin5/token/ERC20/IERC20.sol";
+import {Test} from "forge-std/Test.sol";
 
 import {ISiloConfig} from "silo-core/contracts/interfaces/ISiloConfig.sol";
-import {ISilo} from "silo-core/contracts/interfaces/ISilo.sol";
 import {IPartialLiquidation} from "silo-core/contracts/interfaces/IPartialLiquidation.sol";
-import {IShareToken} from "silo-core/contracts/interfaces/IShareToken.sol";
-import {IInterestRateModel} from "silo-core/contracts/interfaces/IInterestRateModel.sol";
-import {SiloLensLib} from "silo-core/contracts/lib/SiloLensLib.sol";
 import {Hook} from "silo-core/contracts/lib/Hook.sol";
-import {AssetTypes} from "silo-core/contracts/lib/AssetTypes.sol";
 
 import {SiloLittleHelper} from "../_common/SiloLittleHelper.sol";
-import {MintableToken} from "../_common/MintableToken.sol";
 
 /*
 FOUNDRY_PROFILE=core-test forge test -vv --ffi --mc LiquidationWrongSiloTest
