1f28a2e528e32fa3e6bec04d73de560f0568a247
Neo Racer <0xNeoRacer@protonmail.com>
2024-06-19 08:11:03 +0200

    remove liquidation hook

--- silo-core/contracts/lib/Hook.sol
+++ silo-core/contracts/lib/Hook.sol
@@ -204,42 +204,6 @@ library Hook {
         address user;
     }
 
-    /// @notice The data structure for the before liquidation hook
-    /// @param siloWithDebt The silo with debt
-    /// @param collateralAsset The collateral asset
-    /// @param debtAsset The debt asset
-    /// @param borrower The borrower
-    /// @param debtToCover The debt to cover
-    /// @param receiveSToken Whether to receive sToken
-    struct BeforeLiquidationInput {
-        address siloWithDebt;
-        address collateralAsset;
-        address debtAsset;
-        address borrower;
-        uint256 debtToCover;
-        bool receiveSToken;
-    }
-
-    /// @notice The data structure for the after liquidation hook
-    /// @param siloWithDebt The silo with debt
-    /// @param collateralAsset The collateral asset
-    /// @param debtAsset The debt asset
-    /// @param borrower The borrower
-    /// @param debtToCover The debt to cover
-    /// @param receiveSToken Whether to receive sToken
-    /// @param withdrawCollateral The amount of collateral withdrawn
-    /// @param repayDebtAssets The amount of debt repaid
-    struct AfterLiquidationInput {
-        address siloWithDebt;
-        address collateralAsset;
-        address debtAsset;
-        address borrower;
-        uint256 debtToCover;
-        bool receiveSToken;
-        uint256 withdrawCollateral;
-        uint256 repayDebtAssets;
-    }
-
     /// @notice Supported hooks
     /// @dev The hooks are stored as a bitmap and can be combined with bitwise OR
     uint256 internal constant NONE = 0;
@@ -253,11 +217,10 @@ library Hook {
     uint256 internal constant FLASH_LOAN = 2 ** 8;
     uint256 internal constant TRANSITION_COLLATERAL = 2 ** 9;
     uint256 internal constant SWITCH_COLLATERAL = 2 ** 10;
-    uint256 internal constant LIQUIDATION = 2 ** 11;
-    uint256 internal constant SHARE_TOKEN_TRANSFER = 2 ** 12;
-    uint256 internal constant COLLATERAL_TOKEN = 2 ** 13;
-    uint256 internal constant PROTECTED_TOKEN = 2 ** 14;
-    uint256 internal constant DEBT_TOKEN = 2 ** 15;
+    uint256 internal constant SHARE_TOKEN_TRANSFER = 2 ** 11;
+    uint256 internal constant COLLATERAL_TOKEN = 2 ** 12;
+    uint256 internal constant PROTECTED_TOKEN = 2 ** 13;
+    uint256 internal constant DEBT_TOKEN = 2 ** 14;
 
     // note: currently we can support hook value up to 2 ** 23,
     // because for optimisation purposes, we storing hooks as uint24
@@ -794,46 +757,6 @@ library Hook {
             ISilo.CollateralType(collateralType),
             depositedShares,
             borrowedShares
-        );
-    }
-
-    /// @dev Decodes packed data from the before liquidation hook
-    /// @param packed The packed data (via abi.encodePacked)
-    /// @return input decoded
-    function beforeLiquidationDecode(bytes memory packed)
-        internal
-        pure
-        returns (BeforeLiquidationInput memory input)
-    {
-        address siloWithDebt;
-        address collateralAsset;
-        address debtAsset;
-        address borrower;
-        uint256 debtToCover;
-        uint8 receiveSToken;
-
-        assembly { // solhint-disable-line no-inline-assembly
-            let pointer := PACKED_ADDRESS_LENGTH
-            siloWithDebt := mload(add(packed, pointer))
-            pointer := add(pointer, PACKED_ADDRESS_LENGTH)
-            collateralAsset := mload(add(packed, pointer))
-            pointer := add(pointer, PACKED_ADDRESS_LENGTH)
-            debtAsset := mload(add(packed, pointer))
-            pointer := add(pointer, PACKED_ADDRESS_LENGTH)
-            borrower := mload(add(packed, pointer))
-            pointer := add(pointer, PACKED_FULL_LENGTH)
-            debtToCover := mload(add(packed, pointer))
-            pointer := add(pointer, PACKED_BOOL_LENGTH)
-            receiveSToken := mload(add(packed, pointer))
-        }
-
-        input = BeforeLiquidationInput(
-            siloWithDebt,
-            collateralAsset,
-            debtAsset,
-            borrower,
-            debtToCover,
-            _toBoolean(receiveSToken)
         );
     }
 
