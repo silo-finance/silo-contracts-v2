66572be9d4c439702974deab535fba7921cae61b
Neo Racer <0xNeoRacer@protonmail.com>
2024-06-19 17:59:08 +0200

    compilable

--- silo-core/contracts/interfaces/IPartialLiquidation.sol
+++ silo-core/contracts/interfaces/IPartialLiquidation.sol
@@ -68,8 +68,8 @@ interface IPartialLiquidation {
     /// @return shares The equivalent number of shares for the provided asset amount
     function liquidationRepay(uint256 _assets, address _borrower, address _repayer) external returns (uint256 shares);
 
-    /// @dev this is only for liquidation, should be called as delegate call
-    /// copy of `_withdraw` from Silo
+    /// @dev that method allow to finish liquidation process by giving up collateral to liquidator
+    /// @notice this withdraw is only for liquidation, because it must be called as delegate call from Silo
     function liquidationWithdraw(
         uint256 _assets,
         uint256 _shares,
@@ -79,16 +79,6 @@ interface IPartialLiquidation {
     )
         external
         returns (uint256 assets, uint256 shares);
-
-    /// @dev that method allow to finish liquidation process by giving up collateral to liquidator
-    /// @notice this withdraw is only for liquidation, because it must be called as delegate call from Silo
-    function withdrawCollateralsToLiquidator(
-        uint256 _withdrawAssetsFromCollateral,
-        uint256 _withdrawAssetsFromProtected,
-        address _borrower,
-        address _liquidator,
-        bool _receiveSToken
-    ) external;
 
     /// @dev debt is keep growing over time, so when dApp use this view to calculate max, tx should never revert
     /// because actual max can be only higher
