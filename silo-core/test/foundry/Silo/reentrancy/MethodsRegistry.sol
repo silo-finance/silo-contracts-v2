450f3a5205f8a7bb1fe6deb038653bdc160c0653
IhorSF <ihor@silo.finance>
2024-06-24 13:14:42 +0400

    more tests

--- silo-core/test/foundry/Silo/reentrancy/MethodsRegistry.sol
+++ silo-core/test/foundry/Silo/reentrancy/MethodsRegistry.sol
@@ -2,22 +2,38 @@
 pragma solidity ^0.8.20;
 
 import {IMethodReentrancyTest} from "./interfaces/IMethodReentrancyTest.sol";
+import {AccrueInterestReentrancyTest} from "./silo-methods/AccrueInterestReentrancyTest.sol";
+import {AccrueInterestForConfigReentrancyTest} from "./silo-methods/AccrueInterestForConfigReentrancyTest.sol";
+import {AllowanceReentrancyTest} from "./silo-methods/AllowanceReentrancyTest.sol";
+import {ApproveReentrancyTest} from "./silo-methods/ApproveReentrancyTest.sol";
+import {AssetReentrancyTest} from "./silo-methods/AssetReentrancyTest.sol";
+import {BalanceOfReentrancyTest} from "./silo-methods/BalanceOfReentrancyTest.sol";
+import {BorrowReentrancyTest} from "./silo-methods/BorrowReentrancyTest.sol";
+import {BorrowSharesReentrancyTest} from "./silo-methods/BorrowSharesReentrancyTest.sol";
+import {CallOnBehalfOfSiloReentrancyTest} from "./silo-methods/CallOnBehalfOfSiloReentrancyTest.sol";
 import {DepositReentrancyTest} from "./silo-methods/DepositReentrancyTest.sol";
 import {DepositWithTypeReentrancyTest} from "./silo-methods/DepositWithTypeReentrancyTest.sol";
 import {WithdrawReentrancyTest} from "./silo-methods/WithdrawReentrancyTest.sol";
 import {WithdrawWithTypeReentrancyTest} from "./silo-methods/WithdrawWithTypeReentrancyTest.sol";
-import {BorrowReentrancyTest} from "./silo-methods/BorrowReentrancyTest.sol";
 
 contract MethodsRegistry {
     mapping(bytes4 methodSig => IMethodReentrancyTest) public methods;
     bytes4[] public supportedMethods;
 
     constructor() {
+        _registerMethod(new AccrueInterestReentrancyTest());
+        _registerMethod(new AccrueInterestForConfigReentrancyTest());
+        _registerMethod(new AllowanceReentrancyTest());
+        _registerMethod(new ApproveReentrancyTest());
+        _registerMethod(new AssetReentrancyTest());
+        _registerMethod(new BalanceOfReentrancyTest());
+        _registerMethod(new BorrowReentrancyTest());
+        _registerMethod(new BorrowSharesReentrancyTest());
+        _registerMethod(new CallOnBehalfOfSiloReentrancyTest());
         _registerMethod(new DepositReentrancyTest());
         _registerMethod(new DepositWithTypeReentrancyTest());
         _registerMethod(new WithdrawReentrancyTest());
         _registerMethod(new WithdrawWithTypeReentrancyTest());
-        _registerMethod(new BorrowReentrancyTest());
     }
 
     function supportedMethodsLength() external view returns (uint256) {
---
+++ silo-core/test/foundry/Silo/reentrancy/silo-methods/CallOnBehalfOfSiloReentrancyTest.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+
+import {ISilo} from "silo-core/contracts/interfaces/ISilo.sol";
+import {ISiloConfig} from "silo-core/contracts/interfaces/ISiloConfig.sol";
+import {IShareToken} from "silo-core/contracts/interfaces/IShareToken.sol";
+import {IMethodReentrancyTest} from "../interfaces/IMethodReentrancyTest.sol";
+import {TestStateLib} from "../TestState.sol";
+import {MaliciousToken} from "../MaliciousToken.sol";
+
+contract CallOnBehalfOfSiloReentrancyTest is Test, IMethodReentrancyTest {
+    function callMethod() external {
+        emit log_string("\tEnsure it will not revert");
+        _ensureItWillNotRevert();
+    }
+
+    function verifyReentrancy() external {
+        _ensureItWillNotRevert();
+    }
+
+    function methodDescription() external pure returns (string memory description) {
+        description = "borrowShares(uint256,address,address,bool)";
+    }
+
+    function methodSignature() external pure returns (bytes4 sig) {
+        sig = ISilo.borrowShares.selector;
+    }
+
+    function _ensureItWillNotRevert() internal {
+        ISilo silo0 = TestStateLib.silo0();
+        ISilo silo1 = TestStateLib.silo1();
+        ISiloConfig config = TestStateLib.siloConfig();
+
+        ISiloConfig.ConfigData memory config0 = config.getConfig(address(silo0));
+
+        address anyAddress = makeAddr("Any address");
+        bytes memory payload = abi.encodeWithSelector(IShareToken.balanceOfAndTotalSupply.selector, anyAddress);
+
+        vm.prank(address(config0.hookReceiver));
+        (bool success,) = silo0.callOnBehalfOfSilo(
+            config0.protectedShareToken,
+            0 /* eth value */,
+            ISilo.CallType.Call,
+            payload
+        );
+
+        if (!success) revert();
+
+        vm.prank(address(config0.hookReceiver));
+        (success,) = silo1.callOnBehalfOfSilo(
+            config0.protectedShareToken,
+            0 /* eth value */,
+            ISilo.CallType.Call,
+            payload
+        );
+
+        if (!success) revert();
+    }
+}
