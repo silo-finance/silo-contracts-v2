ad0f95be66c0f94a2079b1d4722d464029c6d53d
IhorSF <ihor@silo.finance>
2024-06-27 08:55:04 +0400

    collateral share token transfer

--- silo-core/test/foundry/Silo/reentrancy/registries/CollateralShareTokenMethodsRegistry.sol
+++ silo-core/test/foundry/Silo/reentrancy/registries/CollateralShareTokenMethodsRegistry.sol
@@ -3,8 +3,11 @@ pragma solidity ^0.8.20;
 
 import {ShareTokenMethodsRegistry} from "./ShareTokenMethodsRegistry.sol";
 
+import {TransferReentrancyTest} from "../methods/collateral-share-token/TransferReentrancyTest.sol";
+
 contract CollateralShareTokenMethodsRegistry is ShareTokenMethodsRegistry {
     constructor() ShareTokenMethodsRegistry() {
+        _registerMethod(new TransferReentrancyTest());
     }
 
     function abiFile() external pure override returns (string memory) {
