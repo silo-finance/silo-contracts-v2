95e6c5565a61cfc1b7fa022a435edede22bd80ca
IhorSF <ihor@silo.finance>
2024-06-28 12:03:36 +0400

    AccrueInterestAndGetConfigOptimisedReentrancyTest

--- silo-core/test/foundry/Silo/reentrancy/methods/silo-config/AccrueInterestAndGetConfigOptimisedReentrancyTest.sol
+++ silo-core/test/foundry/Silo/reentrancy/methods/silo-config/AccrueInterestAndGetConfigOptimisedReentrancyTest.sol
@@ -6,19 +6,30 @@ import {ISiloConfig} from "silo-core/contracts/interfaces/ISiloConfig.sol";
 import {MethodReentrancyTest} from "../MethodReentrancyTest.sol";
 import {TestStateLib} from "../../TestState.sol";
 
+contract FakeSilo {
+    function accrueInterestForConfig(address,uint256,uint256) external {}
+}
+
 contract AccrueInterestAndGetConfigOptimisedReentrancyTest is MethodReentrancyTest {
     function callMethod() external {
         emit log_string("\tEnsure it will revert (permissions test)");
         ISiloConfig config = TestStateLib.siloConfig();
 
-        vm.expectRevert(ISiloConfig.OnlySiloOrDebtShareToken.selector);
+        vm.expectRevert(); // failed to accrued interest for the msg.sender
         config.accrueInterestAndGetConfigOptimised(0, ISilo.CollateralType.Collateral);
+
+        // trying with mailicous silo
+        FakeSilo fakeSilo = new FakeSilo();
+
+         vm.expectRevert(ISiloConfig.WrongSilo.selector);
+         vm.prank(address(fakeSilo));
+         config.accrueInterestAndGetConfigOptimised(0, ISilo.CollateralType.Collateral);
     }
 
     function verifyReentrancy() external {
         ISiloConfig config = TestStateLib.siloConfig();
 
-        vm.expectRevert(ISiloConfig.CrossReentrantCall.selector); // TODO: update error after permissions bug fix
+        vm.expectRevert(ISiloConfig.CrossReentrantCall.selector);
         config.accrueInterestAndGetConfigOptimised(0, ISilo.CollateralType.Collateral);
     }
 
--- silo-core/test/foundry/Silo/reentrancy/registries/SiloConfigMethodsRegistry.sol
+++ silo-core/test/foundry/Silo/reentrancy/registries/SiloConfigMethodsRegistry.sol
@@ -30,7 +30,7 @@ contract SiloConfigMethodsRegistry is IMethodsRegistry {
     constructor() {
         _registerMethod(new SiloIDReentrancyTest());
         // _registerMethod(new AccrueInterestAndGetConfigReentrancyTest()); // TODO: bug with permissions
-        // _registerMethod(new AccrueInterestAndGetConfigOptimisedReentrancyTest()); // TODO: bug with permissions
+        _registerMethod(new AccrueInterestAndGetConfigOptimisedReentrancyTest());
         // _registerMethod(new AccrueInterestAndGetConfigsReentrancyTest()); // TODO: bug with permissions
         _registerMethod(new CloseDebtReentrancyTest());
         _registerMethod(new CrossNonReentrantAfterReentrancyTest());
