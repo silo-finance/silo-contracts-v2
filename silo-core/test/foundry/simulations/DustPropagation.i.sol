40a05c65d53a57c7c1e1150a6eb70b7d008a9ad8
IhorSF <ihor@silo.finance>
2024-07-17 16:05:38 +0300

    dust propagation setup

--- silo-core/test/foundry/simulations/DustPropagation.i.sol
+++ silo-core/test/foundry/simulations/DustPropagation.i.sol
@@ -219,7 +219,7 @@ contract DustPropagationTest is SiloLittleHelper, Test {
     }
 
     function _printState(string memory _title) private {
-        (ISiloConfig.ConfigData memory collateralConfig,) = siloConfig.getConfigs(BORROWER);
+        ISiloConfig.ConfigData memory collateralConfig = siloConfig.getConfig(address(silo0));
 
         emit log_named_string("================ ", _title);
 
