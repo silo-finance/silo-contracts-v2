3a3297067f5cfd1476aff5bead5a035c771ddfa3
Neo Racer <0xNeoRacer@protonmail.com>
2024-07-26 11:52:14 +0200

    debug

--- silo-core/contracts/lib/Actions.sol
+++ silo-core/contracts/lib/Actions.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.20;
 
+import {console} from "forge-std/console.sol";
+
 import {IERC20} from "openzeppelin5/token/ERC20/IERC20.sol";
 import {SafeERC20} from "openzeppelin5/token/ERC20/utils/SafeERC20.sol";
 
@@ -136,6 +138,9 @@ library Actions {
         ISiloConfig.ConfigData memory debtConfig;
 
         (collateralConfig, debtConfig) = siloConfig.getConfigsForBorrow({_debtSilo: address(this)});
+
+        console.log("collateralConfig.silo", collateralConfig.silo);
+        console.log("debtConfig.silo", debtConfig.silo);
 
         (assets, shares) = SiloLendingLib.borrow(
             debtConfig.debtShareToken,
