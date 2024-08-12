955b84439ded5d2b249deab03dbcdfadeec2d046
Neo Racer <0xNeoRacer@protonmail.com>
2024-08-12 11:04:57 +0200

    too deep

--- silo-core/test/foundry/Silo/max/maxLiquidation/MaxLiquidation_withChunks.i.sol
+++ silo-core/test/foundry/Silo/max/maxLiquidation/MaxLiquidation_withChunks.i.sol
@@ -20,14 +20,10 @@ contract MaxLiquidationWithChunksTest is MaxLiquidationTest {
         override
         returns (uint256 withdrawCollateral, uint256 repayDebtAssets)
     {
-        (
-            uint256 totalCollateralToLiquidate, uint256 totalDebtToCover,
-        ) = partialLiquidation.maxLiquidation(borrower);
-
+        (uint256 totalCollateralToLiquidate, uint256 totalDebtToCover,) = partialLiquidation.maxLiquidation(borrower);
 
         for (uint256 i; i < 5; i++) {
             emit log_named_uint("[MaxLiquidationWithChunks] case ------------------------", i);
-            bool isSolvent = silo0.isSolvent(borrower);
 
             emit log_named_string("isSolvent", silo0.isSolvent(borrower) ? "YES" : "NO");
             emit log_named_decimal_uint("[MaxLiquidationWithChunks] ltv before", silo0.getLtv(borrower), 16);
@@ -38,11 +34,15 @@ contract MaxLiquidationWithChunksTest is MaxLiquidationTest {
 
             assertTrue(!sTokenRequired, "sTokenRequired not required");
 
-            // this conditions caught bug
-            if (isSolvent && debtToCover != 0) revert("if we solvent there should be no liquidation");
-            if (!isSolvent && debtToCover == 0) revert("if we NOT solvent there should be a liquidation");
+            { // too deep
+                bool isSolvent = silo0.isSolvent(borrower);
 
-            if (isSolvent) break;
+                // this conditions caught bug
+                if (isSolvent && debtToCover != 0) revert("if we solvent there should be no liquidation");
+                if (!isSolvent && debtToCover == 0) revert("if we NOT solvent there should be a liquidation");
+
+                if (isSolvent) break;
+            }
 
             uint256 testDebtToCover = _calculateChunk(debtToCover, i);
 
