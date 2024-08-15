4f04b82f8d9b2d44dd97e6b9049a3fa732d5351f
Neo Racer <0xNeoRacer@protonmail.com>
2024-08-15 12:24:28 +0200

    ..

--- silo-core/contracts/lib/ERC20Lib.sol
+++ silo-core/contracts/lib/ERC20Lib.sol
@@ -3,16 +3,72 @@ pragma solidity ^0.8.20;
 
 import {IERC20Errors} from "openzeppelin5/interfaces/draft-IERC6093.sol";
 
-import {ISiloERC20} from "../interfaces/ISiloERC20.sol";
+/**
+ * @notice this is copy of ERC20Upgradeable. Changes:
+ * - events are commented out
+ * - `onlyInitializing` removed
+ * - `emitEvent` flag removed
+ *
+ * @dev Implementation of the {IERC20} interface.
+ *
+ * This implementation is agnostic to the way tokens are created. This means
+ * that a supply mechanism has to be added in a derived contract using {_mint}.
+ *
+ * TIP: For a detailed writeup see our guide
+ * https://forum.openzeppelin.com/t/how-to-implement-erc20-supply-mechanisms/226[How
+ * to implement supply mechanisms].
+ *
+ * The default value of {decimals} is 18. To change this, you should override
+ * this function so it returns a different value.
+ *
+ * We have followed general OpenZeppelin Contracts guidelines: functions revert
+ * instead returning `false` on failure. This behavior is nonetheless
+ * conventional and does not conflict with the expectations of ERC-20
+ * applications.
+ */
+library ERC20Upgradeable {
+    /// @custom:storage-location erc7201:openzeppelin.storage.ERC20
+    struct ERC20Storage {
+        mapping(address account => uint256) _balances;
 
-/// @dev this is copy of ERC20Upgradeable. Changes:
-/// - events are commented out
-/// - `ISiloERC20.ERC20Storage storage $` added as parameter
-library ERC20Lib {
+        mapping(address account => mapping(address spender => uint256)) _allowances;
+
+        uint256 _totalSupply;
+
+        string _name;
+        string _symbol;
+    }
+
+    // keccak256(abi.encode(uint256(keccak256("openzeppelin.storage.ERC20")) - 1)) & ~bytes32(uint256(0xff))
+    bytes32 private constant ERC20StorageLocation = 0x52c63247e1f47db19d5ce0460030c497f067ca4cebf71ba98eeadabe20bace00;
+
+    function _getERC20Storage() private pure returns (ERC20Storage storage $) {
+        assembly {
+            $.slot := ERC20StorageLocation
+        }
+    }
+
+    /**
+     * @dev Sets the values for {name} and {symbol}.
+     *
+     * All two of these values are immutable: they can only be set once during
+     * construction.
+     */
+    function __ERC20_init(string memory name_, string memory symbol_) internal {
+        __ERC20_init_unchained(name_, symbol_);
+    }
+
+    function __ERC20_init_unchained(string memory name_, string memory symbol_) internal {
+        ERC20Storage storage $ = _getERC20Storage();
+        $._name = name_;
+        $._symbol = symbol_;
+    }
+
     /**
      * @dev Returns the name of the token.
      */
-    function name(ISiloERC20.ERC20Storage storage $) external view returns (string memory) {
+    function name() public view returns (string memory) {
+        ERC20Storage storage $ = _getERC20Storage();
         return $._name;
     }
 
@@ -20,24 +76,44 @@ library ERC20Lib {
      * @dev Returns the symbol of the token, usually a shorter version of the
      * name.
      */
-    function symbol(ISiloERC20.ERC20Storage storage $) public view returns (string memory) {
+    function symbol() public view returns (string memory) {
+        ERC20Storage storage $ = _getERC20Storage();
         return $._symbol;
+    }
+
+    /**
+     * @dev Returns the number of decimals used to get its user representation.
+     * For example, if `decimals` equals `2`, a balance of `505` tokens should
+     * be displayed to a user as `5.05` (`505 / 10 ** 2`).
+     *
+     * Tokens usually opt for a value of 18, imitating the relationship between
+     * Ether and Wei. This is the default value returned by this function, unless
+     * it's overridden.
+     *
+     * NOTE: This information is only used for _display_ purposes: it in
+     * no way affects any of the arithmetic of the contract, including
+     * {IERC20-balanceOf} and {IERC20-transfer}.
+     */
+    function decimals() public view returns (uint8) {
+        return 18;
     }
 
     /**
      * @dev See {IERC20-totalSupply}.
      */
-    function totalSupply(ISiloERC20.ERC20Storage storage $) public view returns (uint256) {
+    function totalSupply() public view returns (uint256) {
+        ERC20Storage storage $ = _getERC20Storage();
         return $._totalSupply;
     }
 
     /**
      * @dev See {IERC20-balanceOf}.
      */
-    function balanceOf(ISiloERC20.ERC20Storage storage $, address account) public view returns (uint256) {
+    function balanceOf(address account) public view returns (uint256) {
+        ERC20Storage storage $ = _getERC20Storage();
         return $._balances[account];
     }
-    
+
     /**
      * @dev See {IERC20-transfer}.
      *
@@ -46,20 +122,17 @@ library ERC20Lib {
      * - `to` cannot be the zero address.
      * - the caller must have a balance of at least `value`.
      */
-    function transfer(ISiloERC20.ERC20Storage storage $, address to, uint256 value) public returns (bool) {
+    function transfer(address to, uint256 value) public returns (bool) {
         address owner = _msgSender();
-        _transfer($, owner, to, value);
+        _transfer(owner, to, value);
         return true;
     }
 
     /**
      * @dev See {IERC20-allowance}.
      */
-    function allowance(ISiloERC20.ERC20Storage storage $, address owner, address spender)
-        public
-        view
-        returns (uint256)
-    {
+    function allowance(address owner, address spender) public view returns (uint256) {
+        ERC20Storage storage $ = _getERC20Storage();
         return $._allowances[owner][spender];
     }
 
@@ -73,9 +146,9 @@ library ERC20Lib {
      *
      * - `spender` cannot be the zero address.
      */
-    function approve(ISiloERC20.ERC20Storage storage $, address spender, uint256 value) public returns (bool) {
+    function approve(address spender, uint256 value) public returns (bool) {
         address owner = _msgSender();
-        _approve($, owner, spender, value);
+        _approve(owner, spender, value);
         return true;
     }
 
@@ -95,13 +168,10 @@ library ERC20Lib {
      * - the caller must have allowance for ``from``'s tokens of at least
      * `value`.
      */
-    function transferFrom(ISiloERC20.ERC20Storage storage $, address from, address to, uint256 value)
-        public
-        returns (bool)
-    {
+    function transferFrom(address from, address to, uint256 value) public returns (bool) {
         address spender = _msgSender();
-        _spendAllowance($, from, spender, value);
-        _transfer($, from, to, value);
+        _spendAllowance(from, spender, value);
+        _transfer(from, to, value);
         return true;
     }
 
@@ -113,16 +183,16 @@ library ERC20Lib {
      *
      * Emits a {Transfer} event.
      *
-     * NOTE: This function is not virtual, {_update} should be overridden instead.
+     * NOTE: This function is not, {_update} should be overridden instead.
      */
-    function _transfer(ISiloERC20.ERC20Storage storage $, address from, address to, uint256 value) internal {
+    function _transfer(address from, address to, uint256 value) internal {
         if (from == address(0)) {
             revert IERC20Errors.ERC20InvalidSender(address(0));
         }
         if (to == address(0)) {
             revert IERC20Errors.ERC20InvalidReceiver(address(0));
         }
-        _update($, from, to, value);
+        _update(from, to, value);
     }
 
     /**
@@ -132,7 +202,8 @@ library ERC20Lib {
      *
      * Emits a {Transfer} event.
      */
-    function _update(ISiloERC20.ERC20Storage storage $, address from, address to, uint256 value) internal {
+    function _update(address from, address to, uint256 value) internal {
+        ERC20Storage storage $ = _getERC20Storage();
         if (from == address(0)) {
             // Overflow check required: The rest of the code assumes that totalSupply never overflows
             $._totalSupply += value;
@@ -154,7 +225,7 @@ library ERC20Lib {
             }
         } else {
             unchecked {
-                // Overflow not possible: balance + value is at most totalSupply, which we know fits into a uint256.
+            // Overflow not possible: balance + value is at most totalSupply, which we know fits into a uint256.
                 $._balances[to] += value;
             }
         }
@@ -168,13 +239,13 @@ library ERC20Lib {
      *
      * Emits a {Transfer} event with `from` set to the zero address.
      *
-     * NOTE: This function is not virtual, {_update} should be overridden instead.
+     * NOTE: This function is not, {_update} should be overridden instead.
      */
-    function _mint(ISiloERC20.ERC20Storage storage $, address account, uint256 value) internal {
+    function _mint(address account, uint256 value) internal {
         if (account == address(0)) {
             revert IERC20Errors.ERC20InvalidReceiver(address(0));
         }
-        _update($, address(0), account, value);
+        _update(address(0), account, value);
     }
 
     /**
@@ -183,13 +254,13 @@ library ERC20Lib {
      *
      * Emits a {Transfer} event with `to` set to the zero address.
      *
-     * NOTE: This function is not virtual, {_update} should be overridden instead
+     * NOTE: This function is not, {_update} should be overridden instead
      */
-    function _burn(ISiloERC20.ERC20Storage storage $, address account, uint256 value) internal {
+    function _burn(address account, uint256 value) internal {
         if (account == address(0)) {
             revert IERC20Errors.ERC20InvalidSender(address(0));
         }
-        _update($, account, address(0), value);
+        _update(account, address(0), value);
     }
 
     /**
@@ -203,14 +274,15 @@ library ERC20Lib {
      * true using the following override:
      *
      * ```solidity
-     * function _approve(address owner, address spender, uint256 value, bool) internal virtual override {
+     * function _approve(address owner, address spender, uint256 value, bool) internal override {
      *     super._approve(owner, spender, value, true);
      * }
      * ```
      *
      * Requirements are the same as {_approve}.
      */
-    function _approve(ISiloERC20.ERC20Storage storage $, address owner, address spender, uint256 value) internal {
+    function _approve(address owner, address spender, uint256 value) internal {
+        ERC20Storage storage $ = _getERC20Storage();
         if (owner == address(0)) {
             revert IERC20Errors.ERC20InvalidApprover(address(0));
         }
@@ -218,7 +290,9 @@ library ERC20Lib {
             revert IERC20Errors.ERC20InvalidSpender(address(0));
         }
         $._allowances[owner][spender] = value;
-        // emit Approval(owner, spender, value);
+        // if (emitEvent) {
+        //    emit Approval(owner, spender, value);
+        // }
     }
 
     /**
@@ -227,21 +301,20 @@ library ERC20Lib {
      * Does not update the allowance value in case of infinite allowance.
      * Revert if not enough allowance is available.
      *
+     * Does not emit an {Approval} event.
      */
-    function _spendAllowance(ISiloERC20.ERC20Storage storage $, address owner, address spender, uint256 value)
-        internal
-    {
-        uint256 currentAllowance = allowance($, owner, spender);
+    function _spendAllowance(address owner, address spender, uint256 value) internal {
+        uint256 currentAllowance = allowance(owner, spender);
         if (currentAllowance != type(uint256).max) {
             if (currentAllowance < value) {
                 revert IERC20Errors.ERC20InsufficientAllowance(spender, currentAllowance, value);
             }
             unchecked {
-                _approve($, owner, spender, currentAllowance - value);
+                _approve(owner, spender, currentAllowance - value);
             }
         }
     }
-    
+
     function _msgSender() internal view returns (address) {
         return msg.sender;
     }
