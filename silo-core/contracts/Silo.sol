0aa1d19b5254d0b90485659eb43faed90941ffd0
Neo Racer <0xNeoRacer@protonmail.com>
2024-08-21 15:03:19 +0200

    hookReceiver

--- silo-core/contracts/Silo.sol
+++ silo-core/contracts/Silo.sol
@@ -74,10 +74,13 @@ contract Silo is SiloERC4626 {
         }
     }
 
+    function hookReceiver() external view returns (address) {
+        return ShareTokenLib.getShareTokenStorage().hookSetup.hookReceiver;
+    }
+
     /// @inheritdoc ISilo
     function initialize(ISiloConfig _siloConfig, address _modelConfigAddress) external virtual {
         address hookReceiver = Actions.initialize(_siloConfig, _modelConfigAddress);
-
     }
 
     /// @inheritdoc ISilo
