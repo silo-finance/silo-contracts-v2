e194c96669bdc25c80bc39de2c5d45246d9c5d8b
Neo Racer <0xNeoRacer@protonmail.com>
2024-09-09 12:02:04 +0200

    ..

--- silo-core/test/foundry/lib/SiloMathLib/GetDebtAmountsWithInterest.t.sol
+++ silo-core/test/foundry/lib/SiloMathLib/GetDebtAmountsWithInterest.t.sol
@@ -7,10 +7,35 @@ import "silo-core/contracts/lib/SiloMathLib.sol";
 // forge test -vv --mc GetDebtAmountsWithInterestTest
 contract GetDebtAmountsWithInterestTest is Test {
     /*
-    forge test -vv --mt test_getDebtAmountsWithInterest
+    forge test -vv --mt test_getDebtAmountsWithInterest_pass
     */
-    function test_getDebtAmountsWithInterest() public pure {
+    function test_getDebtAmountsWithInterest_pass() public pure {
         uint256 debtAssets;
+        uint256 rcompInDp;
+
+        (uint256 debtAssetsWithInterest, uint256 accruedInterest) =
+            SiloMathLib.getDebtAmountsWithInterest(debtAssets, rcompInDp);
+
+        assertEq(debtAssetsWithInterest, 0);
+        assertEq(accruedInterest, 0);
+
+        rcompInDp = 0.1e18;
+
+        (debtAssetsWithInterest, accruedInterest) = SiloMathLib.getDebtAmountsWithInterest(debtAssets, rcompInDp);
+
+        assertEq(debtAssetsWithInterest, 0, "debtAssetsWithInterest, just rcomp");
+        assertEq(accruedInterest, 0, "accruedInterest, just rcomp");
+
+        debtAssets = 1e18;
+
+        (debtAssetsWithInterest, accruedInterest) = SiloMathLib.getDebtAmountsWithInterest(debtAssets, rcompInDp);
+
+        assertEq(debtAssetsWithInterest, 1.1e18, "debtAssetsWithInterest - no debt, no interest");
+        assertEq(accruedInterest, 0.1e18, "accruedInterest - no debt, no interest");
+    }
+
+    function test_getDebtAmountsWithInterest_overflow_onInterest() public pure {
+        uint256 debtAssets = type(uint256).max ;
         uint256 rcompInDp;
 
         (uint256 debtAssetsWithInterest, uint256 accruedInterest) =
