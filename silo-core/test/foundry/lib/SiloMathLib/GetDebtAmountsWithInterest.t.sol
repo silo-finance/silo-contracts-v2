5c9bdfa3fb3b062ae4f1b3b919822d05fe3acad5
Neo Racer <0xNeoRacer@protonmail.com>
2024-09-09 13:09:12 +0200

    qa

--- silo-core/test/foundry/lib/SiloMathLib/GetDebtAmountsWithInterest.t.sol
+++ silo-core/test/foundry/lib/SiloMathLib/GetDebtAmountsWithInterest.t.sol
@@ -34,28 +34,68 @@ contract GetDebtAmountsWithInterestTest is Test {
         assertEq(accruedInterest, 0.1e18, "accruedInterest - no debt, no interest");
     }
 
-    function test_getDebtAmountsWithInterest_overflow_onInterest() public pure {
-        uint256 debtAssets = type(uint256).max ;
-        uint256 rcompInDp;
+    /*
+    forge test -vv --mt test_getDebtAmountsWithInterest_overflow_max
+    */
+    function test_getDebtAmountsWithInterest_overflow_max() public pure {
+        uint256 debtAssets = type(uint256).max;
+        // this should be impossible because of IRM cap, but for QA we have to support it
+        uint256 rcompInDp = type(uint256).max;
 
-        (uint256 debtAssetsWithInterest, uint256 accruedInterest) =
-            SiloMathLib.getDebtAmountsWithInterest(debtAssets, rcompInDp);
+        (
+            uint256 debtAssetsWithInterest, uint256 accruedInterest
+        ) = SiloMathLib.getDebtAmountsWithInterest(debtAssets, rcompInDp);
 
-        assertEq(debtAssetsWithInterest, 0);
-        assertEq(accruedInterest, 0);
+        assertEq(debtAssetsWithInterest, type(uint256).max, "debtAssetsWithInterest - max");
+        assertEq(accruedInterest, 0, "accruedInterest - overflow cap");
+    }
 
-        rcompInDp = 0.1e18;
+    /*
+    forge test -vv --mt test_getDebtAmountsWithInterest_overflow_interest
+    */
+    function test_getDebtAmountsWithInterest_overflow_interest() public pure {
+        uint256 debtAssets = type(uint256).max - 1e18;
+        // this should be impossible because of IRM cap, but for QA we have to support it
+        uint256 rcompInDp = 1e18; // 100 %
 
-        (debtAssetsWithInterest, accruedInterest) = SiloMathLib.getDebtAmountsWithInterest(debtAssets, rcompInDp);
+        (
+            uint256 debtAssetsWithInterest, uint256 accruedInterest
+        ) = SiloMathLib.getDebtAmountsWithInterest(debtAssets, rcompInDp);
 
-        assertEq(debtAssetsWithInterest, 0, "debtAssetsWithInterest, just rcomp");
-        assertEq(accruedInterest, 0, "accruedInterest, just rcomp");
+        assertEq(debtAssetsWithInterest, type(uint256).max, "debtAssetsWithInterest - max");
+        assertEq(accruedInterest, 1e18, "accruedInterest - overflow cap");
+    }
 
-        debtAssets = 1e18;
+    /*
+    forge test -vv --mt test_getDebtAmountsWithInterest_overflow_one
+    */
+    function test_getDebtAmountsWithInterest_overflow_one() public pure {
+        uint256 debtAssets = type(uint256).max / 2 + 1;
+        // this should be impossible because of IRM cap, but for QA we have to support it
+        uint256 rcompInDp = 1e18; // 100 %
 
-        (debtAssetsWithInterest, accruedInterest) = SiloMathLib.getDebtAmountsWithInterest(debtAssets, rcompInDp);
+        (
+            uint256 debtAssetsWithInterest, uint256 accruedInterest
+        ) = SiloMathLib.getDebtAmountsWithInterest(debtAssets, rcompInDp);
 
-        assertEq(debtAssetsWithInterest, 1.1e18, "debtAssetsWithInterest - no debt, no interest");
-        assertEq(accruedInterest, 0.1e18, "accruedInterest - no debt, no interest");
+        assertEq(debtAssetsWithInterest, type(uint256).max, "debtAssetsWithInterest - max");
+        assertEq(accruedInterest, type(uint256).max / 2, "accruedInterest - overflow cap");
+    }
+
+    /*
+    forge test -vv --mt test_getDebtAmountsWithInterest_below_overflow
+    */
+    function test_getDebtAmountsWithInterest_below_overflow() public pure {
+        uint256 debtAssets = type(uint256).max / 2;
+        // this should be impossible because of IRM cap, but for QA we have to support it
+        uint256 rcompInDp = 1e18; // 100 %
+
+        (
+            uint256 debtAssetsWithInterest, uint256 accruedInterest
+        ) = SiloMathLib.getDebtAmountsWithInterest(debtAssets, rcompInDp);
+
+        assertEq(debtAssetsWithInterest, type(uint256).max - 1, "debtAssetsWithInterest - max");
+        assertEq(accruedInterest, type(uint256).max / 2, "accruedInterest - overflow cap");
+    }
     }
 }
