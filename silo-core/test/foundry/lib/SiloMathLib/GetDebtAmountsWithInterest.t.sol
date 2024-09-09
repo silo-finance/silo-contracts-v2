a413fa0a36a0d061e9dd1ddee3f6c21b12e995f8
Neo Racer <0xNeoRacer@protonmail.com>
2024-09-09 13:20:35 +0200

    qa

--- silo-core/test/foundry/lib/SiloMathLib/GetDebtAmountsWithInterest.t.sol
+++ silo-core/test/foundry/lib/SiloMathLib/GetDebtAmountsWithInterest.t.sol
@@ -1,11 +1,15 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.20;
 
-import "forge-std/Test.sol";
-import "silo-core/contracts/lib/SiloMathLib.sol";
+import {Test} from "forge-std/Test.sol";
+import {Math, SiloMathLib, Rounding} from "silo-core/contracts/lib/SiloMathLib.sol";
 
 // forge test -vv --mc GetDebtAmountsWithInterestTest
 contract GetDebtAmountsWithInterestTest is Test {
+    using Math for uint256;
+
+    uint256 internal constant _PRECISION_DECIMALS = 1e18;
+
     /*
     forge test -vv --mt test_getDebtAmountsWithInterest_pass
     */
@@ -32,6 +36,14 @@ contract GetDebtAmountsWithInterestTest is Test {
 
         assertEq(debtAssetsWithInterest, 1.1e18, "debtAssetsWithInterest - no debt, no interest");
         assertEq(accruedInterest, 0.1e18, "accruedInterest - no debt, no interest");
+    }
+
+    /*
+    forge test -vv --mt test_getDebtAmountsWithInterest_notRevert
+    */
+    /// forge-config: core-test.fuzz.runs = 1000
+    function test_getDebtAmountsWithInterest_notRevert_fuzz(uint256 _debtAssets, uint256 _rcompInDp) public pure {
+        SiloMathLib.getDebtAmountsWithInterest(_debtAssets, _rcompInDp);
     }
 
     /*
@@ -97,5 +109,24 @@ contract GetDebtAmountsWithInterestTest is Test {
         assertEq(debtAssetsWithInterest, type(uint256).max - 1, "debtAssetsWithInterest - max");
         assertEq(accruedInterest, type(uint256).max / 2, "accruedInterest - overflow cap");
     }
+
+    /*
+    forge test -vv --mt test_getDebtAmountsWithInterest_cap
+    */
+    function test_getDebtAmountsWithInterest_cap() public pure {
+        uint256 debtAssets = 1e18;
+        // this should be impossible because of IRM cap, but for QA we have to support it
+        uint256 rcompInDp = type(uint256).max - 1;
+
+        uint256 interest = debtAssets.mulDiv(rcompInDp, _PRECISION_DECIMALS, Rounding.ACCRUED_INTEREST);
+        assertLt(interest, type(uint256).max, "this is just to ensure, we testing cap, not overflow on interest");
+
+        (
+            uint256 debtAssetsWithInterest, uint256 accruedInterest
+        ) = SiloMathLib.getDebtAmountsWithInterest(debtAssets, rcompInDp);
+
+        assertEq(debtAssetsWithInterest, type(uint256).max, "debtAssetsWithInterest - max");
+        assertEq(accruedInterest, type(uint256).max - 1e18, "accruedInterest");
+        assertLt(accruedInterest, interest, "accruedInterest cap");
     }
 }
