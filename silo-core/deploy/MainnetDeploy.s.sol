54dffd735eb2149cd608a583d6a586ea3654951f
IhorSF <ihor@silo.finance>
2024-09-13 12:53:55 +0400

    mainnet deploy with lens and router

--- silo-core/deploy/MainnetDeploy.s.sol
+++ silo-core/deploy/MainnetDeploy.s.sol
@@ -9,6 +9,8 @@ import {InterestRateModelV2Deploy} from "./InterestRateModelV2Deploy.s.sol";
 import {PartialLiquidationDeploy} from "./PartialLiquidationDeploy.s.sol";
 import {GaugeHookReceiverDeploy} from "./GaugeHookReceiverDeploy.s.sol";
 import {SiloDeployerDeploy} from "./SiloDeployerDeploy.s.sol";
+import {SiloLensDeploy} from "./SiloLensDeploy.s.sol";
+import {SiloRouterDeploy} from "./SiloRouterDeploy.s.sol";
 
 /**
     FOUNDRY_PROFILE=core \
@@ -24,6 +26,8 @@ contract MainnetDeploy is CommonDeploy {
         PartialLiquidationDeploy siloLiquidationDeploy = new PartialLiquidationDeploy();
         GaugeHookReceiverDeploy gaugeHookReceiverDeploy = new GaugeHookReceiverDeploy();
         SiloDeployerDeploy siloDeployerDeploy = new SiloDeployerDeploy();
+        SiloLensDeploy siloLensDeploy = new SiloLensDeploy();
+        SiloRouterDeploy siloRouterDeploy = new SiloRouterDeploy();
 
         siloFactoryDeploy.run();
         interestRateModelV2ConfigFactoryDeploy.run();
@@ -31,5 +35,7 @@ contract MainnetDeploy is CommonDeploy {
         siloLiquidationDeploy.run();
         gaugeHookReceiverDeploy.run();
         siloDeployerDeploy.run();
+        siloLensDeploy.run();
+        siloRouterDeploy.run();
     }
 }
