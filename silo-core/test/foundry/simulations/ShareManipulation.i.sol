7fd7be7be5863d4616a1b9b1e1f4d3d0392eeb4c
Neo Racer <0xNeoRacer@protonmail.com>
2024-09-18 12:32:24 +0200

    debt example

--- silo-core/test/foundry/simulations/ShareManipulation.i.sol
+++ silo-core/test/foundry/simulations/ShareManipulation.i.sol
@@ -103,9 +103,93 @@ contract ShareManipulationTest is SiloLittleHelper, Test {
         emit log_named_decimal_uint("maxWithdraw(depositor)", silo1.maxWithdraw(depositor), 18);
         emit log_named_decimal_uint("maxWithdraw(depositor) diff", silo1.maxWithdraw(depositor) - withdrawBefore, 18);
     }
-    
+
+    /*
+    forge test -vv --ffi --mt test_debt_ratio --gas-limit 40000000000
+
+
+    */
+    function test_debt_ratio() public {
+        address borrower = makeAddr("borrower");
+        address depositor = makeAddr("depositor");
+        address borrower2 = makeAddr("borrower2");
+        address borrower3 = makeAddr("borrower3");
+
+        _depositForBorrow(10e18, depositor);
+
+        _depositCollateral(999999e18, borrower, TWO_ASSETS);
+        _depositCollateral(1e18, borrower2, TWO_ASSETS);
+        _depositCollateral(1e18, borrower3, TWO_ASSETS);
+        _borrow(0.75e18, borrower2);
+        _borrow(0.75e18, borrower3);
+
+        uint256 precision = 1e18;
+        // changing offset 1 -> 10 does not change much, it's basically higher precision, but numbers are the same.
+        uint256 offset = 10 ** 0;
+
+        // the higher number you check the faster we get result
+        while(silo1.previewBorrowShares(precision * offset) == precision) {
+            vm.warp(block.timestamp + 1);
+        }
+
+        uint256 ratioBefore = silo1.previewBorrowShares(precision * offset);
+
+        emit log_named_uint("DIFF", precision);
+        emit log_named_uint("DIFF", ratioBefore);
+        emit log_named_decimal_uint("repay:", silo1.maxRepay(borrower), 18);
+        emit log_named_uint("time:", block.timestamp);
+
+        _printBorrowRatio();
+
+//        uint256 moneySpend;
+        uint256 _initial = 1e10; // changing initial amount does not affect ratio
+
+        uint256 withdrawBefore = silo1.maxWithdraw(depositor);
+        uint256 repayBefore = silo1.maxRepay(borrower);
+
+        emit log_named_decimal_uint("maxWithdraw(depositor)", withdrawBefore, 18);
+        emit log_named_decimal_uint("maxRepay(borrower)", repayBefore, 18);
+        emit log_named_decimal_uint("maxRepay(borrower2)", silo1.maxRepay(borrower2), 18);
+        emit log_named_decimal_uint("maxRepay(borrower3)", silo1.maxRepay(borrower3), 18);
+
+        // _repay(silo1.maxRepay(borrower), borrower);
+
+        for(uint256 i; i < 15; i++) {
+            // vm.warp(block.timestamp + 1);
+
+            // emit log_named_uint("#i", i);
+            // emit log_named_decimal_uint("_initial", _initial, 18);
+
+            uint256 borrowAmount = (_initial % 10_000e18);
+
+            _borrow(borrowAmount, borrower);
+            _repay(silo1.maxRepay(borrower) - 1, borrower);
+
+            _initial = silo1.getDebtAssets();
+        }
+
+        emit log_named_uint("ratioBefore", ratioBefore);
+        emit log_named_uint("ratioNow   ", silo1.previewBorrowShares(precision * offset));
+
+        uint256 ratioDiff = ratioBefore - silo1.previewBorrowShares(precision * offset);
+        emit log_named_decimal_uint("ratio DECREASED?? by", ratioDiff, 18);
+//        emit log_named_decimal_uint("moneySpend", moneySpend, 18);
+
+        emit log_named_decimal_uint("maxRepay(borrower)", repayBefore, 18);
+        emit log_named_decimal_uint("maxRepay(borrower)", silo1.maxRepay(borrower), 18);
+        emit log_named_decimal_uint("maxRepay(borrower2)", silo1.maxRepay(borrower2), 18);
+        emit log_named_decimal_uint("maxRepay(borrower3)", silo1.maxRepay(borrower3), 18);
+
+        _repay(silo1.maxRepay(borrower), borrower);
+        _printBorrowRatio();
+
+        emit log_named_decimal_uint("maxWithdraw(depositor)", silo1.maxWithdraw(depositor), 18);
+        emit log_named_decimal_uint("maxWithdraw(depositor) diff", silo1.maxWithdraw(depositor) - withdrawBefore, 18);
+    }
+
     function _printBorrowRatio() internal {
-        emit log_named_uint("1 share =", silo1.previewBorrowShares(1));
+        emit log_named_uint("[silo1] 1e18 share =", silo1.previewBorrowShares(1e18));
+        emit log_named_uint("[silo1] 1 share =", silo1.previewBorrowShares(1));
     }
 
     function _printCollateralRatio(uint256 _offset) internal {
