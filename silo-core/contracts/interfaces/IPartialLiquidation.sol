0c2ac789aa8493cde2901c27eab67f4f5c6cf2f4
Thomas A. Anderson <92584446+0xNeoRacer@users.noreply.github.com>
2024-10-15 12:52:15 +0200

    Update silo-core/contracts/interfaces/IPartialLiquidation.sol
    Co-authored-by: shadowyedd <89228160+shadowyedd@users.noreply.github.com>

--- silo-core/contracts/interfaces/IPartialLiquidation.sol
+++ silo-core/contracts/interfaces/IPartialLiquidation.sol
@@ -48,7 +48,7 @@ interface IPartialLiquidation {
     /// - `_collateralAsset` or `_debtAsset` does not match the position
     /// - `_user` is solvent and there is no debt to cover
     /// - `_maxDebtToCover` is set to cover only part of the debt but full liquidation is required
-    /// - when not enough liquidity to transfer `_user` collateral to liquidator (use `_receiveSToken` in that case)
+    /// - when not enough liquidity to transfer `_user` collateral to liquidator (use `_receiveSToken == true` in that case)
     /// @param _collateralAsset The address of the underlying asset used as collateral, to receive as result
     /// @param _debtAsset The address of the underlying borrowed asset to be repaid with the liquidation
     /// @param _user The address of the borrower getting liquidated
