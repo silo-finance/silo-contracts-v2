7579012c8a694272827d99c90e8fa321ebce5cb6
Neo Racer <0xNeoRacer@protonmail.com>
2024-10-27 13:54:28 +0100

    .

--- silo-core/test/foundry/SiloConfig/SiloConfig.t.sol
+++ silo-core/test/foundry/SiloConfig/SiloConfig.t.sol
@@ -72,6 +72,8 @@ contract SiloConfigTest is Test {
         vm.assume(_configData0.silo != _configData1.silo);
         vm.assume(_configData0.daoFee < 0.5e18);
         vm.assume(_configData0.deployerFee < 0.5e18);
+        vm.assume(_configData0.liquidationTargetLtv < _configData0.lt);
+        vm.assume(_configData1.liquidationTargetLtv < _configData1.lt);
 
         // when using assume, it reject too many inputs
         _configData0.hookReceiver = _configData1.hookReceiver;
@@ -93,6 +95,7 @@ contract SiloConfigTest is Test {
 
         _configData0.daoFee = 1e18;
         _configData0.deployerFee = 0;
+        _configData0.lt = 1;
 
         vm.expectRevert(ISiloConfig.FeeTooHigh.selector);
         new SiloConfig(1, _configData0, _configData1);
@@ -179,6 +182,9 @@ contract SiloConfigTest is Test {
         ISiloConfig.ConfigData memory _configData0,
         ISiloConfig.ConfigData memory _configData1
     ) public {
+        vm.assume(_configData0.liquidationTargetLtv < _configData0.lt);
+        vm.assume(_configData1.liquidationTargetLtv < _configData1.lt);
+
         SiloConfig siloConfig = siloConfigDeploy(_siloId, _configData0, _configData1);
 
         vm.expectRevert(ISiloConfig.WrongSilo.selector);
