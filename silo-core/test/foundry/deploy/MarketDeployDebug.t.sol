77774725ea844596cf368952dc1b8c02edb264f7
IhorSF <ihor@silo.finance>
2024-10-30 17:04:19 +0400

    market deploy debug

---
+++ silo-core/test/foundry/deploy/MarketDeployDebug.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.28;
+
+import {IntegrationTest} from "silo-foundry-utils/networks/IntegrationTest.sol";
+
+import {SiloDeployWithGaugeHookReceiver} from "silo-core/deploy/silo/SiloDeployWithGaugeHookReceiver.s.sol";
+
+// FOUNDRY_PROFILE=core-test forge test -vv --ffi --mc MarketDeployDebugTest
+contract MarketDeployDebugTest is IntegrationTest {
+    SiloDeployWithGaugeHookReceiver siloDeploy;
+
+    function setUp() external {
+        vm.createSelectFork(
+            getChainRpcUrl(ARBITRUM_ONE_ALIAS), // change if needed
+            269194700
+        );
+
+        siloDeploy = new SiloDeployWithGaugeHookReceiver();
+    }
+
+    // FOUNDRY_PROFILE=core-test forge test -vvv --ffi --mt test_it
+    function test_it() public {
+        siloDeploy.useConfig(""); // set market config name
+        siloDeploy.run();
+    }
+}
