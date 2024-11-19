8433e28ae0ec5127504e9dc46a893a7225ef261f
Neo Racer <0xNeoRacer@protonmail.com>
2024-11-19 17:21:12 +0100

    del

--- silo-core/test/foundry/_mocks/DexSwapMock.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.28;
-
-contract DexSwapMock {
-    fallback(bytes calldata) external returns (bytes memory) {
-        // do not revert
-        return "";
-    }
-}
