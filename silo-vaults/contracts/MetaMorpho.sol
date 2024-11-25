61b4b931b5a306a29fc6530df7c6d9824515b81f
Neo Racer <0xNeoRacer@protonmail.com>
2024-11-25 17:25:52 +0100

    remove  // TODO should we use forceApprove? and should we use max?

--- silo-vaults/contracts/MetaMorpho.sol
+++ silo-vaults/contracts/MetaMorpho.sol
@@ -737,7 +737,6 @@ contract MetaMorpho is ERC4626, ERC20Permit, Ownable2Step, Multicall, IMetaMorph
         }
 
         marketConfig.cap = supplyCap;
-        // TODO should we use forceApprove? and should we use max?
         IERC20(asset()).approve(address(market), type(uint256).max);
         emit EventsLib.SetCap(_msgSender(), market, supplyCap);
 
