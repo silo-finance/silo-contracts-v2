81b974270a6b6c75cc09e1827e8f50c1882b2707
Neo Racer <0xNeoRacer@protonmail.com>
2024-11-27 09:01:34 +0100

    wip

--- silo-core/test/foundry/Silo/reentrancy/methods/silo/WithdrawFeesReentrancyTest.sol
+++ silo-core/test/foundry/Silo/reentrancy/methods/silo/WithdrawFeesReentrancyTest.sol
@@ -2,43 +2,25 @@
 pragma solidity ^0.8.28;
 
 import {ISilo} from "silo-core/contracts/interfaces/ISilo.sol";
+import {ICrossReentrancyGuard} from "silo-core/contracts/interfaces/ICrossReentrancyGuard.sol";
 import {MethodReentrancyTest} from "../MethodReentrancyTest.sol";
 import {TestStateLib} from "../../TestState.sol";
 
 contract WithdrawFeesReentrancyTest is MethodReentrancyTest {
     function callMethod() external {
         emit log_string("\tRevert as expected if no fees");
-        _revertAsExpectedIfNoFees();
+        TestStateLib.silo0().withdrawFees();
     }
 
     function verifyReentrancy() external {
-        _revertAsExpectedIfNoFees();
+        vm.expectRevert(ICrossReentrancyGuard.CrossReentrantCall.selector);
+        TestStateLib.silo0().withdrawFees();
+
+        vm.expectRevert(ICrossReentrancyGuard.CrossReentrantCall.selector);
+        TestStateLib.silo1().withdrawFees();
     }
 
     function methodDescription() external pure returns (string memory description) {
         description = "withdrawFees()";
-    }
-
-    function _revertAsExpectedIfNoFees() internal {
-        ISilo silo0 = TestStateLib.silo0();
-        ISilo silo1 = TestStateLib.silo1();
-
-        uint192 daoAndDeployerRevenue;
-
-        (daoAndDeployerRevenue,,,,) = silo0.getSiloStorage();
-
-        if (daoAndDeployerRevenue == 0) {
-            vm.expectRevert(ISilo.EarnedZero.selector);
-        }
-
-        silo0.withdrawFees();
-
-        (daoAndDeployerRevenue,,,,) = silo1.getSiloStorage();
-
-        if (daoAndDeployerRevenue == 0) {
-            vm.expectRevert(ISilo.EarnedZero.selector);
-        }
-
-        silo1.withdrawFees();
     }
 }
