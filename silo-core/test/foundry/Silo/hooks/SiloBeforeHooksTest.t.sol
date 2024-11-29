973f40aebac6f97d75a0a782b6cdeb0d1145ace0
Neo Racer <0xNeoRacer@protonmail.com>
2024-11-29 16:02:56 +0100

    .

--- silo-core/test/foundry/Silo/hooks/SiloBeforeHooksTest.t.sol
+++ silo-core/test/foundry/Silo/hooks/SiloBeforeHooksTest.t.sol
@@ -2,24 +2,12 @@
 pragma solidity ^0.8.28;
 
 import {Test} from "forge-std/Test.sol";
-import {IERC20} from "openzeppelin5/token/ERC20/IERC20.sol";
-import {AddrLib} from "silo-foundry-utils/lib/AddrLib.sol";
-
-import {VeSiloContracts} from "ve-silo/common/VeSiloContracts.sol";
-
-import {HookReceiverMock} from "silo-core/test/foundry/_mocks/HookReceiverMock.sol";
-import {SiloConfigsNames} from "silo-core/deploy/silo/SiloDeployments.sol";
 
 import {IHookReceiver} from "silo-core/contracts/interfaces/IHookReceiver.sol";
 import {ISiloConfig} from "silo-core/contracts/interfaces/ISiloConfig.sol";
-import {ISiloDeployer} from "silo-core/contracts/interfaces/ISiloDeployer.sol";
-import {IShareToken} from "silo-core/contracts/interfaces/IShareToken.sol";
 import {ISilo} from "silo-core/contracts/interfaces/ISilo.sol";
 import {Hook} from "silo-core/contracts/lib/Hook.sol";
-import {ContractThatAcceptsETH} from "silo-core/test/foundry/_mocks/ContractThatAcceptsETH.sol";
-import {SiloStorageExtension} from "silo-core/test/foundry/_mocks/SiloStorageExtension.sol";
 import {SiloFixtureWithVeSilo as SiloFixture} from "../../_common/fixtures/SiloFixtureWithVeSilo.sol";
-import {SiloConfigOverride} from "../../_common/fixtures/SiloFixture.sol";
 import {SiloLittleHelper} from "../../_common/SiloLittleHelper.sol";
 import {MintableToken} from "../../_common/MintableToken.sol";
 
@@ -66,7 +54,7 @@ contract HookReceiver is IHookReceiver, Test {
             // we want to use higher collateral, to create debt, and then when we back from hook,
             // we want to try to switch
             vm.prank(receiver);
-            ISilo(silo1).borrow(1, receiver, receiver);
+            ISilo(silo1).borrow(10, receiver, receiver);
         } else {
             revert("should not happen");
         }
@@ -89,9 +77,9 @@ contract HookReceiver is IHookReceiver, Test {
 }
 
 /*
-FOUNDRY_PROFILE=core-test forge test -vvv --ffi --mc SiloHooksBorrow2AssetsTest
+FOUNDRY_PROFILE=core-test forge test -vvv --ffi --mc SiloBeforeHooksTest
 */
-contract SiloHooksBorrow2AssetsTest is SiloLittleHelper, Test {
+contract SiloBeforeHooksTest is SiloLittleHelper, Test {
     address immutable BORROWER;
     address immutable DEPOSITOR;
 
@@ -171,16 +159,16 @@ contract SiloHooksBorrow2AssetsTest is SiloLittleHelper, Test {
     */
     function test_switchCollateralToThisSilo_debt() public {
         _hookReceiver.setBefore(uint24(Hook.SWITCH_COLLATERAL));
-        silo0.updateHooks();
+        silo1.updateHooks();
 
-        vm.expectRevert(ISilo.BorrowNotPossible.selector);
+        vm.expectRevert(ISilo.NotSolvent.selector);
         vm.prank(BORROWER);
-        silo0.borrowSameAsset(0.5e18, BORROWER, BORROWER);
+        silo1.switchCollateralToThisSilo();
 
         _hookReceiver.setBefore(uint24(0));
-        silo0.updateHooks();
+        silo1.updateHooks();
 
         vm.prank(BORROWER);
-        silo0.borrowSameAsset(0.5e18, BORROWER, BORROWER);
+        silo1.switchCollateralToThisSilo();
     }
 }
