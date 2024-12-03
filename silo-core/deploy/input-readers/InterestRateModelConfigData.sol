01e5c440102cbd067d298aaf335d7ac784b24a27
Neo Racer <0xNeoRacer@protonmail.com>
2024-12-03 17:20:33 +0100

    onlt tcrit

--- silo-core/deploy/input-readers/InterestRateModelConfigData.sol
+++ silo-core/deploy/input-readers/InterestRateModelConfigData.sol
@@ -12,6 +12,7 @@ contract InterestRateModelConfigData {
 
     // must be in alphabetic order
     struct ModelConfig {
+        int256 Tcrit;
         int256 beta;
         int256 kcrit;
         int256 ki;
@@ -44,6 +45,8 @@ contract InterestRateModelConfigData {
         ConfigData[] memory configs = _readDataFromJson();
 
         for (uint256 index = 0; index < configs.length; index++) {
+            revert("dddd");
+            console2.log("iteration", index);
             if (keccak256(bytes(configs[index].name)) == keccak256(bytes(_name))) {
                 modelConfig.beta = configs[index].config.beta;
                 modelConfig.ki = configs[index].config.ki;
@@ -53,6 +56,9 @@ contract InterestRateModelConfigData {
                 modelConfig.ucrit = configs[index].config.ucrit;
                 modelConfig.ulow = configs[index].config.ulow;
                 modelConfig.uopt = configs[index].config.uopt;
+                modelConfig.Tcrit = int128(configs[index].config.Tcrit);
+
+                print(modelConfig);
 
                 return modelConfig;
             }
@@ -70,5 +76,7 @@ contract InterestRateModelConfigData {
         console2.log("ucrit", _configData.ucrit);
         console2.log("ulow", _configData.ulow);
         console2.log("uopt", _configData.uopt);
+
+        console2.log("Tcrit", _configData.Tcrit);
     }
 }
