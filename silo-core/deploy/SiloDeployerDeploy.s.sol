fa99abafc254b7a1c5d14ef4c269d8f33c4b0763
Neo Racer <0xNeoRacer@protonmail.com>
2024-12-10 13:32:21 +0100

    .

--- silo-core/deploy/SiloDeployerDeploy.s.sol
+++ silo-core/deploy/SiloDeployerDeploy.s.sol
@@ -15,6 +15,38 @@ import {ShareDebtToken} from "silo-core/contracts/utils/ShareDebtToken.sol";
     FOUNDRY_PROFILE=core \
         forge script silo-core/deploy/SiloDeployerDeploy.s.sol \
         --ffi --broadcast --rpc-url http://127.0.0.1:8545
+
+    Lib verification:
+
+    ETHERSCAN_API_KEY=$ARBISCAN_API_KEY FOUNDRY_PROFILE=core forge verify-contract \
+    0xe2eB6FD7743521d834650699d1419B247d3B34ef Actions \
+    --chain 42161 --watch --compiler-version v0.8.28+commit.7893614a
+
+    ETHERSCAN_API_KEY=$ARBISCAN_API_KEY FOUNDRY_PROFILE=core forge verify-contract \
+    0x6De1C2dfc9061ABF37ee709Fe2776c74f09715c5 ShareCollateralTokenLib \
+    --chain 42161 --watch --compiler-version v0.8.28+commit.7893614a
+
+    ETHERSCAN_API_KEY=$ARBISCAN_API_KEY FOUNDRY_PROFILE=core forge verify-contract \
+    0x2f332bBF37E0F42Ef01cDB2327cFd21BE2f5f41F ShareTokenLib \
+    --chain 42161 --watch --compiler-version v0.8.28+commit.7893614a
+
+    ETHERSCAN_API_KEY=$ARBISCAN_API_KEY FOUNDRY_PROFILE=core forge verify-contract \
+    0x44Fa9F351fC12C47f1c7935E7d29A9B8dabAfe24 SiloLendingLib \
+    --chain 42161 --watch --compiler-version v0.8.28+commit.7893614a
+
+    ETHERSCAN_API_KEY=$ARBISCAN_API_KEY FOUNDRY_PROFILE=core forge verify-contract \
+    0x08D5095e6e8B13D43C7A0C172373ae24e3B66895 Views \
+    --chain 42161 --watch --compiler-version v0.8.28+commit.7893614a
+
+
+
+    cast abi-encode "constructor(address,uint256,address,string,string)" \
+    0xB85420016C1Df4e6Ad6e461Cf927913B5E04A430 86400 0x82aF49447D8a07e3bd95BD0d56f35241523fBab1 "Test Vault1" "TV1"
+
+    ETHERSCAN_API_KEY=$ARBISCAN_API_KEY FOUNDRY_PROFILE=vaults forge verify-contract \
+    0xdA72ab48AD4389B427b44d0dad393D5E5b209514 silo-vaults/contracts/MetaMorpho.sol:MetaMorpho \
+    --chain 42161 --watch --compiler-version v0.8.28+commit.7893614a \
+    --constructor-args <cast abi-encode output>
  */
 contract SiloDeployerDeploy is CommonDeploy {
     function run() public returns (ISiloDeployer siloDeployer) {
