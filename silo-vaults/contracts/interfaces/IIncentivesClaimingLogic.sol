8103dbf869f102b33f3eb151daa1a111345d433d
IhorSF <ihor@silo.finance>
2024-12-16 14:49:01 +0400

    silo-vaults: SiloIncentivesController incentives claiming logic

--- silo-vaults/contracts/interfaces/IIncentivesClaimingLogic.sol
+++ silo-vaults/contracts/interfaces/IIncentivesClaimingLogic.sol
@@ -5,5 +5,5 @@ pragma solidity >=0.5.0;
 interface IIncentivesClaimingLogic {
     /// @notice Claim and distribute rewards to the vault.
     /// @dev Can claim rewards from multiple sources and distribute them to the vault users.
-    function claimAndDistribute() external;
+    function claimRewardsAndDistribute() external;
 }
