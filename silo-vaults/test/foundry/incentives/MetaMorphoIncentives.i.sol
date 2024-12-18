010313649c64c36bc140ff9ea053d899fc5fa3f2
Neo Racer <0xNeoRacer@protonmail.com>
2024-12-18 07:32:14 +0100

    simple test is failing

--- silo-vaults/test/foundry/incentives/MetaMorphoIncentives.i.sol
+++ silo-vaults/test/foundry/incentives/MetaMorphoIncentives.i.sol
@@ -1,23 +1,34 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.28;
 
-import {UtilsLib} from "morpho-blue/libraries/UtilsLib.sol";
+import {SiloIncentivesController} from "silo-core/contracts/incentives/SiloIncentivesController.sol";
+import {MintableToken} from "silo-core/test/foundry/_common/MintableToken.sol";
+import {DistributionTypes} from "silo-core/contracts/incentives/lib/DistributionTypes.sol";
 
-import {IVaultIncentivesModule} from "../../../contracts/interfaces/IVaultIncentivesModule.sol";
 import {ErrorsLib} from "../../../contracts/libraries/ErrorsLib.sol";
+
+import {INotificationReceiver} from "../../../contracts/interfaces/INotificationReceiver.sol";
+import {IVaultIncentivesModule} from "../../../contracts/interfaces/IVaultIncentivesModule.sol";
 import {IntegrationTest} from "../helpers/IntegrationTest.sol";
 import {NB_MARKETS, CAP, MIN_TEST_ASSETS, MAX_TEST_ASSETS} from "../helpers/BaseTest.sol";
+
 
 /*
  FOUNDRY_PROFILE=vaults-tests forge test --ffi --mc MetaMorphoInternalTest -vvv
 */
 contract MetaMorphoIncentivesTest is IntegrationTest {
+    MintableToken reward1 = new MintableToken(18);
+    MintableToken reward2 = new MintableToken(18);
+
     function setUp() public override {
         super.setUp();
 
         _setCap(allMarkets[0], CAP);
         _setCap(allMarkets[1], CAP);
         _setCap(allMarkets[2], CAP);
+
+        reward1.setOnDemand(true);
+        reward2.setOnDemand(true);
     }
 
     /*
@@ -39,6 +50,58 @@ contract MetaMorphoIncentivesTest is IntegrationTest {
         // does not revert without incentives setup
         vault.deposit(1, address(this));
 
+        // does not revert without incentives setup
+        vault.claimRewards();
+    }
+
+    /*
+     FOUNDRY_PROFILE=vaults-tests forge test --ffi --mt test_vaults_incentives_deposit_withRewardsSetup -vv
+    */
+    function test_vaults_incentives_deposit_withRewardsSetup() public {
+        address user = makeAddr("user");
+
+        IVaultIncentivesModule vaultIncentivesModule = vault.INCENTIVES_MODULE();
+
+        // NOTICE: notificator must be vaultIncentivesModule not vault
+        // TODO add test when notifier will be wrong and expect no rewards
+        SiloIncentivesController vaultIncentivesController = new SiloIncentivesController(address(this), address(vaultIncentivesModule));
+
+        // standard program for vault users
+        vaultIncentivesController.createIncentivesProgram(DistributionTypes.IncentivesProgramCreationInput({
+            name: "x",
+            rewardToken: address(reward1),
+            emissionPerSecond: 1,
+            distributionEnd: uint40(block.timestamp + 10)
+        }));
+
+        // add normal program
+        // and add immediate distributionj
+
+        vm.prank(OWNER);
+        vaultIncentivesModule.addNotificationReceiver(INotificationReceiver(address(vaultIncentivesController)));
+
+        vm.expectCall(
+            address(vaultIncentivesController),
+            abi.encodeWithSelector(
+                INotificationReceiver.afterTokenTransfer.selector,
+                address(0),
+                0,
+                user,
+                1,
+                1,
+                1
+            )
+        );
+
+        vm.prank(OWNER);
+        vaultIncentivesModule.addNotificationReceiver(INotificationReceiver(address(vaultIncentivesController)));
+
+
+        // does not revert without incentives setup
+        vm.prank(user);
+        vault.deposit(1, user);
+
+        vaultIncentivesController.getRewardsBalance(user, "x");
         // does not revert without incentives setup
         vault.claimRewards();
     }
