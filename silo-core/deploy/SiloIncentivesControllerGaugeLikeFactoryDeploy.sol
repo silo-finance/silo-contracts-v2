36393fb784664989a06a22ca7fd7b3a9c9d5817e
IhorSF <ihor@silo.finance>
2024-12-19 12:52:36 +0400

    depl script comment

--- silo-core/deploy/SiloIncentivesControllerGaugeLikeFactoryDeploy.sol
+++ silo-core/deploy/SiloIncentivesControllerGaugeLikeFactoryDeploy.sol
@@ -6,6 +6,11 @@ import {SiloCoreContracts} from "silo-core/common/SiloCoreContracts.sol";
 import {SiloIncentivesControllerGaugeLikeFactory} from "silo-core/contracts/incentives/SiloIncentivesControllerGaugeLikeFactory.sol";
 import {ISiloIncentivesControllerGaugeLikeFactory} from "silo-core/contracts/incentives/interfaces/ISiloIncentivesControllerGaugeLikeFactory.sol";
 
+/**
+    FOUNDRY_PROFILE=core \
+        forge script silo-core/deploy/SiloIncentivesControllerGaugeLikeFactoryDeploy.s.sol \
+        --ffi --broadcast --rpc-url http://127.0.0.1:8545 --verify
+ */
 contract SiloIncentivesControllerGaugeLikeFactoryDeploy is CommonDeploy {
     function run() public returns (ISiloIncentivesControllerGaugeLikeFactory factory) {
         uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
