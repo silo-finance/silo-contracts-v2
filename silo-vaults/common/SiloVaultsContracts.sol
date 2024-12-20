2e6ed0b209b048b305fbe0edf3348b91d4282cd8
IhorSF <ihor@silo.finance>
2024-12-20 11:20:12 +0400

    silo-vaults: incentives controller claiming logic factory

--- silo-vaults/common/SiloVaultsContracts.sol
+++ silo-vaults/common/SiloVaultsContracts.sol
@@ -7,6 +7,7 @@ library SiloVaultsContracts {
     string public constant SILO_VAULTS_FACTORY = "SiloVaultsFactory.sol";
     string public constant PUBLIC_ALLOCATOR = "PublicAllocator.sol";
     string public constant VAULT_INCENTIVES_MODULE = "VaultIncentivesModule.sol";
+    string public constant SILO_INCENTIVES_CONTROLLER_CL_FACTORY = "SiloIncentivesControllerCLFactory.sol";
 }
 
 library SiloVaultsDeployments {
