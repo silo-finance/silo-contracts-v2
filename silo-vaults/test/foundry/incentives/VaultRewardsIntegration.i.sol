a874aa4720fe1a4e4097668d668c6f8a15310c34
Neo Racer <0xNeoRacer@protonmail.com>
2025-01-03 20:08:43 +0100

    qa

--- silo-vaults/test/foundry/incentives/VaultRewardsIntegration.i.sol
+++ silo-vaults/test/foundry/incentives/VaultRewardsIntegration.i.sol
@@ -1,70 +1,21 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.28;
 
-import {Ownable} from "openzeppelin5/access/Ownable2Step.sol";
 import {Strings} from "openzeppelin5/utils/Strings.sol";
 import {Hook} from "silo-core/contracts/lib/Hook.sol";
 
-import {SiloIncentivesControllerGaugeLike} from "silo-core/contracts/incentives/SiloIncentivesControllerGaugeLike.sol";
 import {DistributionTypes} from "silo-core/contracts/incentives/lib/DistributionTypes.sol";
-import {SiloIncentivesController} from "silo-core/contracts/incentives/SiloIncentivesController.sol";
 import {SiloMathLib} from "silo-core/contracts/lib/SiloMathLib.sol";
-import {IShareToken} from "silo-core/contracts/interfaces/IShareToken.sol";
-import {IGaugeHookReceiver} from "silo-core/contracts/interfaces/IGaugeHookReceiver.sol";
-import {IGaugeLike} from "silo-core/contracts/interfaces/IGaugeLike.sol";
 import {IHookReceiver} from "silo-core/contracts/interfaces/IHookReceiver.sol";
-import {MintableToken} from "silo-core/test/foundry/_common/MintableToken.sol";
-
-import {SiloIncentivesControllerCL} from "../../../contracts/incentives/claiming-logics/SiloIncentivesControllerCL.sol";
 
 import {INotificationReceiver} from "../../../contracts/interfaces/INotificationReceiver.sol";
-import {IntegrationTest} from "../helpers/IntegrationTest.sol";
 
-import {CAP} from "../helpers/BaseTest.sol";
+import {VaultRewardsIntegrationSetup} from "./VaultRewardsIntegrationSetup.sol";
 
 /*
  FOUNDRY_PROFILE=vaults-tests forge test --ffi --mc VaultRewardsIntegrationTest -vvv
 */
-contract VaultRewardsIntegrationTest is IntegrationTest {
-    MintableToken reward1 = new MintableToken(18);
-
-    SiloIncentivesControllerGaugeLike siloIncentivesController;
-    SiloIncentivesController vaultIncentivesController;
-
-    function setUp() public virtual override {
-        super.setUp();
-
-        _setCap(allMarkets[0], _cap());
-        _setCap(allMarkets[1], _cap());
-        _setCap(allMarkets[2], _cap());
-
-        reward1.setOnDemand(true);
-
-        vaultIncentivesController = new SiloIncentivesController(address(this), address(vault));
-        vm.label(address(vaultIncentivesController), "VaultIncentivesController");
-
-        // SiloIncentivesController is per silo
-        siloIncentivesController = new SiloIncentivesControllerGaugeLike(
-            address(this), address(partialLiquidation), address(silo1)
-        );
-
-        // set SiloIncentivesController as gauge for hook
-        vm.prank(Ownable(address(partialLiquidation)).owner());
-        IGaugeHookReceiver(address(partialLiquidation)).setGauge(
-            IGaugeLike(address(siloIncentivesController)), IShareToken(address(silo1))
-        );
-
-        _sortSupplyQueueIdleLast();
-        assertEq(address(vault.supplyQueue(0)), address(silo1), "supplyQueue[0] == silo1");
-
-        (, uint24 hooksAfter) = IGaugeHookReceiver(address(partialLiquidation)).hookReceiverConfig(address(silo1));
-        assertEq(hooksAfter, Hook.COLLATERAL_TOKEN | Hook.SHARE_TOKEN_TRANSFER, "hook after");
-    }
-
-    function _cap() internal view virtual returns (uint256) {
-        return CAP;
-    }
-
+contract VaultRewardsIntegrationTest is VaultRewardsIntegrationSetup {
     /*
      FOUNDRY_PROFILE=vaults-tests forge test --ffi --mt test_vaults_rewards_noRevert -vv
     */
@@ -257,14 +208,15 @@ contract VaultRewardsIntegrationTest is IntegrationTest {
     }
 
     /*
-     FOUNDRY_PROFILE=vaults-tests forge test --ffi --mt test_1secondDistribution -vv --mc VaultRewardsIntegrationTest
+     FOUNDRY_PROFILE=vaults-tests forge test --ffi --mt test_1secondDistribution_pass -vv
     */
-    function test_1secondDistribution() public {
+    function test_1secondDistribution_pass() public {
+
+        uint128 depositAmount = 123e18;
+        uint128 rewardsPerSec = 123456789123453000; // zeros at the end to avoid precision errors
+
         _setupIncentives();
 
-        uint256 rewardsPerSec = 3210;
-
-        uint256 depositAmount = 1e5;
         vault.deposit(depositAmount, address(this));
 
         siloIncentivesController.createIncentivesProgram(DistributionTypes.IncentivesProgramCreationInput({
@@ -276,35 +228,58 @@ contract VaultRewardsIntegrationTest is IntegrationTest {
 
         vm.warp(block.timestamp + 100);
 
-        assertEq(
-            siloIncentivesController.getRewardsBalance(address(vault), "program1"),
-            rewardsPerSec,
-            "expected rewards for silo for 1s"
-        );
+        uint256 siloRewardPreview = siloIncentivesController.getRewardsBalance(address(vault), "program1");
+
+        assertEq(siloRewardPreview, rewardsPerSec, "expected rewards for silo for 1s");
 
         vault.claimRewards();
         string memory programName1 = Strings.toHexString(address(reward1));
 
-        assertEq(
-            vaultIncentivesController.getRewardsBalance(address(this), programName1),
-            rewardsPerSec,
-            "expected rewards for vault for 1s"
-        );
+        uint256 vaultRewardPreview = vaultIncentivesController.getRewardsBalance(address(this), programName1);
+
+        assertEq(siloRewardPreview, vaultRewardPreview, "with 1 user rewards for silo == rewards for vault");
+
+        assertEq(vaultRewardPreview, rewardsPerSec, "expected rewards for vault for 1s");
 
         vaultIncentivesController.claimRewards(address(this));
 
-        assertEq(reward1.balanceOf(address(this)), rewardsPerSec, "got reward token");
+        assertEq(reward1.balanceOf(address(this)), vaultRewardPreview, "user got rewards");
     }
 
-    function _setupIncentives() private {
-        vm.prank(OWNER);
-        vaultIncentivesModule.addNotificationReceiver(INotificationReceiver(address(vaultIncentivesController)));
+    /*
+     FOUNDRY_PROFILE=vaults-tests forge test --ffi --mt test_1secondDistribution_fuzz -vv
+    */
+    function test_1secondDistribution_fuzz(uint128 _depositAmount, uint128 _rewardsPerSec) public {
+        vm.assume(_rewardsPerSec > 1e3);
 
-        SiloIncentivesControllerCL cl = new SiloIncentivesControllerCL(
-            address(vaultIncentivesController), address(siloIncentivesController)
-        );
+        _setupIncentives();
 
-        vm.prank(OWNER);
-        vaultIncentivesModule.addIncentivesClaimingLogic(address(silo1), cl);
+        vault.deposit(_depositAmount, address(this));
+
+        siloIncentivesController.createIncentivesProgram(DistributionTypes.IncentivesProgramCreationInput({
+            name: "program1",
+            rewardToken: address(reward1),
+            emissionPerSecond: uint104(_rewardsPerSec),
+            distributionEnd: uint40(block.timestamp + 1)
+        }));
+
+        vm.warp(block.timestamp + 100);
+
+        uint256 siloRewardPreview = siloIncentivesController.getRewardsBalance(address(vault), "program1");
+
+        assertLe(siloRewardPreview, _rewardsPerSec,"[silo] we can not get more that was set for 1s");
+
+        vault.claimRewards();
+        string memory programName1 = Strings.toHexString(address(reward1));
+
+        uint256 vaultRewardPreview = vaultIncentivesController.getRewardsBalance(address(this), programName1);
+
+        assertLe(vaultRewardPreview, siloRewardPreview, "vault rewards can not be higher than claimed from silo");
+
+        assertLe(vaultRewardPreview, _rewardsPerSec, "[vault] we can not get more that was set for 1s");
+
+        vaultIncentivesController.claimRewards(address(this));
+
+        assertEq(reward1.balanceOf(address(this)), vaultRewardPreview, "user got rewards");
     }
 }
