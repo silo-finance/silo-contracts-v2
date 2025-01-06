65dd3e032f1b3ad9661712e9ca0e092ee11b9a65
IhorSF <ihor@silo.finance>
2025-01-06 12:34:11 +0400

    silo-core: Silo router multicall

--- silo-core/deploy/SiloRouterDeploy.s.sol
+++ silo-core/deploy/SiloRouterDeploy.s.sol
@@ -1,13 +1,9 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.28;
 
-import {AddrLib} from "silo-foundry-utils/lib/AddrLib.sol";
-import {ChainsLib} from "silo-foundry-utils/lib/ChainsLib.sol";
-
 import {CommonDeploy} from "./_CommonDeploy.sol";
 import {SiloCoreContracts} from "silo-core/common/SiloCoreContracts.sol";
 import {SiloRouter} from "silo-core/contracts/SiloRouter.sol";
-import {AddrKey} from "common/addresses/AddrKey.sol";
 
 /**
     FOUNDRY_PROFILE=core \
@@ -18,11 +14,9 @@ contract SiloRouterDeploy is CommonDeploy {
     function run() public returns (SiloRouter siloRouter) {
         uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
 
-        address nativeToken = _nativeToken();
-
         vm.startBroadcast(deployerPrivateKey);
 
-        siloRouter = new SiloRouter(nativeToken);
+        siloRouter = new SiloRouter();
 
         vm.stopBroadcast();
 
