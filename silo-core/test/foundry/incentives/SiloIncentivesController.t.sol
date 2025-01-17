b666044ed2802c98f5762b3a74ea08a0b91109ee
IhorSF <ihor@silo.finance>
2025-01-17 16:32:52 +0400

    silo-core: setDistributionEnd updates lastUpdateTimestamp

--- silo-core/test/foundry/incentives/SiloIncentivesController.t.sol
+++ silo-core/test/foundry/incentives/SiloIncentivesController.t.sol
@@ -260,6 +260,30 @@ contract SiloIncentivesControllerTest is Test {
         assertEq(ERC20Mock(_rewardToken).balanceOf(user2), 10e18, "invalid user2 balance");
     }
 
+    // FOUNDRY_PROFILE=core-test forge test -vvv --ffi --mt test_setDistributionEnd_Success
+    function test_setDistributionEnd_Success() public {
+        vm.prank(_owner);
+        _controller.createIncentivesProgram(DistributionTypes.IncentivesProgramCreationInput({
+            name: _PROGRAM_NAME,
+            rewardToken: _rewardToken,
+            distributionEnd: uint40(block.timestamp + 100),
+            emissionPerSecond: 1e18
+        }));
+
+        IDistributionManager.IncentiveProgramDetails memory details = _controller.incentivesProgram(_PROGRAM_NAME);
+
+        uint256 lastUpdateTimestamp = details.lastUpdateTimestamp;
+        assertEq(lastUpdateTimestamp, block.timestamp, "invalid lastUpdateTimestamp");
+
+        vm.warp(block.timestamp + 100);
+
+        vm.prank(_owner);
+        _controller.setDistributionEnd(_PROGRAM_NAME, uint40(block.timestamp + 1000));
+
+        details = _controller.incentivesProgram(_PROGRAM_NAME);
+        assertEq(details.lastUpdateTimestamp, block.timestamp, "invalid lastUpdateTimestamp");
+    }
+
     // FOUNDRY_PROFILE=core-test forge test -vvv --ffi --mt test_decrease_rewards
     function test_decrease_rewards() public {
         ERC20Mock(_rewardToken).mint(address(_controller), 11e18);
