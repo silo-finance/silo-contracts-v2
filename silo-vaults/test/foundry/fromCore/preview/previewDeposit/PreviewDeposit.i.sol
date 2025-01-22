4ae9607b27002426b834d08c40c2b034abf5c46f
Neo Racer <0xNeoRacer@protonmail.com>
2025-01-22 12:54:18 +0100

    .

--- silo-vaults/test/foundry/fromCore/preview/previewDeposit/PreviewDeposit.i.sol
+++ silo-vaults/test/foundry/fromCore/preview/previewDeposit/PreviewDeposit.i.sol
@@ -13,76 +13,64 @@ contract PreviewDepositTest is VaultsLittleHelper {
         depositor = makeAddr("Depositor");
     }
 
-    // TODO can we
     /*
     forge test -vv --ffi --mt test_previewDeposit_beforeInterest_fuzz
     */
     /// forge-config: core-test.fuzz.runs = 10000
-    function test_previewDeposit_beforeInterest_fuzz(uint128 _assets, bool _defaultType, uint8 _type) public {
+    function test_previewDeposit_beforeInterest_fuzz(uint128 _assets) public {
         vm.assume(_assets > 0);
-        vm.assume(_type == uint8(ISilo.AssetType.Collateral) || _type == uint8(ISilo.AssetType.Protected));
 
-        (ISilo.CollateralType cType, ISilo.AssetType aType) = _castToTypes(_defaultType, _type);
-
-        uint256 previewShares = _defaultType ? silo0.previewDeposit(_assets) : silo0.previewDeposit(_assets, cType);
-        uint256 shares = _defaultType ? _deposit(_assets, depositor) : _deposit(_assets, depositor, cType);
+        uint256 previewShares =vault.previewDeposit(_assets);
+        uint256 shares = _deposit(_assets, depositor);
 
         assertEq(previewShares, shares, "previewDeposit must return as close but NOT more");
-        assertEq(previewShares, silo0.convertToShares(_assets, aType), "previewDeposit == convertToShares");
+        assertEq(previewShares, vault.convertToShares(_assets), "previewDeposit == convertToShares");
     }
 
     /*
     forge test -vv --ffi --mt test_previewDeposit_afterNoInterest
     */
     /// forge-config: core-test.fuzz.runs = 10000
-    function test_previewDeposit_afterNoInterest_fuzz(uint128 _assets, bool _defaultType, uint8 _type) public {
+    function test_previewDeposit_afterNoInterest_fuzz(uint128 _assets) public {
         vm.assume(_assets > 0);
-        vm.assume(_type == uint8(ISilo.AssetType.Collateral) || _type == uint8(ISilo.AssetType.Protected));
 
-        (ISilo.CollateralType cType, ISilo.AssetType aType) = _castToTypes(_defaultType, _type);
-
-        uint256 sharesBefore = _defaultType ? _deposit(_assets, depositor) : _deposit(_assets, depositor, cType);
+        uint256 sharesBefore = _deposit(_assets, depositor);
 
         vm.warp(block.timestamp + 365 days);
         silo0.accrueInterest();
+        silo1.accrueInterest();
 
-        uint256 previewShares = _defaultType ? silo0.previewDeposit(_assets) : silo0.previewDeposit(_assets, cType);
-        uint256 gotShares = _defaultType ? _deposit(_assets, depositor) : _deposit(_assets, depositor, cType);
+        uint256 previewShares = vault.previewDeposit(_assets);
+        uint256 gotShares = _deposit(_assets, depositor);
 
         assertEq(previewShares, gotShares, "previewDeposit must return as close but NOT more");
         assertEq(previewShares, sharesBefore, "without interest shares must be the same");
-        assertEq(previewShares, silo0.convertToShares(_assets, aType), "previewDeposit == convertToShares");
+        assertEq(previewShares, silo0.convertToShares(_assets), "previewDeposit == convertToShares");
     }
 
     /*
     forge test -vv --ffi --mt test_previewDeposit_withInterest
     */
     /// forge-config: core-test.fuzz.runs = 10000
-    function test_previewDeposit_withInterest_1token_fuzz(uint256 _assets, bool _protected) public {
-        _previewDeposit_withInterest(_assets, _protected);
+    function test_previewDeposit_withInterest_1token_fuzz(uint256 _assets) public {
+        _previewDeposit_withInterest(_assets);
     }
 
-    function _previewDeposit_withInterest(uint256 _assets, bool _protected) private {
+    function _previewDeposit_withInterest(uint256 _assets) private {
         vm.assume(_assets < type(uint128).max);
         vm.assume(_assets > 0);
 
-        ISilo.CollateralType cType = _protected ? ISilo.CollateralType.Protected : ISilo.CollateralType.Collateral;
-        ISilo.AssetType aType = _protected ? ISilo.AssetType.Protected : ISilo.AssetType.Collateral;
-
-        uint256 sharesBefore = _deposit(_assets, depositor, cType);
+        uint256 sharesBefore = _deposit(_assets, depositor);
         _depositForBorrow(_assets, depositor);
 
-        if (_protected) {
-            _makeDeposit(silo1, token1, _assets, depositor, ISilo.CollateralType.Protected);
-        }
-
+        address borrower = makeAddr("Borrower");
         _deposit(_assets / 10 == 0 ? 2 : _assets, borrower);
         _borrow(_assets / 10 + 1, borrower); // +1 ensure we not borrowing 0
 
         vm.warp(block.timestamp + 365 days);
 
-        uint256 previewShares0 = silo0.previewDeposit(_assets, cType);
-        uint256 previewShares1 = silo1.previewDeposit(_assets, cType);
+        uint256 previewShares0 = vault.previewDeposit(_assets);
+        uint256 previewShares1 = vault.previewDeposit(_assets);
 
         assertLe(
             previewShares1,
@@ -92,7 +80,7 @@ contract PreviewDepositTest is VaultsLittleHelper {
 
         if (previewShares1 == 0) {
             // if preview is zero for `_assets`, then deposit should also reverts
-            _depositForBorrowRevert(_assets, depositor, cType, ISilo.InputZeroShares.selector);
+            _depositForBorrowRevert(_assets, depositor, ISilo.InputZeroShares.selector);
         } else {
             assertEq(
                 previewShares1,
