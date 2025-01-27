da135deb12a29e36aac353f545e375774dd8f35f
Neo Racer <0xNeoRacer@protonmail.com>
2025-01-27 16:15:49 +0100

    deployment

--- silo-core/deploy/SiloLensDeploy.s.sol
+++ silo-core/deploy/SiloLensDeploy.s.sol
@@ -14,6 +14,13 @@ import {ISiloLens} from "silo-core/contracts/interfaces/ISiloLens.sol";
         forge script silo-core/deploy/SiloLensDeploy.s.sol \
         --ffi --rpc-url $RPC_SONIC --broadcast --verify
 
+    ETHERSCAN_API_KEY=$VERIFIER_API_KEY_SONIC \
+    forge verify-contract 0xE05966aee69CeCD677a30f469812Ced650cE3b5E \
+        SiloLens \
+        --compiler-version 0.8.28 \
+        --rpc-url $RPC_SONIC \
+        --watch
+
     remember to run `TowerRegistration` script after deployment!
  */
 contract SiloLensDeploy is CommonDeploy {
