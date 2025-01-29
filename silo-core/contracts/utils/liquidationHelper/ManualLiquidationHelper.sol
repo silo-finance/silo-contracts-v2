0625547b73172fba1b90af2e7d10a4e6cec08a82
Neo Racer <0xNeoRacer@protonmail.com>
2025-01-29 20:18:32 +0100

    linter

--- silo-core/contracts/utils/liquidationHelper/ManualLiquidationHelper.sol
+++ silo-core/contracts/utils/liquidationHelper/ManualLiquidationHelper.sol
@@ -14,7 +14,8 @@ import {IWrappedNativeToken} from "../../interfaces/IWrappedNativeToken.sol";
 
 import {TokenRescuer} from "../TokenRescuer.sol";
 
-/// @notice ManualLiquidationHelper is a utility contract that can be changed and replaced at any point. It is not considered a part of Silo protocol. Use at your own risk.
+/// @notice ManualLiquidationHelper is a utility contract that can be changed and replaced at any point.
+/// It is not considered a part of Silo protocol. Use at your own risk.
 contract ManualLiquidationHelper is TokenRescuer {
     using Address for address payable;
     using SafeERC20 for IERC20;
