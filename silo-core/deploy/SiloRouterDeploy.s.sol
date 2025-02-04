cbd525addb8df3b119e805257bbe11e125ad1fc2
IhorSF <102213749+IhorSF@users.noreply.github.com>
2025-02-04 12:31:57 +0400

    silo-core: Silo router redesign (#961)

--- silo-core/deploy/SiloRouterDeploy.s.sol
+++ silo-core/deploy/SiloRouterDeploy.s.sol
@@ -3,20 +3,24 @@ pragma solidity 0.8.28;
 
 import {CommonDeploy} from "./_CommonDeploy.sol";
 import {SiloCoreContracts} from "silo-core/common/SiloCoreContracts.sol";
-import {SiloRouter} from "silo-core/contracts/SiloRouter.sol";
+import {SiloRouter} from "silo-core/contracts/silo-router/SiloRouter.sol";
+import {SiloRouterImplementation} from "silo-core/contracts/silo-router/SiloRouterImplementation.sol";
 
 /**
     FOUNDRY_PROFILE=core \
         forge script silo-core/deploy/SiloRouterDeploy.s.sol \
-        --ffi --broadcast --rpc-url http://127.0.0.1:8545 --verify
+        --ffi --rpc-url http://127.0.0.1:8545 --broadcast --verify
  */
 contract SiloRouterDeploy is CommonDeploy {
     function run() public returns (SiloRouter siloRouter) {
         uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
+        address deployer = vm.addr(deployerPrivateKey);
 
         vm.startBroadcast(deployerPrivateKey);
 
-        siloRouter = new SiloRouter();
+        SiloRouterImplementation implementation = new SiloRouterImplementation();
+
+        siloRouter = new SiloRouter(deployer, address(implementation));
 
         vm.stopBroadcast();
 
