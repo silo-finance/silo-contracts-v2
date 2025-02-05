af1bc309eddbcba61176e5d8687eb6aed38a9c7a
IhorSF <ihor@silo.finance>
2025-02-05 15:51:57 +0400

    silo-core: more methods in the SiloLens

--- silo-core/contracts/SiloLens.sol
+++ silo-core/contracts/SiloLens.sol
@@ -1,15 +1,20 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.28;
 
+import {IERC20} from "openzeppelin5/token/ERC20/IERC20.sol";
+
 import {ISiloLens, ISilo} from "./interfaces/ISiloLens.sol";
 import {IShareToken} from "./interfaces/IShareToken.sol";
 import {SiloLensLib} from "./lib/SiloLensLib.sol";
 import {SiloStdLib} from "./lib/SiloStdLib.sol";
 import {IPartialLiquidation} from "./interfaces/IPartialLiquidation.sol";
-
+import {IInterestRateModel} from "./interfaces/IInterestRateModel.sol";
+import {ISiloConfig} from "./interfaces/ISiloConfig.sol";
 
 /// @title SiloLens is a helper contract for integrations and UI
 contract SiloLens is ISiloLens {
+    uint256 internal constant _PRECISION_DECIMALS = 1e18;
+
     /// @inheritdoc ISiloLens
     function getRawLiquidity(ISilo _silo) external view virtual returns (uint256 liquidity) {
         return SiloLensLib.getRawLiquidity(_silo);
@@ -100,5 +105,89 @@ contract SiloLens is ISiloLens {
 
         uint256 maxRepay = _silo.maxRepay(_borrower);
         fullLiquidation = maxRepay == debtToRepay;
+    }
+
+    function borrowAPY(ISilo _silo) public view returns (uint256 rcur) {
+        IInterestRateModel irm = getModel(_silo);
+
+        rcur = irm.getCurrentInterestRate(address(_silo), block.timestamp);
+    }
+
+    function getDepositAmount(ISilo _silo, address _user, uint256 _timestamp)
+        public
+        view
+        returns (uint256 totalUserDeposits)
+    {
+        ISiloConfig _config = _silo.config();
+
+        (, address collateralShareToken,) = _config.getShareTokens(address(_silo));
+
+        uint256 share = IERC20(collateralShareToken).balanceOf(_user);
+
+        if (share == 0) {
+            return 0;
+        }
+
+        IInterestRateModel irm = IInterestRateModel(getModel(_silo));
+
+        uint256 rcomp = irm.getCompoundInterestRate(address(_silo), _timestamp);
+
+        (,, uint256 daoFee, uint256 deployerFee,) = SiloStdLib.getFeesAndFeeReceiversWithAsset(_silo);
+
+        (,,, uint256 collateralAssets, uint256 debtAssets) = _silo.getSiloStorage();
+
+        totalUserDeposits = _totalDepositsWithInterest(collateralAssets, debtAssets, daoFee + deployerFee, rcomp);
+    }
+
+    function totalBorrowAmountWithInterest(ISilo _silo) public view returns (uint256 totalBorrowAmount) {
+        IInterestRateModel irm = IInterestRateModel(getModel(_silo));
+
+        uint256 rcomp = irm.getCompoundInterestRate(address(_silo), block.timestamp);
+
+        (,,,, uint256 debtAssets) = _silo.getSiloStorage();
+
+        totalBorrowAmount = _totalBorrowAmountWithInterest(debtAssets, rcomp);
+    }
+
+    function getModel(ISilo _silo) public view returns (IInterestRateModel irm) {
+        ISiloConfig _config = _silo.config();
+
+        (address silo0, address silo1) = _config.getSilos();
+
+        bool isSilo0;
+
+        if (address(_silo) == silo0) {
+            isSilo0 = true;
+        } else if (address(_silo) != silo1) {
+            revert InvalidSilo();
+        }
+
+        ISiloConfig.ConfigData memory _configData = isSilo0 ? _config.getConfig(silo0) : _config.getConfig(silo1);
+
+        irm = IInterestRateModel(_configData.interestRateModel);
+    }
+
+    function _totalDepositsWithInterest(
+        uint256 _assetTotalDeposits,
+        uint256 _assetTotalBorrows,
+        uint256 _protocolShareFee,
+        uint256 _rcomp
+    )
+        internal
+        pure
+        returns (uint256 _totalDepositsWithInterests)
+    {
+        uint256 depositorsShare = _PRECISION_DECIMALS - _protocolShareFee;
+
+        return _assetTotalDeposits + _assetTotalBorrows * _rcomp / _PRECISION_DECIMALS * depositorsShare /
+            _PRECISION_DECIMALS;
+    }
+
+    function _totalBorrowAmountWithInterest(uint256 _totalBorrowAmount, uint256 _rcomp)
+        internal
+        pure
+        returns (uint256 totalBorrowAmountWithInterests)
+    {
+        totalBorrowAmountWithInterests = _totalBorrowAmount + _totalBorrowAmount * _rcomp / _PRECISION_DECIMALS;
     }
 }
