81714d8d1e80670e66f4ede904b6c95567ccf7c2
Neo Racer <0xNeoRacer@protonmail.com>
2025-02-17 13:05:46 +0100

    gas

--- silo-core/test/foundry/gas/TransitionCollateral.gas.sol
+++ silo-core/test/foundry/gas/TransitionCollateral.gas.sol
@@ -32,7 +32,7 @@ contract TransitionCollateralTest is Gas, Test {
             address(silo0),
             abi.encodeCall(ISilo.transitionCollateral, (ASSETS, BORROWER, ISilo.CollateralType.Collateral)),
             "transitionCollateral (when debt)",
-            292682 // 74K for interest
+            292907 // 74K for interest
         );
     }
 }
