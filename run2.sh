4c4a2f1ab2e2733eef09eb8ebef101490410fcc5
Otakar <Otakar@certora.com>
2025-02-17 09:24:24 +0100

    loop iter changed to 2

--- run2.sh
+++ run2.sh
@@ -1,11 +1,20 @@
-# certoraRun.py certora/config/vaults/consistentState.conf --prover_version master
+# certoraRun.py certora/config/vaults/consistentState.conf --prover_version master --rule supplyCapIsEnabled --msg supplyCapIsEnabled
+# certoraRun.py certora/config/vaults/consistentState.conf --prover_version master --exclude_rule supplyCapIsEnabled
+
 # certoraRun.py certora/config/vaults/distinctIdentifiers.conf --prover_version master
+# certoraRun.py certora/config/vaults/enabled.conf --prover_version master --exclude_rule isInDepositQThenIsInWithdrawQ
 # certoraRun.py certora/config/vaults/enabled.conf --prover_version master --rule nonZeroCapHasPositiveRank --msg nonZeroCapHasPositiveRank
-certoraRun.py certora/config/vaults/enabled.conf --prover_version master --rule isInDepositQThenIsInWithdrawQ --msg isInDepositQThenIsInWithdrawQ
+# certoraRun.py certora/config/vaults/enabled.conf --prover_version master --rule addedToSupplyQThenIsInWithdrawQ --msg addedToSupplyQThenIsInWithdrawQ
+
+# certoraRun.py certora/config/vaults/enabled.conf --prover_version master --rule inWithdrawQueueIsEnabled --msg inWithdrawQueueIsEnabled
+# certoraRun.py certora/config/vaults/enabled.conf --prover_version master --rule inWithdrawQueueIsEnabledPreservedUpdateWithdrawQueue --msg inWithdrawQueueIsEnabled2
 
 # certoraRun.py certora/config/vaults/immutability.conf --prover_version master
 # certoraRun.py certora/config/vaults/lastUpdated.conf --prover_version master
-# certoraRun.py certora/config/vaults/liveness.conf --prover_version master
+
+# certoraRun.py certora/config/vaults/tokenApproval.conf --prover_version master
+
+# certoraRun.py certora/config/vaults/liveness.conf --prover_version master --rule canPauseSupply
 # certoraRun.py certora/config/vaults/marketInteractions.conf --prover_version master
 # certoraRun.py certora/config/vaults/pendingValues.conf --prover_version master
 # certoraRun.py certora/config/vaults/range.conf --prover_version master
@@ -13,5 +22,7 @@ certoraRun.py certora/config/vaults/enabled.conf --prover_version master --rule 
 # certoraRun.py certora/config/vaults/reverts.conf --prover_version master
 # certoraRun.py certora/config/vaults/roles.conf --prover_version master
 # certoraRun.py certora/config/vaults/siloVault.conf --prover_version master
-# certoraRun.py certora/config/vaults/tokens.conf --prover_version master --rule onlySpecicifeidMethodsCanDecreaseMarketBalance
-# certoraRun.py certora/config/vaults/timelock.conf --prover_version master
+# certoraRun.py certora/config/vaults/tokens.conf --prover_version master
+# certoraRun.py certora/config/vaults/timelock.conf --prover_version master --exclude_rule removableTime
+
+certoraRun.py certora/config/vaults/tokens.conf --prover_version master --verify SiloVaultHarness:certora/specs/vaults/MarketBalance.spec --msg onlySpecicifeidMethodsCanDecreaseMarketBalance --parametric_contracts SiloVaultHarness
