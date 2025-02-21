b0cb7a85eb6ca43b5dc0598249f737ab4ada2a7d
Neo Racer <0xNeoRacer@protonmail.com>
2025-02-21 11:25:38 +0100

    .

--- silo-vaults/contracts/SiloVaultsFactory.sol
+++ silo-vaults/contracts/SiloVaultsFactory.sol
@@ -43,7 +43,7 @@ contract SiloVaultsFactory is ISiloVaultsFactory {
             Clones.clone(VAULT_INCENTIVES_MODULE_IMPLEMENTATION)
         );
 
-        vaultIncentivesModule.__VaultIncentivesModule_init(msg.sender);
+        vaultIncentivesModule.__VaultIncentivesModule_init(initialOwner);
 
         siloVault = ISiloVault(address(
             new SiloVault(initialOwner, initialTimelock, vaultIncentivesModule, asset, name, symbol))
