c2b6de4da6c3221d1fbc6673c3d4a1b693e08f4a
IhorSF <ihor@silo.finance>
2025-02-24 11:07:37 +0400

    transfer, transferFrom tests

--- silo-vaults/test/foundry/call-and-reenter/methods/TransferFromTest.sol
+++ silo-vaults/test/foundry/call-and-reenter/methods/TransferFromTest.sol
@@ -6,22 +6,12 @@ import {IERC20Errors} from "openzeppelin5/interfaces/draft-IERC6093.sol";
 import {MethodReentrancyTest} from "silo-core/test/foundry/Silo/reentrancy/methods/MethodReentrancyTest.sol";
 import {TestStateLib} from "silo-vaults/test/foundry/call-and-reenter/TestState.sol";
 import {ISiloVault} from "silo-vaults/contracts/interfaces/ISiloVault.sol";
+import {ErrorsLib} from "silo-vaults/contracts/libraries/ErrorsLib.sol";
 
 contract TransferFromTest is MethodReentrancyTest {
     function callMethod() external {
-        emit log_string("\tEnsure it is protected");
-        _ensureItIsProtected();
-    }
+        emit log_string("\tEnsure it reverts as expected");
 
-    function verifyReentrancy() external {
-        _ensureItIsProtected();
-    }
-
-    function methodDescription() external pure returns (string memory description) {
-        description = "transferFrom(address,address,uint256)";
-    }
-
-    function _ensureItIsProtected() internal {
         ISiloVault vault = TestStateLib.vault();
 
         vm.expectRevert(abi.encodeWithSelector(
@@ -29,5 +19,16 @@ contract TransferFromTest is MethodReentrancyTest {
         ));
 
         vault.transferFrom(address(0), address(0), 100e18);
+    }
+
+    function verifyReentrancy() external {
+        ISiloVault vault = TestStateLib.vault();
+
+        vm.expectRevert(abi.encodeWithSelector(ErrorsLib.ReentrancyError.selector));
+        vault.transferFrom(address(0), address(0), 100e18);
+    }
+
+    function methodDescription() external pure returns (string memory description) {
+        description = "transferFrom(address,address,uint256)";
     }
 }
