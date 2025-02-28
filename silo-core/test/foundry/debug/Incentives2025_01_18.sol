9edaff7a83d4d154f95907960bc9fd9ce0fb7fd3
Neo Racer <0xNeoRacer@protonmail.com>
2025-02-28 13:54:14 +0100

    use TEN_POW_PRECISION but test stil failing

--- silo-core/test/foundry/debug/Incentives2025_01_18.sol
+++ silo-core/test/foundry/debug/Incentives2025_01_18.sol
@@ -415,7 +415,11 @@ contract Incentives20250118Test is IntegrationTest {
         emit log_named_uint("rewardsBalance", rewardsBalance);
         emit log_named_uint("expectedRewards", expectedRewards);
 
-        assertEq(rewardsBalance, expectedRewards, "rewardsBalance should be equal to expectedRewards");
+        assertEq(
+            rewardsBalance,
+            expectedRewards,
+            "[_getRewardWithIndex] rewardsBalance should be equal to expectedRewards"
+        );
     }
 
     function _getRewardsNoIndex(
@@ -434,16 +438,20 @@ contract Incentives20250118Test is IntegrationTest {
         expectedRewards = _emissionPerSecond * _timeDelta * siloBalance / siloTotalSupply;
         emit log_named_uint("expectedRewards", expectedRewards);
 
-        assertEq(rewardsBalance, expectedRewards, "rewardsBalance should be equal to expectedRewards");
+        assertEq(
+            rewardsBalance,
+            expectedRewards,
+            "[_getRewardsNoIndex] rewardsBalance should be equal to expectedRewards"
+        );
     }
 
     function _getRewards(
         uint256 principalUserBalance,
         uint256 reserveIndex,
         uint256 userIndex
-    ) internal pure virtual returns (uint256 rewards) {
+    ) internal view virtual returns (uint256 rewards) {
         rewards = principalUserBalance * (reserveIndex - userIndex);
-        unchecked { rewards /= 10 ** 18; }
+        unchecked { rewards /= _controller.TEN_POW_PRECISION(); }
     }
 
     function _getIncentivesProgramIndex(
