df7006efe21fac084b399a34a0f89826d62d70fb
IhorSF <102213749+IhorSF@users.noreply.github.com>
2025-03-19 08:22:29 +0400

    silo-vaults: setCap fn in the SiloVaultActionsLib (#1108)

--- silo-vaults/contracts/libraries/SiloVaultActionsLib.sol
+++ silo-vaults/contracts/libraries/SiloVaultActionsLib.sol
@@ -1,13 +1,18 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.28;
 
-import {IERC4626} from "openzeppelin5/interfaces/IERC4626.sol";
+import {IERC4626, IERC20} from "openzeppelin5/interfaces/IERC4626.sol";
 import {UtilsLib} from "morpho-blue/libraries/UtilsLib.sol";
+import {SafeERC20} from "openzeppelin5/token/ERC20/utils/SafeERC20.sol";
 
 import {ErrorsLib} from "./ErrorsLib.sol";
 import {EventsLib} from "./EventsLib.sol";
+import {PendingUint192, MarketConfig} from "./PendingLib.sol";
+import {ConstantsLib} from "./ConstantsLib.sol";
 
 library SiloVaultActionsLib {
+    using SafeERC20 for IERC20;
+
     function setIsAllocator(
         address _newAllocator,
         bool _newIsAllocator,
@@ -18,6 +23,45 @@ library SiloVaultActionsLib {
         _isAllocator[_newAllocator] = _newIsAllocator;
 
         emit EventsLib.SetIsAllocator(_newAllocator, _newIsAllocator);
+    }
+
+    /// @dev Sets the cap of the market.
+    function setCap(
+        IERC4626 _market,
+        uint184 _supplyCap,
+        address _asset,
+        mapping(IERC4626 => MarketConfig) storage _config,
+        mapping(IERC4626 => PendingUint192) storage _pendingCap,
+        IERC4626[] storage _withdrawQueue
+    ) external returns (bool updateTotalAssets) {
+        MarketConfig storage marketConfig = _config[_market];
+        uint256 approveValue;
+
+        if (_supplyCap > 0) {
+            if (!marketConfig.enabled) {
+                _withdrawQueue.push(_market);
+
+                if (_withdrawQueue.length > ConstantsLib.MAX_QUEUE_LENGTH) revert ErrorsLib.MaxQueueLengthExceeded();
+
+                marketConfig.enabled = true;
+
+                // Take into account assets of the new market without applying a fee.
+                updateTotalAssets = true;
+
+                emit EventsLib.SetWithdrawQueue(msg.sender, _withdrawQueue);
+            }
+
+            marketConfig.removableAt = 0;
+            // one time approval, so market can pull any amount of tokens from SiloVault in a future
+            approveValue = type(uint256).max;
+        }
+
+        marketConfig.cap = _supplyCap;
+        IERC20(_asset).forceApprove(address(_market), approveValue);
+
+        emit EventsLib.SetCap(msg.sender, _market, _supplyCap);
+
+        delete _pendingCap[_market];
     }
 
     /// @dev Simulates a withdraw of `assets` from ERC4626 vault.
