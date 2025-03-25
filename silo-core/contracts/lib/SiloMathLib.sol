eafb7593bc2cc20f4d2ff86b973745e1488e0940
Thomas A. Anderson <92584446+0xNeoRacer@users.noreply.github.com>
2025-03-25 19:47:22 +0100

    Update silo-core/contracts/lib/SiloMathLib.sol
    Co-authored-by: shadowyedd <89228160+shadowyedd@users.noreply.github.com>

--- silo-core/contracts/lib/SiloMathLib.sol
+++ silo-core/contracts/lib/SiloMathLib.sol
@@ -348,11 +348,13 @@ library SiloMathLib {
             }
 
             // safe to unchecked because we checked for overflow in above `if`
-            uint256 result36 = _total * _percent + _currentFraction;
-
             // safe, because max value after modulo will be 1e18 (_PRECISION_DECIMALS) and this is less than 2 ** 64
-            fraction = uint64(result36 % _PRECISION_DECIMALS);
-            integral = fraction < _currentFraction ? 1 : 0;
+            // calculate remainder for current interest
+            // integral is amount above 1e18 after adding _currentFraction and remainder
+            // fraction is what we get below 1e18
+            uint256 remainder = (_total * _percent) % _PRECISION_DECIMALS;
+            integral = (_currentFraction + remainder) / _PRECISION_DECIMALS;
+            fraction = uint64((_currentFraction + remainder) % _PRECISION_DECIMALS);
         }
     }
 }
