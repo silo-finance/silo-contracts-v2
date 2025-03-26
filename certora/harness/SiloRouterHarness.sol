08700a5bfd4bd612767b66eb83df6354a3fb0098
Alexander Nutz <alex@certora.com>
2025-03-26 11:43:13 +0200

    tentative setup fixes after update

--- certora/harness/SiloRouterHarness.sol
+++ certora/harness/SiloRouterHarness.sol
@@ -3,7 +3,7 @@ import { SiloRouter } from "silo-core/contracts/silo-router/SiloRouter.sol";
 
 contract SiloRouterHarness is SiloRouter {
     constructor (address _initialOwner, address _implementation) SiloRouter(_initialOwner, _implementation) {
-        __Ownable_init(_initialOwner);
+        // __Ownable_init(_initialOwner);
 
         IMPLEMENTATION = _implementation;
     }
