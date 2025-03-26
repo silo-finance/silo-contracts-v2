2605558b6c7dbb275f91b5d92fdda03f82595880
Sergey Maksimov <sergey@silo.finance>
2025-03-27 00:00:21 +0700

    fmt

--- silo-oracles/contracts/newMarkets/FrxUsd_scUSD.t.sol
+++ silo-oracles/contracts/newMarkets/FrxUsd_scUSD.t.sol
@@ -3,7 +3,6 @@ pragma solidity 0.8.28;
 
 import {NewMarketTest} from "silo-oracles/contracts/newMarkets/common/NewMarket.sol";
 
-
 contract FrxUSD_scUSD_Test is NewMarketTest {
     constructor() NewMarketTest(16123358, 0x6452b9aE8011800457b42C4fBBDf4579afB96228, 112, 100) {}
 }
--- silo-oracles/contracts/newMarkets/StS_wS.t.sol
+++ silo-oracles/contracts/newMarkets/StS_wS.t.sol
@@ -3,7 +3,6 @@ pragma solidity 0.8.28;
 
 import {NewMarketTest} from "silo-oracles/contracts/newMarkets/common/NewMarket.sol";
 
-
 contract StS_wS_Test is NewMarketTest {
     constructor() NewMarketTest(16133130, 0xA3BF8b1eE377bBe6152A6885eaeE8747dcBEa35D, 6158, 6073) {}
 }
