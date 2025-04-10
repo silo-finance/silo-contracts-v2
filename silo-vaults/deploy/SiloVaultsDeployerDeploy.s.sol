e36ec806e53564d310a3c8a6d4427996432daa87
Neo Racer <0xNeoRacer@protonmail.com>
2025-04-10 08:55:39 +0200

    .

--- silo-vaults/deploy/SiloVaultsDeployerDeploy.s.sol
+++ silo-vaults/deploy/SiloVaultsDeployerDeploy.s.sol
@@ -5,27 +5,30 @@ import {SiloVaultsContracts} from "silo-vaults/common/SiloVaultsContracts.sol";
 
 import {SiloVaultsDeployer} from "../contracts/SiloVaultsDeployer.sol";
 
-import {ISiloVaultsFactory} from "./interfaces/ISiloVaultsFactory.sol";
-import {IdleVaultsFactory} from "./IdleVaultsFactory.sol";
+import {ISiloVaultsFactory} from "../contracts/interfaces/ISiloVaultsFactory.sol";
+import {IdleVaultsFactory} from "../contracts/IdleVaultsFactory.sol";
 
 import {CommonDeploy} from "./common/CommonDeploy.sol";
 
 /*
     ETHERSCAN_API_KEY=$ETHERSCAN_API_KEY FOUNDRY_PROFILE=vaults \
         forge script silo-vaults/deploy/SiloVaultsDeployerDeploy.s.sol:SiloVaultsDeployerDeploy \
-        --ffi --broadcast --rpc-url http://127.0.0.1:8545 \
-        --verify
+        --ffi --rpc-url http://127.0.0.1:8545 \
+        --broadcast --verify
 */
 contract SiloVaultsDeployerDeploy is CommonDeploy {
     function run() public returns (SiloVaultsDeployer vaultsDeployer) {
         uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
 
-        ISiloVaultsFactory vaultsFactory = getDeployedAddress(SiloCoreContracts.SILO_VAULTS_FACTORY);
-        ISiloVaultsFactory idleVaultsFactory = getDeployedAddress(SiloCoreContracts.IDLE_VAULTS_FACTORY);
+        address vaultsFactory = getDeployedAddress(SiloVaultsContracts.SILO_VAULTS_FACTORY);
+        address idleVaultsFactory = getDeployedAddress(SiloVaultsContracts.IDLE_VAULTS_FACTORY);
 
         vm.startBroadcast(deployerPrivateKey);
 
-        vaultsDeployer = new SiloVaultsDeployer(vaultsFactory, idleVaultsFactory);
+        vaultsDeployer = new SiloVaultsDeployer(
+            ISiloVaultsFactory(vaultsFactory),
+            IdleVaultsFactory(idleVaultsFactory)
+        );
 
         vm.stopBroadcast();
 
