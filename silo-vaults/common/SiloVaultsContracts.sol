b2c40ca3a5ffd95cdbc6b59e9196d6ab98d714df
Neo Racer <0xNeoRacer@protonmail.com>
2025-04-10 08:48:50 +0200

    .

--- silo-vaults/common/SiloVaultsContracts.sol
+++ silo-vaults/common/SiloVaultsContracts.sol
@@ -7,6 +7,7 @@ library SiloVaultsContracts {
     string public constant SILO_VAULTS_FACTORY = "SiloVaultsFactory.sol";
     string public constant PUBLIC_ALLOCATOR = "PublicAllocator.sol";
     string public constant IDLE_VAULTS_FACTORY = "IdleVaultsFactory.sol";
+    string public constant SILO_VAULTS_DEPLOYER = "SiloVaultsDeployer.sol";
     string public constant SILO_INCENTIVES_CONTROLLER_CL_FACTORY = "SiloIncentivesControllerCLFactory.sol";
 }
 
