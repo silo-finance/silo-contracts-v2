75c7d6ae9b16ecaf69704729b3dc66ba155f149a
Neo Racer <0xNeoRacer@protonmail.com>
2025-04-23 16:23:16 +0200

    qa with debt

--- silo-core/test/foundry/lib/SiloERC4626Lib/MaxRedeem.t.sol
+++ silo-core/test/foundry/lib/SiloERC4626Lib/MaxRedeem.t.sol
@@ -12,6 +12,7 @@ import {IShareToken} from "silo-core/contracts/interfaces/IShareToken.sol";
 import {SiloLensLib} from "silo-core/contracts/lib/SiloLensLib.sol";
 import {SiloERC4626Lib} from "silo-core/contracts/lib/SiloERC4626Lib.sol";
 import {ShareTokenLib} from "silo-core/contracts/lib/ShareTokenLib.sol";
+import {ShareDebtToken} from "silo-core/contracts/utils/ShareDebtToken.sol";
 import {ShareTokenDecimalsPowLib} from "../../_common/ShareTokenDecimalsPowLib.sol";
 
 import {SiloLittleHelper} from "../../_common/SiloLittleHelper.sol";
@@ -25,30 +26,62 @@ contract MaxRedeemTest is SiloLittleHelper, Test {
     }
 
     /*
-        FOUNDRY_PROFILE=core_test forge test -vv --ffi --mt test_maxWithdraw_protected_dust
+        FOUNDRY_PROFILE=core_test forge test -vv --ffi --mt test_maxWithdraw_dust_protected
     */
-    function test_maxWithdraw_protected_dust() public {
-        _maxWithdraw_dust(ISilo.CollateralType.Protected);
+    function test_maxWithdraw_dust_protected() public {
+        _maxWithdraw_dust({_type: ISilo.CollateralType.Protected, _withDebt: false});
+
     }
 
     /*
-        FOUNDRY_PROFILE=core_test forge test -vv --ffi --mt test_maxWithdraw_collateral_dust
+        FOUNDRY_PROFILE=core_test forge test -vv --ffi --mt test_maxWithdraw_dust_collateral
     */
-    function test_maxWithdraw_collateral_dust() public {
-        _maxWithdraw_dust(ISilo.CollateralType.Collateral);
+    function test_maxWithdraw_dust_collateral() public {
+        _maxWithdraw_dust({_type: ISilo.CollateralType.Collateral, _withDebt: false});
     }
 
-    function _maxWithdraw_dust(ISilo.CollateralType _type) internal {
+    /*
+        FOUNDRY_PROFILE=core_test forge test -vv --ffi --mt test_maxWithdraw_dust_withDebt_protected
+    */
+    function test_maxWithdraw_dust_withDebt_protected() public {
+        _maxWithdraw_dust({_type: ISilo.CollateralType.Protected, _withDebt: true});
+
+    }
+
+    /*
+        FOUNDRY_PROFILE=core_test forge test -vv --ffi --mt test_maxWithdraw_dust_withDebt_collateral
+    */
+    function test_maxWithdraw_dust_withDebt_collateral() public {
+        _maxWithdraw_dust({_type: ISilo.CollateralType.Collateral, _withDebt: true});
+    }
+
+    function _maxWithdraw_dust(ISilo.CollateralType _type, bool _withDebt) internal {
         address depositor = makeAddr("depositor");
         address owner = address(this);
+        vm.label(owner, "owner");
 
-        _deposit(1, depositor, _type);
+        _deposit(10, depositor, _type);
 
-        (address protectedShareToken, address collateralShareToken,) = silo0.config().getShareTokens(address(silo0));
+        (
+            address protectedShareToken,
+            address collateralShareToken,
+        ) = silo0.config().getShareTokens(address(silo0));
+
         address shareToken = _type == ISilo.CollateralType.Protected ? protectedShareToken : collateralShareToken;
 
         vm.prank(depositor);
         IShareToken(shareToken).transfer(owner, 999);
+
+        if (_withDebt) {
+            (,,address debtShareToken) = silo1.config().getShareTokens(address(silo1));
+
+            _depositForBorrow(9, address(2));
+            _borrow(3, depositor);
+
+            ShareDebtToken(debtShareToken).setReceiveApproval(depositor, 3);
+            vm.prank(depositor);
+            IShareToken(debtShareToken).transfer(owner, 1);
+        }
 
         uint256 maxRedeem = silo0.maxRedeem(owner, _type);
         assertEq(maxRedeem, 0, "max redeem should return 0 on dust shares");
