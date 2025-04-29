a0d78d43517dd389a30c705a951acc513d33e272
Neo Racer <0xNeoRacer@protonmail.com>
2025-04-29 16:41:11 +0200

    wip

--- silo-core/contracts/leverage/SiloLeverage.sol
+++ silo-core/contracts/leverage/SiloLeverage.sol
@@ -8,16 +8,18 @@ import {Math} from "openzeppelin5/utils/math/Math.sol";
 import {ISilo, IERC3156FlashLender} from "silo-core/contracts/interfaces/ISilo.sol";
 import {IERC3156FlashBorrower} from "silo-core/contracts/interfaces/IERC3156FlashBorrower.sol";
 
-import {ISilo, IERC4626, IERC3156FlashLender} from "./interfaces/ISilo.sol";
-import {IShareToken} from "./interfaces/IShareToken.sol";
+import {ISilo, IERC4626, IERC3156FlashLender} from "../interfaces/ISilo.sol";
+import {IShareToken} from "../interfaces/IShareToken.sol";
 
-import {IERC3156FlashBorrower} from "./interfaces/IERC3156FlashBorrower.sol";
-import {ISiloConfig} from "./interfaces/ISiloConfig.sol";
-import {ISiloFactory} from "./interfaces/ISiloFactory.sol";
-import {ISiloOracle} from "./interfaces/ISiloOracle.sol";
-import {ISiloLeverage} from "./ISiloLeverage.sol";
+import {IERC3156FlashBorrower} from "../interfaces/IERC3156FlashBorrower.sol";
+import {ISiloConfig} from "../interfaces/ISiloConfig.sol";
+import {ISiloFactory} from "../interfaces/ISiloFactory.sol";
+import {ISiloOracle} from "../interfaces/ISiloOracle.sol";
+import {ISiloLeverage} from "../interfaces/ISiloLeverage.sol";
 
-contract SiloLeverage is ISiloLeverage, IERC3156FlashBorrower {
+contract SiloLeverage is ISiloLeverage
+//, IERC3156FlashBorrower
+{
     uint256 internal constant _DECIMALS = 1e18;
     
     /// @dev Silo is not designed to work with ether, but it can act as a middleware
@@ -33,74 +35,72 @@ contract SiloLeverage is ISiloLeverage, IERC3156FlashBorrower {
         uint64 _multiplier,
         IERC3156FlashLender _flashLoanLender,
         uint256 _borrowAmount
-    ) external view virtual override {
+    ) external virtual {
         // TODO
     }
 
     /// @inheritdoc ISiloLeverage
     function previewLeverage(
         ISilo _silo,
-        uint256 _deposit,
-        uint64 _multiplier,
-        IERC3156FlashLender _flashloanLender,
-        uint64 _swapSlippage
-    ) external view virtual override returns (
+        uint256 _deposit, // 2e18
+        uint64 _multiplier, // 1.5e18
+        IERC3156FlashLender _flashLoanLender,
+        uint64 _swapSlippage, // 0.01e18,
+        uint256 _collateralToDebtRatio // 500000000e18
+    ) external view virtual returns (
         uint256 flashLoanAmount,
         uint256 borrowAmount,
         uint64 finalMultiplier
     ) {
+        uint256 leverageFeeInDebtToken = 1;
         ISiloConfig config = _silo.config();
 
+        (address silo0, address silo1) = config.getSilos();
+
         (
             ISiloConfig.ConfigData memory collateralConfig,
             ISiloConfig.ConfigData memory debtConfig
-        ) = config.getConfigsForBorrow();
+        ) = config.getConfigsForBorrow(silo0 == address(_silo) ? silo1 : silo0);
 
-        uint256 maxFlashLoan = _flashloanLender.maxFlashLoan(collateralConfig.token);
+        // maxFlashLoan: unlimited
+        uint256 maxFlashLoan = _flashLoanLender.maxFlashLoan(collateralConfig.token);
+        // flashLoanAmount = (2e18 * 1.5e18 / 1e18) = 3e18
         flashLoanAmount = Math.min(_deposit * _multiplier / _DECIMALS, maxFlashLoan);
-        finalMultiplier = flashLoanAmount * _DECIMALS / _deposit;
+        // finalMultiplier = 3e18 * 1e18 / 2e18 = 1.5e18
+        finalMultiplier = uint64(flashLoanAmount * _DECIMALS / _deposit);
 
-        (
-            ISiloConfig.ConfigData memory collateralConfig,
-            ISiloConfig.ConfigData memory debtConfig
-        ) = config.getConfigsForBorrow();
+        // flashLoanFeeInCollateralToken = 0.005e18
+        uint256 flashLoanFeeInCollateralToken = _flashLoanLender.flashFee(collateralConfig.token, flashLoanAmount);
 
         // TODO notice we using maxLtvOracle
+        // flashValue = 3e18 * $2000 = 6000e6
         uint256 flashValue = _quote(collateralConfig.maxLtvOracle, flashLoanAmount, collateralConfig.token);
+        // totalCollateral = 3e18 + 2e18 = 5e18
+        uint256 totalCollateral = flashLoanAmount + _deposit;
 
+        // totalCollateralValue = 5e18 * $2000 = 10_000e6
         uint256 totalCollateralValue = collateralConfig.maxLtvOracle == address(0)
-            ? flashLoanAmount
-            : _calculateValueBasedOnRatio(flashLoanAmount, flashValue, flashLoanAmount + _deposit);
+            ? totalCollateral
+            : _calculateValueBasedOnRatio(flashLoanAmount, flashValue, totalCollateral);
 
         // we need to calculate amount to borrow,
         // for that we need to figure out ratio between collateral token and borrow token
         // would that be easier to provide by BE? our oracles are not ment for swap
 
-        uint256 debtSampleAmount = 1e18;
-        uint256 debtSampleValue = _quote(debtConfig.maxLtvOracle, debtSampleAmount, debtConfig.token);
-
-        uint256 maxBorrowValue = totalCollateralValue * collateralConfig.maxLtv / _DECIMALS;
-
-        uint256 borrowAmountForWholeCollateral = debtSampleAmount * totalCollateralValue / debtSampleValue;
         // maxLTV % of total collateral (flashLoanAmount + _deposit)
-        uint256 maxBorrowAmount = debtSampleAmount * maxBorrowValue / debtSampleValue;
+        // maxPossibleBorrowAmount = 5e18 * 0.80e18 / 500000000e18 = 8000e6
+        uint256 maxPossibleBorrowAmount = totalCollateral * collateralConfig.maxLtv / _collateralToDebtRatio;
 
-        // TODO or BE can provide collateralToDebtRatio based on sample quote?
-        uint256 collateralToDebtRatio = _collateralToDebtRatio({
-            _totalCollateralValue: totalCollateralValue,
-            _maxLtv: collateralConfig.maxLtv,
-            _debtOracle: debtConfig.maxLtvOracle,
-            _debtToken: debtConfig.token
-        });
-
+        // collateralSlippage = 0.01e18 * (3e18 + 0.005e18) / 1e18 = 0.03005e18
         uint256 collateralSlippage = _swapSlippage * (flashLoanAmount + flashLoanFeeInCollateralToken) / _DECIMALS;
         // amount of collateral needed after swap
+        // amountOut = 3e18 + 0.005e18 + 0.03005e18 = 3.03505e18
         uint256 amountOut = flashLoanAmount + flashLoanFeeInCollateralToken + collateralSlippage;
-        uint256 amountOutInDebtToken = amountOut * _DECIMALS / collateralToDebtRatio;
+        // amountOutInDebtToken = 3.03505e18 * 1e18 / 500000000e18 = 6070.1e18
+        uint256 amountOutInDebtToken = amountOut * _DECIMALS / _collateralToDebtRatio;
 
-        uint256 flashLoanFeeInDebtToken = _flashloanLender.flashFee(collateralConfig.token, flashLoanAmount) * _DECIMALS / collateralToDebtRatio;
-
-        borrowAmount = Math.min(amountOutInDebtToken + leverageFeeInDebtToken, maxBorrowAmount);
+        // borrowAmount = min(6070.1e18 + 1, 8000e6) = 6070.1e18
+        borrowAmount = Math.min(amountOutInDebtToken + leverageFeeInDebtToken, maxPossibleBorrowAmount);
     }
 
     function _calculateValueBasedOnRatio(uint256 _amount, uint256 _value, uint256 _inputAmount)
@@ -121,31 +121,11 @@ contract SiloLeverage is ISiloLeverage, IERC3156FlashBorrower {
             : ISiloOracle(_oracle).quote(_baseAmount, _baseToken);
     }
 
-
-    function _collateralToDebtRatio(
-        uint256 _totalCollateralValue,
-        uint256 _maxLtv,
-        address _debtOracle,
-        address _debtToken
-    )
-        internal
-        view
-        returns (uint256 collateralToDebtRatio)
-    {
-        uint256 debtSampleAmount = 1e18;
-        uint256 debtSampleValue = _quote(_debtOracle, debtSampleAmount, _debtToken);
-
-        uint256 maxBorrowValue = _totalCollateralValue * _maxLtv / _DECIMALS;
-
-        uint256 borrowAmountForWholeCollateral = debtSampleAmount * _totalCollateralValue / debtSampleValue;
-        collateralToDebtRatio = _totalCollateralValue * _DECIMALS / borrowAmountForWholeCollateral;
-    }
-
     function closeLeverage(
         ISilo _silo,
         ISilo.CollateralType _collateralType,
-        IERC3156FlashLender _flashloanLender
-    ) external view virtual override returns (ISilo) {
+        IERC3156FlashLender _flashLoanLender
+    ) external view virtual returns (ISilo) {
         // TODO
     }
 
@@ -155,19 +135,19 @@ contract SiloLeverage is ISiloLeverage, IERC3156FlashBorrower {
     {
         // TODO
     }
-
-    function borrowFlashloan(
-        IERC3156FlashBorrower _receiver,
-        address _token,
-        uint256 _amount,
-        bytes calldata _data
-    ) {
-        // TODO
-        IERC3156FlashLender.flashLoan({
-            _receiver: _receiver,
-            _token: _token,
-            _amount: _amount,
-            _data: _data
-        });
-    }
+//
+//    function borrowFlashloan(
+//        IERC3156FlashBorrower _receiver,
+//        address _token,
+//        uint256 _amount,
+//        bytes calldata _data
+//    ) {
+//        // TODO
+//        IERC3156FlashLender.flashLoan({
+//            _receiver: _receiver,
+//            _token: _token,
+//            _amount: _amount,
+//            _data: _data
+//        });
+//    }
 }
