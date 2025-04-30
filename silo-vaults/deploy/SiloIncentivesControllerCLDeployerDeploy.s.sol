6a6ed0eba8a30112e8c71e863f8d43c55e26fba5
Sergey Maksimov <sergey@silo.finance>
2025-04-30 15:35:32 +0300

    deployment script

---
+++ silo-vaults/deploy/SiloIncentivesControllerCLDeployerDeploy.s.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.28;
+
+import {SiloVaultsContracts, SiloVaultsDeployments} from "silo-vaults/common/SiloVaultsContracts.sol";
+import {ChainsLib} from "silo-foundry-utils/lib/ChainsLib.sol";
+
+import {
+    SiloIncentivesControllerCLDeployer
+} from "silo-vaults/contracts/incentives/claiming-logics/SiloIncentivesControllerCLDeployer.sol";
+
+import {
+    ISiloIncentivesControllerCLFactory
+} from "silo-vaults/contracts/interfaces/ISiloIncentivesControllerCLFactory.sol";
+
+import {CommonDeploy} from "./common/CommonDeploy.sol";
+
+/*
+    ETHERSCAN_API_KEY=$ETHERSCAN_API_KEY FOUNDRY_PROFILE=vaults \
+        forge script silo-vaults/deploy/SiloIncentivesControllerCLDeployerDeploy.s.sol \
+        --ffi --rpc-url $RPC_SONIC --verify --broadcast
+        
+*/
+contract SiloIncentivesControllerCLDeployerDeploy is CommonDeploy {
+    function run() public returns (address factory) {
+        uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
+
+        ISiloIncentivesControllerCLFactory siloIncentivesControllerCLFactory = ISiloIncentivesControllerCLFactory(
+            SiloVaultsDeployments.get(SiloVaultsContracts.SILO_INCENTIVES_CONTROLLER_CL_FACTORY, ChainsLib.chainAlias())
+        );
+
+        vm.startBroadcast(deployerPrivateKey);
+        factory = address(new SiloIncentivesControllerCLDeployer(siloIncentivesControllerCLFactory));
+        vm.stopBroadcast();
+
+        _registerDeployment(address(factory), SiloVaultsContracts.SILO_INCENTIVES_CONTROLLER_CL_DEPLOYER);
+    }
+}
