877ddbf56c1d0a4cc4fdaae41a5e68f77e42b7e2
Sergey Maksimov <sergey@silo.finance>
2025-05-02 11:39:11 +0300

    remove unused

--- silo-core/contracts/token/SILOv2.sol
+++ silo-core/contracts/token/SILOv2.sol
@@ -1,21 +1,21 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.28;
 
-import {ERC20, IERC20} from "openzeppelin5/token/ERC20/ERC20.sol";
+import {ERC20} from "openzeppelin5/token/ERC20/ERC20.sol";
 import {ERC20Burnable} from "openzeppelin5/token/ERC20/extensions/ERC20Burnable.sol";
 import {ERC20Permit} from "openzeppelin5/token/ERC20/extensions/ERC20Permit.sol";
 import {Ownable2Step, Ownable} from "openzeppelin5/access/Ownable2Step.sol";
 import {Pausable} from "openzeppelin5/utils/Pausable.sol";
 
 /// @title SILOv2 token.
-contract SILOv2 is ERC20, ERC20Burnable, ERC20Permit, Ownable2Step, Pausable {
+contract SILOv2 is ERC20Burnable, ERC20Permit, Ownable2Step, Pausable {
     /// @notice $SILO token address used for $SILOv2 migration.
     ERC20Burnable public immutable SILO_V1; // solhint-disable-line var-name-mixedcase
 
     constructor(address _initialOwner, ERC20Burnable _siloV1)
         ERC20("SILOv2", "SiloGovernanceTokenV2")
-        Ownable(_initialOwner)
         ERC20Permit("SILOv2")
+        Ownable(_initialOwner)
     {
         SILO_V1 = _siloV1;
     }
