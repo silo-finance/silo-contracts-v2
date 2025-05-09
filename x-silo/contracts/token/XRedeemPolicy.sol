abd7904cd66050a31d7c4da4f7601c2b60d6fd5b
Neo Racer <0xNeoRacer@protonmail.com>
2025-05-09 17:14:01 +0200

    cleanup: rename vars, adapt ERC4626 standard to xSilo behaviour
    TODO: fix transfer/shares

--- x-silo/contracts/token/XRedeemPolicy.sol
+++ x-silo/contracts/token/XRedeemPolicy.sol
@@ -21,7 +21,7 @@ abstract contract XRedeemPolicy is Ownable2Step, TransientReentrancy {
     struct RedeemInfo {
         uint256 siloAmount;
         uint256 xSiloAmount;
-        uint256 xSiloAfterVestingRatio;
+        uint256 siloAmountAfterVesting;
         uint256 endTime;
     }
 
@@ -40,56 +40,107 @@ abstract contract XRedeemPolicy is Ownable2Step, TransientReentrancy {
     mapping(address => RedeemInfo[]) public userRedeems;
 
     event UpdateRedeemSettings(uint256 minRedeemRatio, uint256 maxRedeemRatio, uint256 minRedeemDuration, uint256 maxRedeemDuration);
-    event StartRedeem(address indexed userAddress, uint256 siloAmount, uint256 xSiloAmount, uint256 xSiloAfterVestingRatio, uint256 duration);
-    event FinalizeRedeem(address indexed userAddress, uint256 xSiloToRedeem, uint256 xSiloToBurn);
-    event CancelRedeem(address indexed userAddress, uint256 xSiloToReturn, uint256 xSiloToBurn);
+    event StartRedeem(address indexed _userAddress, uint256 siloAmount, uint256 xSiloAmount, uint256 siloAmountAfterVesting, uint256 duration);
+    event FinalizeRedeem(address indexed _userAddress, uint256 xSiloToRedeem, uint256 xSiloToBurn);
+    event CancelRedeem(address indexed _userAddress, uint256 xSiloToReturn, uint256 xSiloToBurn);
 
-    modifier validateRedeem(address userAddress, uint256 redeemIndex) {
-        require(redeemIndex < userRedeems[userAddress].length, "validateRedeem: redeem entry does not exist");
+    modifier validateRedeem(address _userAddress, uint256 redeemIndex) {
+        require(redeemIndex < userRedeems[_userAddress].length, "validateRedeem: redeem entry does not exist");
         _;
     }
 
-    function getAmountByVestingDuration(uint256 amount, uint256 duration) public view returns (uint256 underlyingAmount) {
-        if (duration < minRedeemDuration) {
+    function getAmountByVestingDuration(uint256 _xSiloAmount, uint256 _duration)
+        public
+        view
+        virtual
+        returns (uint256 siloAmount)
+    {
+        if (_xSiloAmount == 0 || _duration < minRedeemDuration) {
             return 0;
         }
 
         // capped to maxRedeemDuration
-        if (duration > maxRedeemDuration) {
-            return amount * maxRedeemRatio / 100;
+        if (_duration > maxRedeemDuration) {
+            return _xSiloAmount * maxRedeemRatio / 100;
         }
 
-        uint256 ratio = minRedeemRatio + ((duration - minRedeemDuration) * (maxRedeemRatio - minRedeemRatio) / (maxRedeemDuration - minRedeemDuration));
+        uint256 ratio = minRedeemRatio +
+            Math.mulDiv(
+                _duration - minRedeemDuration,
+                maxRedeemRatio - minRedeemRatio,
+                maxRedeemDuration - minRedeemDuration
+            );
 
-        return amount * ratio / 100;
+        siloAmount = _xSiloAmount * ratio / 100;
     }
 
-    function getUserRedeemsBalance(address userAddress) external view returns (uint256 redeemingAmount) {
-        uint256 len = userRedeems[userAddress].length;
+    function getSharesByVestingDuration(uint256 _siloAmount, uint256 _duration)
+        public
+        view
+        virtual
+        returns (uint256 xSiloAmount)
+    {
+        if (_siloAmount == 0 || _duration < minRedeemDuration) {
+            return 0;
+        }
+
+        // capped to maxRedeemDuration
+        if (_duration > maxRedeemDuration) {
+            return _siloAmount * 100 / maxRedeemRatio;
+        }
+
+        uint256 ratio = minRedeemRatio +
+            Math.mulDiv(
+                _duration - minRedeemDuration,
+                maxRedeemRatio - minRedeemRatio,
+                maxRedeemDuration - minRedeemDuration
+            );
+
+        xSiloAmount = _siloAmount * 100 / ratio;
+    }
+
+    function getUserRedeemsBalance(address _userAddress)
+        external
+        view
+        virtual
+        returns (uint256 redeemingSiloAmount)
+    {
+        uint256 len = userRedeems[_userAddress].length;
 
         if (len == 0) return 0;
 
         for (uint256 i = 0; i < len; i++) {
-            RedeemInfo storage redeemCache = userRedeems[userAddress][i];
-            redeemingAmount += redeemCache.siloAmount;
+            RedeemInfo storage redeemCache = userRedeems[_userAddress][i];
+            redeemingSiloAmount += redeemCache.siloAmount;
         }
     }
 
-    function getUserRedeemsLength(address userAddress) external view returns (uint256) {
-        return userRedeems[userAddress].length;
+    function getUserRedeemsLength(address _userAddress) external view returns (uint256) {
+        return userRedeems[_userAddress].length;
     }
 
-    function getUserRedeem(address userAddress, uint256 redeemIndex)
+    function getUserRedeem(address _userAddress, uint256 _redeemIndex)
         external
         view
-        validateRedeem(userAddress, redeemIndex)
-        returns (uint256 siloAmount, uint256 xSiloAmount, uint256 xSiloAfterVestingRatio, uint256 endTime)
+        validateRedeem(_userAddress, _redeemIndex)
+        returns (uint256 siloAmount, uint256 xSiloAmount, uint256 siloAmountAfterVesting, uint256 endTime)
     {
-        RedeemInfo storage redeemCache = userRedeems[userAddress][redeemIndex];
-        return (redeemCache.siloAmount, redeemCache.xSiloAmount, redeemCache.xSiloAfterVestingRatio, redeemCache.endTime);
+        RedeemInfo storage redeemCache = userRedeems[_userAddress][_redeemIndex];
+
+        return (
+            redeemCache.siloAmount,
+            redeemCache.xSiloAmount,
+            redeemCache.siloAmountAfterVesting,
+            redeemCache.endTime
+        );
     }
 
-    function updateRedeemSettings(uint256 _minRedeemRatio, uint256 _maxRedeemRatio, uint256 _minRedeemDuration, uint256 _maxRedeemDuration) external onlyOwner {
+    function updateRedeemSettings(
+        uint256 _minRedeemRatio,
+        uint256 _maxRedeemRatio,
+        uint256 _minRedeemDuration,
+        uint256 _maxRedeemDuration
+    ) external onlyOwner {
         require(_minRedeemRatio <= _maxRedeemRatio, "updateRedeemSettings: wrong ratio values");
         require(_minRedeemDuration < _maxRedeemDuration, "updateRedeemSettings: wrong duration values");
         // should never exceed 100%
@@ -103,35 +154,34 @@ abstract contract XRedeemPolicy is Ownable2Step, TransientReentrancy {
         emit UpdateRedeemSettings(_minRedeemRatio, _maxRedeemRatio, _minRedeemDuration, _maxRedeemDuration);
     }
 
-    /* TODO we need public method for that
-    all custom methods related to queue management should be implemented in this contract
-    */
-    function _redeemXSilo(uint256 xSiloAmount, uint256 duration) internal {
-        require(xSiloAmount > 0, "redeem: xSiloAmount cannot be null");
-        require(duration >= minRedeemDuration, "redeem: duration too low");
+    function redeemXSilo(uint256 _xSiloAmount, uint256 _duration) external virtual {
+        require(_xSiloAmount > 0, "redeem: xSiloAmount cannot be null");
+        require(_duration >= minRedeemDuration, "redeem: duration too low");
 
-        _transferShares(msg.sender, address(this), xSiloAmount);
+        // TODO
+//        _transferShares(msg.sender, address(this), _xSiloAmount);
 
         // get corresponding SILO amount based on duration
-        uint256 xSiloAfterVestingRatio = getAmountByVestingDuration(xSiloAmount, duration);
-        uint256 siloAmount = convertToAssets(xSiloAmount);
+        uint256 siloAmountAfterVesting = getAmountByVestingDuration(_xSiloAmount, _duration);
+        uint256 siloAmount = convertToAssets(_xSiloAmount);
 
-        emit StartRedeem(msg.sender, siloAmount, xSiloAmount, xSiloAfterVestingRatio, duration);
+        emit StartRedeem(msg.sender, siloAmount, _xSiloAmount, siloAmountAfterVesting, _duration);
 
         // if redeeming is not immediate, go through vesting process
-        if (duration > 0) {
+        if (_duration > 0) {
             // add redeeming entry
             userRedeems[msg.sender].push(
                 RedeemInfo({
                     siloAmount: siloAmount,
-                    xSiloAmount: xSiloAmount,
-                    xSiloAfterVestingRatio: xSiloAfterVestingRatio,
-                    endTime: block.timestamp + duration
+                    xSiloAmount: _xSiloAmount,
+                    siloAmountAfterVesting: siloAmountAfterVesting,
+                    endTime: block.timestamp + _duration
                 })
             );
         } else {
             // immediately redeem for SILO
-            _redeemAndBurn(msg.sender, xSiloAfterVestingRatio, xSiloAmount - xSiloAfterVestingRatio);
+            // TODO I think there is a bug here, we need to know shares to redeem, not  siloAmountAfterVesting
+            _redeemAndBurn(msg.sender, siloAmountAfterVesting, _xSiloAmount - siloAmountAfterVesting);
         }
     }
 
@@ -139,46 +189,55 @@ abstract contract XRedeemPolicy is Ownable2Step, TransientReentrancy {
         RedeemInfo storage redeemCache = userRedeems[msg.sender][redeemIndex];
         require(block.timestamp >= redeemCache.endTime, "finalizeRedeem: vesting duration has not ended yet");
 
-        _redeemAndBurn(msg.sender, redeemCache.xSiloAfterVestingRatio, redeemCache.xSiloAmount - redeemCache.xSiloAfterVestingRatio);
+        _redeemAndBurn(msg.sender, redeemCache.siloAmountAfterVesting, redeemCache.xSiloAmount - redeemCache.siloAmountAfterVesting);
 
         // remove redeem entry
         _deleteRedeemEntry(redeemIndex);
     }
 
-    function _redeemAndBurn(address userAddress, uint256 xSiloToRedeem, uint256 xSiloToBurn) internal {
-        redeem(xSiloToRedeem, userAddress, address(this));
-        _burnShares(userAddress, xSiloToBurn);
+    function _redeemAndBurn(address _userAddress, uint256 _xSiloToRedeem, uint256 _xSiloToBurn) internal {
+        // TODO something here is not right, we need separated method to transfer underlying
+        // and burn shares
+//        _withdraw(msg.sender);
+//        _transferShares(_xSiloToRedeem, _userAddress, address(this));
+//        _burnShares(_userAddress, _xSiloToBurn);
 
-        emit FinalizeRedeem(userAddress, xSiloToRedeem, xSiloToBurn);
+        emit FinalizeRedeem(_userAddress, _xSiloToRedeem, _xSiloToBurn);
     }
 
-    function cancelRedeem(uint256 redeemIndex) external nonReentrant validateRedeem(msg.sender, redeemIndex) {
-        RedeemInfo storage redeemCache = userRedeems[msg.sender][redeemIndex];
+    function cancelRedeem(uint256 _redeemIndex) external nonReentrant validateRedeem(msg.sender, _redeemIndex) {
+        RedeemInfo storage redeemCache = userRedeems[msg.sender][_redeemIndex];
 
         uint256 xSiloToReturn = convertToShares(redeemCache.siloAmount);
         uint256 xSiloToBurn = redeemCache.xSiloAmount - xSiloToReturn;
 
-        _burnShares(msg.sender, xSiloToBurn);
-        _transferShares(address(this), msg.sender, xSiloToReturn);
+        // TODO fix this
+//        _withdraw(msg.sender, );
+//        _burnShares(msg.sender, xSiloToBurn);
+//        _transferShares(address(this), msg.sender, xSiloToReturn);
 
         emit CancelRedeem(msg.sender, xSiloToReturn, xSiloToBurn);
 
         // remove redeem entry
-        _deleteRedeemEntry(redeemIndex);
+        _deleteRedeemEntry(_redeemIndex);
     }
 
-    function _deleteRedeemEntry(uint256 index) internal {
-        userRedeems[msg.sender][index] = userRedeems[msg.sender][userRedeems[msg.sender].length - 1];
+    function _deleteRedeemEntry(uint256 _index) internal {
+        userRedeems[msg.sender][_index] = userRedeems[msg.sender][userRedeems[msg.sender].length - 1];
         userRedeems[msg.sender].pop();
     }
-
-    function redeem(uint256 _shares, address _receiver, address _owner) public virtual returns (uint256);
 
     function convertToShares(uint256 _value) public virtual returns (uint256);
 
     function convertToAssets(uint256 _value) public virtual returns (uint256);
 
-    function _transferShares(address _from, address _to, uint256 _value) internal virtual;
+//    function _withdraw(
+//        address _caller,
+//        address _receiver,
+//        address _owner,
+//        uint256 _assetsToTransfer,
+//        uint256 _sharesToBurn
+//    ) internal virtual;
 
     function _burnShares(address _owner, uint256 _shares) internal virtual;
 }
--- x-silo/contracts/token/XSiloToken.sol
+++ x-silo/contracts/token/XSiloToken.sol
@@ -31,102 +31,18 @@ contract XSiloToken is ERC4626, XRedeemPolicy {
     }
 
     /** @dev See {IERC4626-maxWithdraw}. */
-    function maxWithdraw(address owner) public view virtual override returns (uint256) {
-        return convertToAssets(maxRedeem(owner));
-    }
-
-    /** @dev See {IERC4626-maxRedeem}. */
-    function maxRedeem(address owner) public view virtual override returns (uint256 shares) {
-        uint256 shares = balanceOf(owner);
-
-        uint256 len = userRedeems[owner].length;
-
-        if (len == 0) shares = getAmountByVestingDuration(shares, 0);
-        else {
-            for (uint256 i = 0; i < len; i++) {
-                RedeemInfo storage _redeem = userRedeems[owner][i];
-                shares += (_redeem.endTime <= block.timestamp ? _redeem.siloAmount : 0);
-            }
-        }
+    function maxWithdraw(address _owner) public view virtual override returns (uint256 assets) {
+        assets = getAmountByVestingDuration(balanceOf(_owner), 0);
     }
 
     /** @dev See {IERC4626-previewWithdraw}. */
-    function previewWithdraw(uint256 assets) public view virtual returns (uint256) {
-        return _convertToShares(assets, Math.Rounding.Ceil);
+    function previewWithdraw(uint256 _assets) public view virtual override returns (uint256 shares) {
+        shares = getSharesByVestingDuration(_assets, 0);
     }
 
     /** @dev See {IERC4626-previewRedeem}. */
-    function previewRedeem(uint256 shares) public view virtual returns (uint256) {
-        return _convertToAssets(shares, Math.Rounding.Floor);
-    }
-
-    // TODO previewWithdraw and previewRedeem probably need to stay as is, because
-    // they are not based on address, just raw amounts
-    // >> getAmountByVestingDuration(amount, 0)
-
-    /** @dev See {IERC4626-withdraw}. */
-    function withdraw(uint256 assets, address receiver, address owner) public virtual override returns (uint256) {
-        /* TODO
-        we can go over queue first, to withdraw tokens that are after period
-        should we sort queue? if we want to give most optimal solution we have to sort
-        if not - user can waste money for no reason
-        we can sort when we adding to queue, unfortunately this will mess with indexes
-
-        once we done with queue and we still need assets we can do immediate redeem for the missing part
-
-        */
-        uint256 maxAssets = maxWithdraw(owner);
-        if (assets > maxAssets) {
-            revert ERC4626ExceededMaxWithdraw(owner, assets, maxAssets);
-        }
-
-        uint256 shares = previewWithdraw(assets);
-        _withdraw(_msgSender(), receiver, owner, assets, shares);
-
-        return shares;
-    }
-
-    /** @dev See {IERC4626-redeem}. */
-    function redeem(uint256 shares, address receiver, address owner) public virtual override(ERC4626, XRedeemPolicy) returns (uint256) {
-        /*
-        TODO
-        same issue as for withdraw
-        */
-        uint256 maxShares = maxRedeem(owner);
-        if (shares > maxShares) {
-            revert ERC4626ExceededMaxRedeem(owner, shares, maxShares);
-        }
-
-        uint256 assets = previewRedeem(shares);
-        _withdraw(_msgSender(), receiver, owner, assets, shares);
-
-        return assets;
-    }
-
-    /**
-     * @dev Withdraw/redeem common workflow.
-     */
-    function _withdraw(
-        address caller,
-        address receiver,
-        address owner,
-        uint256 assets,
-        uint256 shares
-    ) internal virtual override {
-        if (caller != owner) {
-            _spendAllowance(owner, caller, shares);
-        }
-
-        // If _asset is ERC-777, `transfer` can trigger a reentrancy AFTER the transfer happens through the
-        // `tokensReceived` hook. On the other hand, the `tokensToSend` hook, that is triggered before the transfer,
-        // calls the vault, which is assumed not malicious.
-        //
-        // Conclusion: we need to do the transfer after the burn so that any reentrancy would happen after the
-        // shares are burned and after the assets are transferred, which is a valid state.
-        _burn(owner, shares);
-        SafeERC20.safeTransfer(IERC20(asset()), receiver, assets);
-
-        emit Withdraw(caller, receiver, owner, assets, shares);
+    function previewRedeem(uint256 _shares) public view virtual override returns (uint256 assets) {
+        assets = getAmountByVestingDuration(_shares, 0);
     }
 
     /// @notice This would make the amount that the user would need to "gift" the market in order to significantly
@@ -139,9 +55,13 @@ contract XSiloToken is ERC4626, XRedeemPolicy {
         return ERC20._burn(_owner, _shares);
     }
 
-    function _transferShares(address _from, address _to, uint256 _value) internal virtual override {
-        return ERC20._transfer(_from, _to, _value);
-    }
+//    function _transferShares(address _from, address _to, uint256 _value) internal virtual override {
+//        return ERC20._transfer(_from, _to, _value);
+//    }
+
+//    function _redeemShares(uint256 _shares, address _receiver, address _owner) internal virtual override {
+//        return ERC4626.redeem(_from, _to, _value);
+//    }
 
     function _update(address _from, address _to, uint256 _value) internal virtual override {
         STREAM.claimRewards();
