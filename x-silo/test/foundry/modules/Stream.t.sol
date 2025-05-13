3ef7c1e12adf79ad0e5dfce2b14dba0fb483b490
Neo Racer <0xNeoRacer@protonmail.com>
2025-05-13 13:35:06 +0200

    3 variation of tests for claim == pending

--- x-silo/test/foundry/modules/Stream.t.sol
+++ x-silo/test/foundry/modules/Stream.t.sol
@@ -3,6 +3,9 @@ pragma solidity 0.8.28;
 
 import {Test} from "forge-std/Test.sol";
 
+import {ERC4626} from "openzeppelin5/token/ERC20/extensions/ERC4626.sol";
+
+import {ERC4626Mock} from "openzeppelin5/mocks/token/ERC4626Mock.sol";
 import {ERC20Mock} from "openzeppelin5/mocks/token/ERC20Mock.sol";
 import {Stream} from "../../../contracts/modules/Stream.sol";
 
@@ -12,11 +15,12 @@ FOUNDRY_PROFILE=x_silo forge test -vv --ffi --mc StreamTest
 contract StreamTest is Test {
     ERC20Mock token;
     Stream stream;
-    address beneficiary = makeAddr("beneficiary");
+    ERC4626 beneficiary;
 
     function setUp() public {
         token = new ERC20Mock();
-        stream = new Stream(address(this), beneficiary);
+        beneficiary = new ERC4626Mock(address(token));
+        stream = new Stream(address(this), address(beneficiary));
 
         _assert_zeros();
     }
@@ -47,7 +51,7 @@ contract StreamTest is Test {
 
         token.mint(address(stream), 50);
         assertEq(stream.claimRewards(), 50, "claimRewards");
-        assertEq(token.balanceOf(beneficiary), 50, "beneficiary got rewards");
+        assertEq(token.balanceOf(address(beneficiary)), 50, "beneficiary got rewards");
 
         // much over the distribution time
         vm.warp(block.timestamp + 3 days);
@@ -57,7 +61,7 @@ contract StreamTest is Test {
 
         token.mint(address(stream), 50);
         assertEq(stream.claimRewards(), 50, "claimRewards");
-        assertEq(token.balanceOf(beneficiary), 100, "beneficiary got 100% rewards");
+        assertEq(token.balanceOf(address(beneficiary)), 100, "beneficiary got 100% rewards");
     }
 
     /*
@@ -73,7 +77,44 @@ contract StreamTest is Test {
 
         assertEq(stream.pendingRewards(), stream.claimRewards(), "#1 pendingRewards must match claim");
 
-        for (uint i = block.timestamp + 1; i < 1 hours; i += 3) {
+        for (uint i = block.timestamp + 1; i < 3 minutes; i += 3) {
+            vm.warp(i);
+            assertEq(stream.pendingRewards(), stream.claimRewards(), "#2 pendingRewards must match claim");
+        }
+    }
+
+    /*
+    FOUNDRY_PROFILE=x_silo forge test -vv --ffi --mt test_pendingRewardsMustMatchClaim_warpLoop_withFounding_fuzz
+    */
+    /// forge-config: x_silo.fuzz.runs = 10000
+    function test_pendingRewardsMustMatchClaim_warpLoop_withFounding_fuzz(uint32 _emissionPerSecond, uint64 _distributionEnd) public {
+        vm.assume(_distributionEnd > 0);
+
+        stream.setEmissions(_emissionPerSecond, block.timestamp + _distributionEnd);
+
+        token.mint(address(stream), stream.fundingGap());
+
+        assertEq(stream.pendingRewards(), stream.claimRewards(), "#1 pendingRewards must match claim");
+
+        for (uint i = block.timestamp + 1; i < 3 minutes; i += 3) {
+            vm.warp(i);
+            token.mint(address(stream), 1e18);
+            assertEq(stream.pendingRewards(), stream.claimRewards(), "#2 pendingRewards must match claim");
+        }
+    }
+
+    /*
+    FOUNDRY_PROFILE=x_silo forge test -vv --ffi --mt test_pendingRewardsMustMatchClaim_warpLoop_withFounding_fuzz
+    */
+    /// forge-config: x_silo.fuzz.runs = 10000
+    function test_pendingRewardsMustMatchClaim_warpLoop_noFounding_fuzz(uint32 _emissionPerSecond, uint64 _distributionEnd) public {
+        vm.assume(_distributionEnd > 0);
+
+        stream.setEmissions(_emissionPerSecond, block.timestamp + _distributionEnd);
+
+        assertEq(stream.pendingRewards(), stream.claimRewards(), "#1 pendingRewards must match claim");
+
+        for (uint i = block.timestamp + 1; i < 3 minutes; i += 3) {
             vm.warp(i);
             assertEq(stream.pendingRewards(), stream.claimRewards(), "#2 pendingRewards must match claim");
         }
