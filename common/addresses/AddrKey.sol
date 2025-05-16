318dbcd3446220000787f8f8c24773a7a6ab17c5
IhorSF <102213749+IhorSF@users.noreply.github.com>
2025-05-16 11:52:32 +0400

    x-silo: integration test (#1265)

--- common/addresses/AddrKey.sol
+++ common/addresses/AddrKey.sol
@@ -7,6 +7,7 @@ library AddrKey {
     string constant public UNISWAP_ROUTER = "UNISWAP_ROUTER";
     string constant public UNISWAP_FACTORY = "UNISWAP_FACTORY";
     string constant public USDC = "USDC";
+    string constant public USDC_E = "USDC.e";
     string constant public USDC_ETH_UNI_POOL = "USDC_ETH_UNI_POOL";
     string constant public SNX_USDC_UNIV3_POOL = "SNX_USDC_UNIV3_POOL";
     string constant public SNX = "SNX";
