eaf141f88071a2c6c94c31facdbc497e3bf9bd04
Neo Racer <0xNeoRacer@protonmail.com>
2025-05-16 15:59:08 +0200

    /// @inheritdoc IXRedeemPolicy

--- x-silo/contracts/modules/XRedeemPolicy.sol
+++ x-silo/contracts/modules/XRedeemPolicy.sol
@@ -13,30 +13,21 @@ import {IXRedeemPolicy} from "../interfaces/IXRedeemPolicy.sol";
 abstract contract XRedeemPolicy is IXRedeemPolicy, Ownable2Step, TransientReentrancy {
     uint256 internal constant _PRECISION = 100;
 
-    /// @dev constant used to require redeem ratio to not be more than 100%, 100 == 100%
+    /// @inheritdoc IXRedeemPolicy
     uint256 public constant MAX_FIXED_RATIO = _PRECISION; // 100%
 
     // Redeeming min/max settings are updatable at any time by owner
 
-    /// @dev `minRedeemRatio` together with `maxRedeemRatio` is used to create range of ratios
-    /// based on which redeem amount is calculated, value is in 2 decimals, 100 == 1.0, eg 50 means ratio of 1:0.5
+    /// @inheritdoc IXRedeemPolicy
     uint256 public minRedeemRatio = 0.5e2; // 1:0.5
 
-    /// @dev `minRedeemRatio` together with `maxRedeemRatio` is used to create range of ratios
-    /// based on which redeem amount is calculated, value is in 2 decimals, 100 == 1.0, eg 100 means ratio of 1:1
+    /// @inheritdoc IXRedeemPolicy
     uint256 public maxRedeemRatio = 1e2; // 1:1
 
-    /// @dev `minRedeemDuration` together with `maxRedeemDuration` is used to create range of durations
-    /// based on which redeem amount is calculated, value is in seconds.
-    /// Eg if set to 2 days, redeem attempt for less duration will be reverted and preview method for lower duration
-    /// will return 0. `minRedeemDuration` can be set to 0, in that case immediate redeem will be possible but it will
-    /// generate loss.
+    /// @inheritdoc IXRedeemPolicy
     uint256 public minRedeemDuration = 0 days;
 
-    /// @dev `minRedeemDuration` together with `maxRedeemDuration` is used to create range of durations
-    /// based on which redeem amount is calculated, value is in seconds.
-    /// Eg if set to 10 days, redeem attempt for less duration will calculate amount based on range, and anything above
-    /// will result in 100% of tokens.
+    /// @inheritdoc IXRedeemPolicy
     uint256 public maxRedeemDuration = 6 * 30 days; // 6 months
 
     mapping(address => RedeemInfo[]) private _userRedeems;
@@ -69,8 +60,7 @@ abstract contract XRedeemPolicy is IXRedeemPolicy, Ownable2Step, TransientReentr
         emit UpdateRedeemSettings(_minRedeemRatio, _maxRedeemRatio, _minRedeemDuration, _maxRedeemDuration);
     }
 
-    /// @notice on redeem, `_xSiloAmount` of shares are burned, so it is no longer available
-    /// when cancel, `_xSiloAmount` of shares will be minted back
+    /// @inheritdoc IXRedeemPolicy
     function redeemSilo(uint256 _xSiloAmountToBurn, uint256 _duration)
         external
         virtual
@@ -182,9 +172,7 @@ abstract contract XRedeemPolicy is IXRedeemPolicy, Ownable2Step, TransientReentr
         );
     }
 
-    /// @param _xSiloAmount xSilo amount to redeem for Silo
-    /// @param _duration duration in seconds after which redeem happen
-    /// @return siloAmountAfterVesting Silo amount user will get after duration
+    /// @inheritdoc IXRedeemPolicy
     function getAmountByVestingDuration(uint256 _xSiloAmount, uint256 _duration)
         public
         view
@@ -195,9 +183,7 @@ abstract contract XRedeemPolicy is IXRedeemPolicy, Ownable2Step, TransientReentr
         siloAmountAfterVesting = convertToAssets(xSiloAfterVesting);
     }
 
-    /// @param _xSiloAmount xSilo amount to use for vesting
-    /// @param _duration duration in seconds
-    /// @return xSiloAfterVesting xSilo amount will be used for redeem after vesting
+    /// @inheritdoc IXRedeemPolicy
     function getXAmountByVestingDuration(uint256 _xSiloAmount, uint256 _duration)
         public
         view
@@ -214,10 +200,7 @@ abstract contract XRedeemPolicy is IXRedeemPolicy, Ownable2Step, TransientReentr
         xSiloAfterVesting = Math.mulDiv(_xSiloAmount, ratio, _PRECISION, Math.Rounding.Floor);
     }
 
-    /// @dev reversed method for getXAmountByVestingDuration
-    /// @param _xSiloAfterVesting amount after vesting
-    /// @param _duration duration in seconds
-    /// @return xSiloAmountIn xSilo amount user will spend to get `_xSiloAfterVesting`
+    /// @inheritdoc IXRedeemPolicy
     function getAmountInByVestingDuration(uint256 _xSiloAfterVesting, uint256 _duration)
         public
         view
