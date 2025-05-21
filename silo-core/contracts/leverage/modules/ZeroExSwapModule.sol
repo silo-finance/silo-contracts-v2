3f5165b49f3e9dd142c23737f60875a4138ce9a8
Neo Racer <0xNeoRacer@protonmail.com>
2025-05-21 11:12:08 +0200

    wip

--- silo-core/contracts/leverage/modules/ZeroExSwapModule.sol
+++ silo-core/contracts/leverage/modules/ZeroExSwapModule.sol
@@ -5,28 +5,13 @@ import {SafeERC20} from "openzeppelin5/token/ERC20/utils/SafeERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import {RevertLib} from "../../lib/RevertLib.sol";
+import {IZeroExSwapModule} from "../interfaces/IZeroExSwapModule.sol";
 
 /// @dev Based on demo contract that swaps its ERC20 balance for another ERC20.
 /// demo source: https://github.com/0xProject/0x-api-starter-guide-code/blob/master/contracts/SimpleTokenSwap.sol
 /// compatible with 0x and ODOS
-contract ZeroExSwapModule {
+contract ZeroExSwapModule is IZeroExSwapModule {
     using SafeERC20 for IERC20;
-
-    /// @notice data for exchange proxy to perform the swap
-    /// @param sellToken The `sellTokenAddress` field from the API response.
-    /// @param buyToken The `buyTokenAddress` field from the API response.
-    /// @param allowanceTarget The `allowanceTarget` (spender) field from the API response.
-    /// @param swapCallData The `data` field from the API response.
-    struct SwapArgs {
-        address exchangeProxy;
-        address sellToken;
-        address buyToken;
-        address allowanceTarget;
-        bytes swapCallData;
-    }
-
-    error ExchangeAddressZero();
-    error SwapCallFailed();
 
     /// @dev Swaps ERC20->ERC20 tokens held by this contract using a API quote.
     /// Must attach ETH equal to the `value` field from the API response.
@@ -48,6 +33,7 @@ contract ZeroExSwapModule {
         (bool success, bytes memory data) = _swapArgs.exchangeProxy.call(_swapArgs.swapCallData);
         if (!success) RevertLib.revertBytes(data, SwapCallFailed.selector);
 
+        // approval 1 is on purpose, this will cover lal weird scenarios
         IERC20(_swapArgs.sellToken).forceApprove(_swapArgs.allowanceTarget, 1);
 
         uint256 balanceAfter = IERC20(_swapArgs.buyToken).balanceOf(address(this));
