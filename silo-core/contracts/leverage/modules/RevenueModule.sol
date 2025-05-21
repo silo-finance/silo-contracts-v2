55a9ff717dd41634c367b983c2b63b12125fc2b0
Neo Racer <0xNeoRacer@protonmail.com>
2025-05-21 13:50:53 +0200

    ai natspec

--- silo-core/contracts/leverage/modules/RevenueModule.sol
+++ silo-core/contracts/leverage/modules/RevenueModule.sol
@@ -6,26 +6,54 @@ import {Ownable2Step, Ownable} from "openzeppelin5/access/Ownable2Step.sol";
 import {SafeERC20} from "openzeppelin5/token/ERC20/utils/SafeERC20.sol";
 import {Math} from "openzeppelin5/utils/math/Math.sol";
 
+/// @title Revenue Module for Leverage Operations
+/// @notice This contract collects and distributes revenue from leveraged operations.
 abstract contract RevenueModule is Ownable2Step {
     using SafeERC20 for IERC20;
 
+    /// @notice Fee base constant (1e18 represents 100%)
     uint256 public constant FEE_DECIMALS = 1e18;
 
+    /// @notice The leverage fee expressed as a fraction of 1e18
     uint256 public leverageFee;
 
+    /// @notice Address where collected fees are sent
     address public revenueReceiver;
 
+    /// @notice Emitted when the leverage fee is updated
+    /// @param leverageFee New leverage fee
     event LeverageFeeChanged(uint256 leverageFee);
+
+    /// @notice Emitted when the revenue receiver address is changed
+    /// @param receiver New receiver address
     event RevenueReceiverChanged(address indexed receiver);
+
+    /// @notice Emitted when leverage revenue is withdrawn
+    /// @param token Address of the token
+    /// @param revenue Amount withdrawn
+    /// @param receiver Address that received the funds
     event LeverageRevenue(address indexed token, uint256 revenue, address indexed receiver);
 
+    /// @dev Thrown when trying to set the same fee as the current one
     error FeeDidNotChanged();
+
+    /// @dev Thrown when trying to set the same revenue receiver
     error ReceiverDidNotChanged();
+
+    /// @dev Thrown when the receiver address is zero
     error ReceiverZero();
+
+    /// @dev Thrown when the provided fee is invalid (>= 100%)
     error InvalidFee();
+
+    /// @dev Thrown when there is no revenue to withdraw
     error NoRevenue();
+
+    /// @dev Thrown when revenue receiver is not set
     error ReceiverNotSet();
 
+    /// @notice Set the leverage fee
+    /// @param _fee New leverage fee (must be < FEE_DECIMALS)
     function setLeverageFee(uint256 _fee) external onlyOwner {
         require(leverageFee != _fee, FeeDidNotChanged());
         require(_fee < FEE_DECIMALS, InvalidFee());
@@ -34,6 +62,8 @@ abstract contract RevenueModule is Ownable2Step {
         emit LeverageFeeChanged(_fee);
     }
 
+    /// @notice Set the address that receives collected revenue
+    /// @param _receiver New address to receive fees
     function setRevenueReceiver(address _receiver) external onlyOwner {
         require(revenueReceiver != _receiver, ReceiverDidNotChanged());
         require(_receiver != address(0), ReceiverZero());
@@ -42,12 +72,16 @@ abstract contract RevenueModule is Ownable2Step {
         emit RevenueReceiverChanged(_receiver);
     }
 
+    /// @notice Withdraw revenue for multiple tokens
+    /// @param _tokens List of tokens to withdraw revenue for
     function withdrawRevenues(IERC20[] calldata _tokens) external {
         for (uint256 i; i < _tokens.length; i++) {
             withdrawRevenue(_tokens[i]);
         }
     }
 
+    /// @notice Withdraw revenue for a specific token
+    /// @param _token ERC20 token to withdraw
     function withdrawRevenue(IERC20 _token) public {
         uint256 balance = _token.balanceOf(address(this));
         require(balance != 0, NoRevenue());
@@ -59,6 +93,10 @@ abstract contract RevenueModule is Ownable2Step {
         emit LeverageRevenue(address(_token), balance, receiver);
     }
 
+    /// @notice Calculates the leverage fee for a given amount
+    /// @dev Will always return at least 1 if fee > 0 and calculation rounds down
+    /// @param _amount The amount to calculate the fee for
+    /// @return leverageFeeAmount The calculated fee amount
     function _calculateLeverageFee(uint256 _amount) internal virtual returns (uint256 leverageFeeAmount) {
         uint256 fee = leverageFee;
         if (fee == 0) return 0;
@@ -67,6 +105,10 @@ abstract contract RevenueModule is Ownable2Step {
         if (leverageFeeAmount == 0) leverageFeeAmount = 1;
     }
 
+    /// @notice Transfers the leverage fee from the deposit to the revenue receiver
+    /// @param _token The ERC20 token used for payment
+    /// @param _totalDeposit Total amount deposited
+    /// @return leverageFeeAmount The amount of fee transferred
     function _transferFee(IERC20 _token, uint256 _totalDeposit) internal virtual returns (uint256 leverageFeeAmount) {
         leverageFeeAmount = _calculateLeverageFee(_totalDeposit);
         if (leverageFeeAmount == 0) return 0;
