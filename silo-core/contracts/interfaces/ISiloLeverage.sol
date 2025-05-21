50ed2b456d1180e11149a59668e0831c5684e26e
Neo Racer <0xNeoRacer@protonmail.com>
2025-05-21 11:12:08 +0200

    wip

--- silo-core/contracts/interfaces/ISiloLeverage.sol
+++ silo-core/contracts/interfaces/ISiloLeverage.sol
@@ -12,20 +12,43 @@ import {ISiloConfig} from "./ISiloConfig.sol";
 import {ISiloFactory} from "./ISiloFactory.sol";
 import {ISiloOracle} from "./ISiloOracle.sol";
 
+import {IZeroExSwapModule} from "../interfaces/IZeroExSwapModule.sol";
+
 interface ISiloLeverage {
-    /// @param _silo Silo address on which we doing leverage
-    /// @param _deposit deposit amount that user actually do
-    /// @param _collateralType collateral type
-    /// @param _multiplier leverage multiplier in 18 decimals, eg x1 == 1e18
-    /// @param _flashDebtLender source for flashloan
-    /// @param _flashBorrow amount of debt token that will be flashloan and swap for collateral
+    /// @param flashDebtLender address from where contract will get flashloan
+    /// @param token token address
+    /// @param amount flash amount
+    struct FlashArgs {
+        address flashDebtLender;
+        address token;
+        uint256 amount;
+    }
+
+    /// @param leverageAmount total deposit amount (sum of user deposit amount + flashloan amount)
+    struct DepositArgs {
+        ISilo silo;
+        uint256 leverageAmount;
+        ISilo.CollateralType collateralType;
+        address receiver;
+    }
+
+    /// @param amount amount to borrow, should be equal to flashloan amount + flashloan fee + leverage fee
+    struct BorrowArgs {
+        ISilo silo;
+        uint256 amount;
+        address receiver;
+    }
+
+    error FlashloanFailed();
+    error InvalidFlashloanLender();
+
+    /// @dev It can revert when:
+    /// - amount is so hi we can not calculate fee
     function leverage(
-        ISilo _silo,
-        uint256 _deposit,
-        ISilo.CollateralType _collateralType,
-        uint64 _multiplier,
-        IERC3156FlashLender _flashDebtLender,
-        uint256 _flashBorrow
+        FlashArgs calldata _flashArgs,
+        IZeroExSwapModule.SwapArgs calldata _swapArgs,
+        DepositArgs calldata _depositArgs,
+        ISilo _borrowSilo
     ) external;
 
     /// @param _silo Silo address on which we doing leverage
@@ -35,7 +58,7 @@ interface ISiloLeverage {
     /// @param _debtFlashloan amount of debt token that will be flashloaned
     /// debt Flashloan amount should be calculated in this way: quote(deposit * leverage)
     /// Swap fee and slippage are not considered. That means actual result will be UP TO provided leverage (usually less)
-     /// @return flashLoanAmount flashloan amount that is required for leverage
+    /// @return flashLoanAmount flashloan amount that is required for leverage
     /// @return debtPreview amount of debt that leverage will create
     /// Borrow amount must be enough to:
     /// - cover leverage fee (fee is in debt token)
--- silo-core/contracts/leverage/modules/ErrorsLib.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.28;
-
-library ErrorsLib {
-    error FlashloanFailed();
-    error InvalidFlashloanLender();
-}
