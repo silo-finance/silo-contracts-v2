1980a3e997f0f429f25ca474078d0a01bee90fd1
Neo Racer <0xNeoRacer@protonmail.com>
2025-05-21 11:57:24 +0200

    remove ipreview

--- silo-core/contracts/interfaces/ISiloLeverage.sol
+++ silo-core/contracts/interfaces/ISiloLeverage.sol
@@ -51,30 +51,6 @@ interface ISiloLeverage {
         ISilo _borrowSilo
     ) external;
 
-    /// @param _silo Silo address on which we doing leverage
-    /// @param _deposit deposit amount that user actually do
-    /// @param _multiplier leverage multiplier in 18 decimals, eg x1 == 1e18
-    /// @param _flashDebtLender source for flashloan of debt token
-    /// @param _debtFlashloan amount of debt token that will be flashloaned
-    /// debt Flashloan amount should be calculated in this way: quote(deposit * leverage)
-    /// Swap fee and slippage are not considered. That means actual result will be UP TO provided leverage (usually less)
-    /// @return flashLoanAmount flashloan amount that is required for leverage
-    /// @return debtPreview amount of debt that leverage will create
-    /// Borrow amount must be enough to:
-    /// - cover leverage fee (fee is in debt token)
-    /// - cover flashloan repay + flashloan fee
-    /// @return finalMultiplier final multiplier of leverage (might be different from input _multiplier)
-    function previewLeverage(
-        ISilo _silo,
-        uint256 _deposit,
-        uint64 _multiplier,
-        IERC3156FlashLender _flashDebtLender,
-        uint256 _debtFlashloan
-    ) external view returns (
-        uint256 flashLoanAmount,
-        uint256 debtPreview,
-        uint64 finalMultiplier
-    );
 //
 //    function closeLeverage(
 //        ISilo _silo,
