316a2c0cf612f96266197e20748d5af685c2b650
Neo Racer <0xNeoRacer@protonmail.com>
2025-05-21 13:08:25 +0200

    qa

--- silo-core/test/foundry/leverage/SiloLeverage.t.sol
+++ silo-core/test/foundry/leverage/SiloLeverage.t.sol
@@ -5,41 +5,48 @@ import {Test} from "forge-std/Test.sol";
 
 import {IERC20} from "openzeppelin5/token/ERC20/IERC20.sol";
 
+import {IERC20R} from "silo-core/contracts/interfaces/IERC20R.sol";
 import {ISiloConfig} from "silo-core/contracts/interfaces/ISiloConfig.sol";
 import {IZeroExSwapModule} from "silo-core/contracts/interfaces/IZeroExSwapModule.sol";
 import {ISilo} from "silo-core/contracts/interfaces/ISilo.sol";
 import {SiloLeverage, ISiloLeverage} from "silo-core/contracts/leverage/SiloLeverage.sol";
 
-import {SiloLittleHelper} from "../../_common/SiloLittleHelper.sol";
+import {SiloLittleHelper} from "../_common/SiloLittleHelper.sol";
+import {SwapRouterMock} from "./mocks/SwapRouterMock.sol";
 
 /*
     FOUNDRY_PROFILE=core_test  forge test -vv --ffi --mc SiloLeverageTest
 */
 contract SiloLeverageTest is SiloLittleHelper, Test {
-    using SiloLensLib for ISilo;
-    using ShareTokenDecimalsPowLib for uint256;
+    ISiloConfig cfg;
 
-    ISiloConfig siloConfig;
+    address debtShareToken;
 
     function setUp() public {
-        siloConfig = _setUpLocalFixture();
+        cfg = _setUpLocalFixture();
 
-        assertTrue(siloConfig.getConfig(address(silo0)).maxLtv != 0, "we need borrow to be allowed");
+        token0.setOnDemand(true);
+        token1.setOnDemand(true);
+
+        _deposit(1000e18, address(1));
+        _depositForBorrow(2000e18, address(2));
+
+        (,, debtShareToken) = cfg.getShareTokens(address(silo1));
     }
 
     /*
     FOUNDRY_PROFILE=core_test forge test -vv --ffi --mt test_leverage_happyPath
     */
     function test_leverage_happyPath() public {
-        token0.setOnDemand(true);
-        token1.setOnDemand(true);
-
         address user = makeAddr("user");
+        uint256 depositAmount = 0.1e18;
 
         SiloLeverage siloLeverage = new SiloLeverage(address(this));
+        SwapRouterMock swap = new SwapRouterMock();
+        swap.setSwap(token1, 1e18, token0, 1.3e18);
 
         ISiloLeverage.FlashArgs memory flashArgs = ISiloLeverage.FlashArgs({
-            amount: 1e18,
+            amount: depositAmount * 10,
             token: silo1.asset(),
             flashDebtLender: address(silo1)
         });
@@ -48,12 +55,12 @@ contract SiloLeverageTest is SiloLittleHelper, Test {
             buyToken: address(silo0.asset()),
             sellToken: address(silo1.asset()),
             allowanceTarget: address(1),
-            exchangeProxy: address(2),
-            swapCallData: ""
+            exchangeProxy: address(swap),
+            swapCallData: "mocked swap"
         });
 
         ISiloLeverage.DepositArgs memory depositArgs = ISiloLeverage.DepositArgs({
-            amount: 0.1e18,
+            amount: depositAmount,
             receiver: user,
             collateralType: ISilo.CollateralType.Collateral,
             silo: silo0
@@ -61,6 +68,15 @@ contract SiloLeverageTest is SiloLittleHelper, Test {
 
         ISilo borrowSilo = silo1;
 
+        vm.startPrank(user);
+
+        IERC20R(debtShareToken).setReceiveApproval(address(siloLeverage), 2e18);
         siloLeverage.leverage(flashArgs, swapArgs, depositArgs, borrowSilo);
+        IERC20R(debtShareToken).setReceiveApproval(address(siloLeverage), 0);
+
+        vm.stopPrank();
+
+        assertEq(silo0.previewRedeem(silo0.balanceOf(user)), 1.4e18, "user got deposit x 10");
+        assertEq(silo1.maxRepay(user), 1.01e18, "user has debt equal to flashloan + fees");
     }
 }
