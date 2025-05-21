e357a041845c75b90fb88052a252e9c74ca4db49
Neo Racer <0xNeoRacer@protonmail.com>
2025-05-21 13:50:53 +0200

    ai natspec

--- silo-core/contracts/interfaces/ISiloLeverage.sol
+++ silo-core/contracts/interfaces/ISiloLeverage.sol
@@ -14,17 +14,25 @@ import {ISiloOracle} from "./ISiloOracle.sol";
 
 import {IZeroExSwapModule} from "../interfaces/IZeroExSwapModule.sol";
 
+/// @title ISiloLeverage Interface
+/// @notice Interface for a contract that enables leveraged deposits using flash loans and token swaps
 interface ISiloLeverage {
-    /// @param flashDebtLender address from where contract will get flashloan
-    /// @param token token address
-    /// @param amount flash amount
+
+    /// @notice Parameters for a flash loan
+    /// @param flashDebtLender The address of the lender providing the flash loan
+    /// @param token The token to borrow
+    /// @param amount The amount of tokens to borrow
     struct FlashArgs {
         address flashDebtLender;
         address token;
         uint256 amount;
     }
 
-    /// @param amount raw deposit amount (without flashloan amount)
+    /// @notice Parameters for deposit after leverage
+    /// @param silo Target Silo for depositing
+    /// @param amount Raw deposit amount (excluding flashloan)
+    /// @param collateralType The type of collateral to use
+    /// @param receiver Address to receive the leveraged position
     struct DepositArgs {
         ISilo silo;
         uint256 amount;
@@ -32,11 +40,19 @@ interface ISiloLeverage {
         address receiver;
     }
 
+    /// @notice Thrown when the flash loan fails to execute
     error FlashloanFailed();
+
+    /// @notice Thrown if the provided flash loan lender is invalid or unsupported
     error InvalidFlashloanLender();
 
-    /// @dev It can revert when:
-    /// - amount is so hi we can not calculate fee
+    /// @notice Performs leverage operation using a flash loan and token swap
+    /// @dev Reverts if the amount is so high that fee calculation fails
+    /// @param _flashArgs Flash loan configuration
+    /// @param _swapArgs Swap call data and settings
+    /// @param _depositArgs Final deposit configuration into a Silo
+    /// @param _borrowSilo The Silo to borrow from
+    /// @return multiplier Leverage multiplier achieved by the operation
     function leverage(
         FlashArgs calldata _flashArgs,
         IZeroExSwapModule.SwapArgs calldata _swapArgs,
--- silo-core/contracts/leverage/modules/ZeroExSwapModule.sol
+++ silo-core/contracts/leverage/modules/ZeroExSwapModule.sol
@@ -7,28 +7,31 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {RevertLib} from "../../lib/RevertLib.sol";
 import {IZeroExSwapModule} from "../../interfaces/IZeroExSwapModule.sol";
 
-/// @dev Based on demo contract that swaps its ERC20 balance for another ERC20.
-/// demo source: https://github.com/0xProject/0x-api-starter-guide-code/blob/master/contracts/SimpleTokenSwap.sol
-/// compatible with 0x and ODOS
+/// @title 0x and ODOS Compatible ERC20 Swap Module
+/// @notice Enables ERC20 token swaps via an external exchange proxy (e.g., 0x, ODOS)
+/// @dev Based on the 0x demo contract: https://github.com/0xProject/0x-api-starter-guide-code/blob/master/contracts/SimpleTokenSwap.sol
 contract ZeroExSwapModule is IZeroExSwapModule {
     using SafeERC20 for IERC20;
 
-    /// @dev Swaps ERC20->ERC20 tokens held by this contract using a API quote.
-    /// Must attach ETH equal to the `value` field from the API response.
-    /// @param _swapArgs see SwapArgs for details
+    /// @notice Executes a token swap using a prebuilt swap quote
+    /// @dev The contract must hold the sell token balance before calling. Requires ETH attached equal to the swap quote value.
+    /// @param _swapArgs Struct containing all parameters for executing a swap
+    /// @param _approval Amount of sell token to approve before the swap
+    /// @return amountOut Amount of buy token received after the swap
     function _fillQuote(SwapArgs memory _swapArgs, uint256 _approval) internal virtual returns (uint256 amountOut) {
         if (_swapArgs.exchangeProxy == address(0)) revert ExchangeAddressZero();
 
         uint256 balanceBefore = IERC20(_swapArgs.buyToken).balanceOf(address(this));
 
+        // Approve token for spending by the exchange
         IERC20(_swapArgs.sellToken).forceApprove(_swapArgs.allowanceTarget, _approval);
 
-        // Call the encoded swap function call on the contract at `swapTarget`
         // solhint-disable-next-line avoid-low-level-calls
+        // Perform low-level call to external exchange proxy
         (bool success, bytes memory data) = _swapArgs.exchangeProxy.call(_swapArgs.swapCallData);
         if (!success) RevertLib.revertBytes(data, SwapCallFailed.selector);
 
-        // approval 1 is on purpose, this will cover lal weird scenarios
+        // Reset approval to 1 to avoid lingering allowances
         IERC20(_swapArgs.sellToken).forceApprove(_swapArgs.allowanceTarget, 1);
 
         uint256 balanceAfter = IERC20(_swapArgs.buyToken).balanceOf(address(this));
