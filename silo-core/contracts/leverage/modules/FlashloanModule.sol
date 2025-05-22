b44d6e7b9813a5aa43a0ee41b7637b5f70e76c31
Neo Racer <0xNeoRacer@protonmail.com>
2025-05-22 20:28:18 +0200

    FlashloanModule

---
+++ silo-core/contracts/leverage/modules/FlashloanModule.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.28;
+
+
+import {ISiloLeverage} from "../../interfaces/ISiloLeverage.sol";
+import {IERC3156FlashBorrower} from "../../interfaces/IERC3156FlashBorrower.sol";
+import {IERC3156FlashLender} from "../../interfaces/IERC3156FlashLender.sol";
+
+abstract contract FlashloanModule is IERC3156FlashBorrower {
+    // TODO transient
+    ISiloLeverage.LeverageAction internal __action;
+    address internal __flashloanTarget;
+
+    bytes32 internal constant _FLASHLOAN_CALLBACK = keccak256("ERC3156FlashBorrower.onFlashLoan");
+
+    function _executeFlashloan(ISiloLeverage.FlashArgs memory _flashArgs, bytes memory _data) internal virtual {
+        require(IERC3156FlashLender(_flashArgs.flashloanTarget).flashLoan({
+            _receiver: this,
+            _token: _flashArgs.token,
+            _amount: _flashArgs.amount,
+            _data: _data
+        }), ISiloLeverage.FlashloanFailed());
+    }
+
+    function onFlashLoan(
+        address _initiator,
+        address _borrowToken,
+        uint256 _flashloanAmount,
+        uint256 _flashloanFee,
+        bytes calldata _data
+    )
+        external
+        returns (bytes32)
+    {
+        // this check prevents call `onFlashLoan` directly
+        require(__flashloanTarget == msg.sender, ISiloLeverage.InvalidFlashloanLender());
+
+        // TODO: _initiator check might be redundant, because of how `__flashloanTarget` works, but atm I see no harm to check it
+        require(_initiator == address(this), ISiloLeverage.InvalidInitiator());
+
+        if (__action == ISiloLeverage.LeverageAction.Open) {
+            _openLeverage(_borrowToken, _flashloanAmount, _flashloanFee, _data);
+        } else if (__action == ISiloLeverage.LeverageAction.Close) {
+            _closeLeverage(_borrowToken, _flashloanAmount, _flashloanFee, _data);
+        } else revert ISiloLeverage.UnknownAction();
+
+        return _FLASHLOAN_CALLBACK;
+    }
+
+    function _openLeverage(
+        address _borrowToken,
+        uint256 _flashloanAmount,
+        uint256 _flashloanFee,
+        bytes calldata _data
+    ) internal virtual;
+
+    function _closeLeverage(
+        address _debtToken,
+        uint256 _flashloanAmount,
+        uint256 _flashloanFee,
+        bytes calldata _data
+    ) internal virtual;
+}
