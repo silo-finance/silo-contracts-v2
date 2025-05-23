047e2031ff4a3158592baffcf616235ac684f2fa
Neo Racer <0xNeoRacer@protonmail.com>
2025-05-23 19:48:58 +0200

    reentrancy params

--- silo-core/contracts/leverage/modules/LeverageReentrancy.sol
+++ silo-core/contracts/leverage/modules/LeverageReentrancy.sol
@@ -2,31 +2,34 @@
 pragma solidity ^0.8.10;
 
 import {ISiloLeverageZeroEx} from "../../interfaces/ISiloLeverageZeroEx.sol";
+import {ISilo} from "../../interfaces/ISilo.sol";
+import {ISiloConfig} from "../../interfaces/ISiloConfig.sol";
 
 contract LeverageReentrancy {
-    address internal  __msgSender;
-    uint256 internal  __totalDeposit;
-    uint256 internal  __totalBorrow;
-    ISiloConfig  __siloConfig;
-    ISiloLeverageZeroEx.LeverageAction  __action;
-    address  __flashloanTarget;
+    address internal transient __msgSender;
+    uint256 internal transient __totalDeposit;
+    uint256 internal transient __totalBorrow;
+    ISiloConfig internal transient __siloConfig;
+    ISiloLeverageZeroEx.LeverageAction internal transient __action;
+    address internal transient __flashloanTarget;
 
-    modifier nonReentrant() { // TODO params?
+    modifier nonReentrant(ISilo _silo, ISiloLeverageZeroEx.LeverageAction _action, address _flashloanTarget) {
         require(__msgSender == address(0), ISiloLeverageZeroEx.Reentrancy());
+        _setTransient(_silo, _action, _flashloanTarget);
 
         _;
 
         _resetTransient();
     }
 
-    function _setTransient(ISilo _silo, ISiloLeverageZeroEx.LeverageAction _action, address _flashloanTarget) internal {
+    function _setTransient(ISilo _silo, ISiloLeverageZeroEx.LeverageAction _action, address _flashloanTarget) private {
         __flashloanTarget = _flashloanTarget;
         __action = _action;
         __msgSender = msg.sender;
         __siloConfig = _silo.config();
     }
 
-    function _resetTransient() internal {
+    function _resetTransient() private {
         __totalDeposit = 0;
         __totalBorrow = 0;
         __flashloanTarget = address(0);
