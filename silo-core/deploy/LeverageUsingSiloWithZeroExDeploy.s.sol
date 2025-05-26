309743e0bd0f1991daea0784b688c9d0b4ab120b
Neo Racer <0xNeoRacer@protonmail.com>
2025-05-26 12:47:21 +0200

    deployment

--- silo-core/deploy/LeverageUsingSiloWithZeroExDeploy.s.sol
+++ silo-core/deploy/LeverageUsingSiloWithZeroExDeploy.s.sol
@@ -6,17 +6,17 @@ import {console2} from "forge-std/console2.sol";
 import {CommonDeploy} from "./_CommonDeploy.sol";
 
 import {SiloCoreContracts} from "silo-core/common/SiloCoreContracts.sol";
-import {SiloLeverage} from "silo-core/contracts/leverage/SiloLeverage.sol";
+import {LeverageUsingSiloWithZeroEx} from "silo-core/contracts/leverage/LeverageUsingSiloWithZeroEx.sol";
 import {ISiloLens} from "silo-core/contracts/interfaces/ISiloLens.sol";
 
 /**
     FOUNDRY_PROFILE=core \
-        forge script silo-core/deploy/SiloLeverageDeploy.s.sol \
+        forge script silo-core/deploy/LeverageUsingSiloWithZeroExDeploy.s.sol \
         --ffi --rpc-url $RPC_SONIC --broadcast --verify
 
     Resume verification:
     FOUNDRY_PROFILE=core \
-        forge script silo-core/deploy/SiloLeverageDeploy.s.sol \
+        forge script silo-core/deploy/LeverageUsingSiloWithZeroExDeploy.s.sol \
         --ffi --rpc-url $RPC_SONIC \
         --verify \
         --verifier blockscout --verifier-url $VERIFIER_URL_INK \
@@ -25,19 +25,19 @@ import {ISiloLens} from "silo-core/contracts/interfaces/ISiloLens.sol";
 
     remember to run `TowerRegistration` script after deployment!
  */
-contract SiloLeverageDeploy is CommonDeploy {
-    function run() public returns (SiloLeverage leverage) {
+contract LeverageUsingSiloWithZeroExDeploy is CommonDeploy {
+    function run() public returns (LeverageUsingSiloWithZeroEx leverage) {
         uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
         address deployer = vm.addr(deployerPrivateKey);
 
         vm.startBroadcast(deployerPrivateKey);
 
-        leverage = new SiloLeverage(deployer);
+        leverage = new LeverageUsingSiloWithZeroEx(deployer);
 
         vm.stopBroadcast();
 
-        console2.log("SiloLeverage redeployed - remember to run `TowerRegistration` script!");
+        console2.log("LeverageUsingSiloWithZeroEx redeployed - remember to run `TowerRegistration` script!");
 
-        _registerDeployment(address(leverage), SiloCoreContracts.SILO_LEVERAGE);
+        _registerDeployment(address(leverage), SiloCoreContracts.SILO_LEVERAGE_USING_SILO_0X);
     }
 }
