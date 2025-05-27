b50b9211da780d4d4442dadeb9fec1870a90326d
Neo Racer <0xNeoRacer@protonmail.com>
2025-05-27 12:40:49 +0200

    .

---
+++ silo-core/contracts/leverage/modules/ZeroExSwapModule.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+import {SafeERC20} from "openzeppelin5/token/ERC20/utils/SafeERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import {RevertLib} from "../../lib/RevertLib.sol";
+import {IZeroExSwapModule} from "../../interfaces/IZeroExSwapModule.sol";
+
+/// @title 0x and ODOS Compatible ERC20 Swap Module
+/// @notice Enables ERC20 token swaps via an external exchange proxy (e.g., 0x, ODOS)
+/// @dev Based on the 0x demo contract: https://github.com/0xProject/0x-api-starter-guide-code/blob/master/contracts/SimpleTokenSwap.sol
+contract ZeroExSwapModule is IZeroExSwapModule {
+    using SafeERC20 for IERC20;
+
+    /// @notice Executes a token swap using a prebuilt swap quote
+    /// @dev The contract must hold the sell token balance before calling.
+    /// @param _swapArgs SwapArgs struct as bytes containing containing all parameters for executing a swap
+    /// @param _approval Amount of sell token to approve before the swap
+    /// @return amountOut Amount of buy token received after the swap including any previous balance that contract has
+    function _fillQuote(bytes memory _swapArgs, uint256 _approval) internal virtual returns (uint256 amountOut) {
+        SwapArgs memory swapArgs = abi.decode(_swapArgs, (SwapArgs));
+
+        if (swapArgs.exchangeProxy == address(0)) revert ExchangeAddressZero();
+
+        // Approve token for spending by the exchange
+        IERC20(swapArgs.sellToken).forceApprove(swapArgs.allowanceTarget, _approval); // TODO max?
+
+        // Perform low-level call to external exchange proxy
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success, bytes memory data) = swapArgs.exchangeProxy.call(swapArgs.swapCallData);
+        if (!success) RevertLib.revertBytes(data, SwapCallFailed.selector);
+
+        // Reset approval to 1 to avoid lingering allowances
+        IERC20(swapArgs.sellToken).forceApprove(swapArgs.allowanceTarget, 1);
+
+        amountOut = IERC20(swapArgs.buyToken).balanceOf(address(this));
+    }
+}
