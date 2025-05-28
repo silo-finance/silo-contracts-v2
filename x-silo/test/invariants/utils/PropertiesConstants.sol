ed1bc9032a10264487821a2e2ffc1783bcd96f1e
Neo Racer <0xNeoRacer@protonmail.com>
2025-05-28 15:07:05 +0200

    wip _setUp();        _setUpActors();

--- x-silo/test/invariants/utils/PropertiesConstants.sol
+++ x-silo/test/invariants/utils/PropertiesConstants.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.19;
 
 abstract contract PropertiesConstants {
     // Constant echidna addresses
-    address constant USER1 = address(0x10000);
-    address constant USER2 = address(0x20000);
-    address constant USER3 = address(0x30000);
+    address constant USER1 = address(0x01111);
+    address constant USER2 = address(0x02222);
+    address constant USER3 = address(0x03333);
     uint256 constant INITIAL_BALANCE = 1000e35;
 
     uint256 internal constant _PRECISION_DECIMALS = 1e18;
