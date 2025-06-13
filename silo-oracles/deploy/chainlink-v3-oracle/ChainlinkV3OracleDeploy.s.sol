bba191777e25f72799a1e47aee162ee24cdcf8c8
Neo Racer <0xNeoRacer@protonmail.com>
2025-06-13 17:27:03 +0200

    silo-core: wstUSR/USDC mainnet market

--- silo-oracles/deploy/chainlink-v3-oracle/ChainlinkV3OracleDeploy.s.sol
+++ silo-oracles/deploy/chainlink-v3-oracle/ChainlinkV3OracleDeploy.s.sol
@@ -15,7 +15,7 @@ import {PriceFormatter} from "silo-core/deploy/lib/PriceFormatter.sol";
 /**
 FOUNDRY_PROFILE=oracles CONFIG=CHAINLINK_scUSD_USDC_USD \
     forge script silo-oracles/deploy/chainlink-v3-oracle/ChainlinkV3OracleDeploy.s.sol \
-    --ffi --rpc-url $RPC_SONIC --broadcast --verify
+    --ffi --rpc-url $RPC_MAINNET --broadcast --verify
  */
 contract ChainlinkV3OracleDeploy is CommonDeploy {
     string public useConfigName;
