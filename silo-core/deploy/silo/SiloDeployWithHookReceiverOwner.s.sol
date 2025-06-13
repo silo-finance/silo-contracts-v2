f2db345ec6144c94bf1943aef5006c4129f516b4
Neo Racer <0xNeoRacer@protonmail.com>
2025-06-13 21:41:45 +0200

    .

--- silo-core/deploy/silo/SiloDeployWithHookReceiverOwner.s.sol
+++ silo-core/deploy/silo/SiloDeployWithHookReceiverOwner.s.sol
@@ -6,9 +6,9 @@ import {AddrLib} from "silo-foundry-utils/lib/AddrLib.sol";
 import {SiloDeploy, ISiloDeployer} from "./SiloDeploy.s.sol";
 
 /**
-FOUNDRY_PROFILE=core CONFIG=Silo_wsrUSD_USDC HOOK_RECEIVER_OWNER=DAO \
+FOUNDRY_PROFILE=core CONFIG=solvBTC.BBN_solvBTC HOOK_RECEIVER_OWNER=DAO \
     forge script silo-core/deploy/silo/SiloDeployWithHookReceiverOwner.s.sol \
-    --ffi --rpc-url $RPC_MAINNET --broadcast --verify
+    --ffi --rpc-url $RPC_SONIC --broadcast --verify
  */
 contract SiloDeployWithHookReceiverOwner is SiloDeploy {
     function _getClonableHookReceiverConfig(address _implementation)
