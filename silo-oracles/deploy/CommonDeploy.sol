641cc713b3f8de0fb29aa047d59d8fd9c81d73ec
Neo Racer <0xNeoRacer@protonmail.com>
2025-06-16 08:15:27 +0200

    .

--- silo-oracles/deploy/CommonDeploy.sol
+++ silo-oracles/deploy/CommonDeploy.sol
@@ -48,9 +48,45 @@ contract CommonDeploy is Deployer {
             out /= 10;
         }
 
-        if (e < 3 || _in < 1e7) return vm.toString(_in);
+        if (e < 3 || _in < 1e6) return string.concat(vm.toString(_in), _digits(_in));
 
-        return string.concat(vm.toString(out), "e", vm.toString(e));
+        return string.concat(vm.toString(out), "e", vm.toString(e), _digits(_in));
+    }
+
+    function _formatPriceInE18(uint256 _in) internal pure returns (string memory) {
+        if (_in < 1e4) return string.concat(vm.toString(_in), _digits(_in));
+        if (_in < 1e7) return _formatNumberInE(_in);
+
+        uint256 integerPart = _in / 1e18;
+        uint256 fractionalPart = _in % 1e18;
+
+        string memory integerStr = vm.toString(integerPart);
+        uint256 leadingZeros = 18 - bytes(vm.toString(fractionalPart)).length;
+
+        while (fractionalPart != 0 && fractionalPart % 10 == 0) {
+            fractionalPart /= 10;
+        }
+
+        string memory fractionalStr = vm.toString(fractionalPart);
+
+        for (uint256 i = 0; i < leadingZeros; i++) {
+            fractionalStr = string.concat("0", fractionalStr);
+        }
+
+        if (integerPart == 0) {
+            return string.concat("0.", fractionalStr, "e18");
+        } if (fractionalPart == 0) {
+            return string.concat(integerStr, "e18");
+        } else {
+            return string.concat(integerStr, ".", fractionalStr, "e18");
+        }
+    }
+
+    function _digits(uint256 _in) internal pure returns (string memory) {
+        uint256 l = bytes(vm.toString(_in)).length;
+        if (l < 6) return "";
+
+        return string.concat(" [", vm.toString(l) ," digits]");
     }
 
     function _printMetadata(address _token) internal view {
