f7dbc64ca20b003569f620406e16136da87756d4
IhorSF <102213749+IhorSF@users.noreply.github.com>
2025-06-19 11:22:32 +0400

    avalanche deployment: silo-core, silo-oracles, silo-vaults (#1400)

--- common/addresses/AddrKey.sol
+++ common/addresses/AddrKey.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.6.2 <0.9.0;
 
 library AddrKey {
     string constant public WETH = "WETH";
+    string constant public WAVAX = "WAVAX";
     string constant public BALANCER_VAULT = "BALANCER_VAULT";
     string constant public UNISWAP_ROUTER = "UNISWAP_ROUTER";
     string constant public UNISWAP_FACTORY = "UNISWAP_FACTORY";
