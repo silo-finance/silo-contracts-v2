3708ab6eed36859680edde5457c663a271de39c9
Neo Racer <0xNeoRacer@protonmail.com>
2025-06-20 14:41:52 +0200

    wip

--- silo-core/deploy/silo/VerifySilo.s.sol
+++ silo-core/deploy/silo/VerifySilo.s.sol
@@ -8,10 +8,10 @@ import {AddrLib} from "silo-foundry-utils/lib/AddrLib.sol";
 import {ISiloConfig} from "silo-core/contracts/interfaces/ISiloConfig.sol";
 
 /**
-FOUNDRY_PROFILE=core CONFIG=0xC1F3d4F5f734d6Dc9E7D4f639EbE489Acd4542ab \
+FOUNDRY_PROFILE=core CONFIG=0xf6F23b56E327f9537621046C1a365A51ED2a0d30 \
     EXTERNAL_PRICE_0=99999 EXTERNAL_PRICE_1=100000 \
     forge script silo-core/deploy/silo/VerifySilo.s.sol \
-    --ffi --rpc-url $RPC_SONIC
+    --ffi --rpc-url $RPC_AVALANCHE
  */
 
 contract VerifySilo is Script, Test {
