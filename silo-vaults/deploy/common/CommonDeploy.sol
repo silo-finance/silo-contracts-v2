327f1ecc781312228e2c0529c0a28f1ddab0e419
Neo Racer <0xNeoRacer@protonmail.com>
2025-07-02 11:22:31 +0200

    Revert "."
    This reverts commit a7df1de2d369986640788a3f2d18773f8be87d78.

--- silo-vaults/deploy/common/CommonDeploy.sol
+++ silo-vaults/deploy/common/CommonDeploy.sol
@@ -8,15 +8,11 @@ import {SiloVaultsDeployments} from "../../common/SiloVaultsContracts.sol";
 contract CommonDeploy is Deployer {
     string internal constant _FORGE_OUT_DIR = "cache/foundry/out/silo-vaults";
 
-    function _contractBaseDir() internal view override virtual returns (string memory baseDir) {
-        baseDir = "";
-    }
-
-    function _forgeOutDir() internal view override virtual returns (string memory) {
+    function _forgeOutDir() internal pure override virtual returns (string memory) {
         return _FORGE_OUT_DIR;
     }
 
-    function _deploymentsSubDir() internal view override virtual returns (string memory) {
+    function _deploymentsSubDir() internal pure override virtual returns (string memory) {
         return SiloVaultsDeployments.DEPLOYMENTS_DIR;
     }
 }
