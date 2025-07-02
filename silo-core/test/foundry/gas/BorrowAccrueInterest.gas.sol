b1b348e9a5c9ccf0192edcc00138f48d0ded9fa7
IhorSF <ihor@silo.finance>
2025-07-02 15:48:25 +0400

    gas

--- silo-core/test/foundry/gas/BorrowAccrueInterest.gas.sol
+++ silo-core/test/foundry/gas/BorrowAccrueInterest.gas.sol
@@ -31,7 +31,7 @@ contract BorrowAccrueInterestGasTest is Gas, Test {
             address(silo1),
             abi.encodeCall(ISilo.borrow, (ASSETS, BORROWER, BORROWER)),
             "BorrowAccrueInterest",
-            237154,
+            234814,
             1000
         );
     }
