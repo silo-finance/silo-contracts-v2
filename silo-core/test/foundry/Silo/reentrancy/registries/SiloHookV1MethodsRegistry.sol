9cba8c232ceef27fb505ddd56da64ab6479df957
IhorSF <102213749+IhorSF@users.noreply.github.com>
2025-07-03 11:59:39 +0400

    silo-core: Ownable with 1 step transfer for the GaugeHookReceiver (#1447)

--- silo-core/test/foundry/Silo/reentrancy/registries/SiloHookV1MethodsRegistry.sol
+++ silo-core/test/foundry/Silo/reentrancy/registries/SiloHookV1MethodsRegistry.sol
@@ -19,6 +19,7 @@ import {RenounceOwnershipReentrancyTest} from "../methods/silo-hook-v1/RenounceO
 import {SetGaugeReentrancyTest} from "../methods/silo-hook-v1/SetGaugeReentrancyTest.sol";
 import {SiloConfigReentrancyTest} from "../methods/silo-hook-v1/SiloConfigReentrancyTest.sol";
 import {TransferOwnershipReentrancyTest} from "../methods/silo-hook-v1/TransferOwnershipReentrancyTest.sol";
+import {TransferOwnership1StepReentrancyTest} from "../methods/silo-hook-v1/TransferOwnership1StepReentrancyTest.sol";
 
 contract SiloHookV1MethodsRegistry is IMethodsRegistry {
     mapping(bytes4 methodSig => IMethodReentrancyTest) public methods;
@@ -40,6 +41,7 @@ contract SiloHookV1MethodsRegistry is IMethodsRegistry {
         _registerMethod(new SetGaugeReentrancyTest());
         _registerMethod(new SiloConfigReentrancyTest());
         _registerMethod(new TransferOwnershipReentrancyTest());
+        _registerMethod(new TransferOwnership1StepReentrancyTest());
     }
 
     function supportedMethodsLength() external view returns (uint256) {
