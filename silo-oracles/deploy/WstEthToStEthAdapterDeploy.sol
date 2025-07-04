87f229872f186259d8b06f592fc5b89237b71187
Sergey Maksimov <sergey@silo.finance>
2025-07-04 22:27:44 +0700

    wstETH adapter

---
+++ silo-oracles/deploy/WstEthToStEthAdapterDeploy.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity >=0.7.6;
+
+import {CommonDeploy} from "./CommonDeploy.sol";
+import {AddrKey} from "common/addresses/AddrKey.sol";
+import {WstEthToStEthAdapter} from "silo-oracles/contracts/custom/WstEthToStEthAdapter.sol";
+import {SiloOraclesContracts} from "./SiloOraclesContracts.sol";
+
+/**
+    FOUNDRY_PROFILE=oracles \
+        forge script silo-oracles/deploy/WstEthToStEthAdapterDeploy.sol \
+        --ffi --rpc-url $RPC_MAINNET --broadcast --verify
+ */
+contract WstEthToStEthAdapterDeploy is CommonDeploy {
+    function run() public returns (WstEthToStEthAdapter adapter) {
+        uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        adapter = new WstEthToStEthAdapter();
+
+        vm.stopBroadcast();
+
+        _registerDeployment(address(adapter), SiloOraclesContracts.WSTETH_TO_STETH_ADAPTER);
+    }
+}
