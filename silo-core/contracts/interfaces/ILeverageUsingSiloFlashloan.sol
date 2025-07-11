ee7d63cafbcc32698167a8977473e4c305f62a33
Thomas A. Anderson <92584446+0xNeoRacer@users.noreply.github.com>
2025-07-11 14:05:44 +0200

    Update silo-core/contracts/interfaces/ILeverageUsingSiloFlashloan.sol
    Co-authored-by: shadowyedd <89228160+shadowyedd@users.noreply.github.com>

--- silo-core/contracts/interfaces/ILeverageUsingSiloFlashloan.sol
+++ silo-core/contracts/interfaces/ILeverageUsingSiloFlashloan.sol
@@ -128,7 +128,7 @@ interface ILeverageUsingSiloFlashloan {
         CloseLeverageArgs calldata _closeLeverageArgs
     ) external;
 
-    /// @notice Does not support fee on transfer tokens. It also does not support borrow on same asset.
+    /// @notice Closes opened leveraged position. Does not support fee on transfer tokens. It also does not support borrow on same asset.
     /// @dev This method requires approval for withdraw all collateral (so minimal requires amount for allowance is
     /// borrower balance). Approval is done using Permit
     /// @param _swapArgs Swap call data and settings, it should swap enough collateral to repay flashloan in debt token
