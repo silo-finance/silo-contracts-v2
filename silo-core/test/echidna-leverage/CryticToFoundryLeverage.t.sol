7d57435ea5fdf0f960ce955d6c2db5b97274c7ef
Neo Racer <0xNeoRacer@protonmail.com>
2025-07-14 14:06:29 +0200

    .

--- silo-core/test/echidna-leverage/CryticToFoundryLeverage.t.sol
+++ silo-core/test/echidna-leverage/CryticToFoundryLeverage.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.19;
 
 // Libraries
-import "forge-std/console.sol";
+import {console2} from "forge-std/console2.sol";
 
 import {Actor} from "silo-core/test/invariants/utils/Actor.sol";
 
