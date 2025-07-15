ff0773fda7727d953485932a85d26eed6e770511
IhorSF <102213749+IhorSF@users.noreply.github.com>
2025-07-15 14:12:42 +0400

    Update common/utils/PausableWithAccessControl.sol
    Co-authored-by: shadowyedd <89228160+shadowyedd@users.noreply.github.com>

--- common/utils/PausableWithAccessControl.sol
+++ common/utils/PausableWithAccessControl.sol
@@ -7,6 +7,7 @@ import {Pausable} from "openzeppelin5/utils/Pausable.sol";
 /// @title Pausable contract with a separate role for pausing
 abstract contract PausableWithAccessControl is AccessControlEnumerable, Pausable {
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant OWNER_ROLE = DEFAULT_ADMIN_ROLE;
 
     function pause() external onlyRole(PAUSER_ROLE) {
         _pause();
