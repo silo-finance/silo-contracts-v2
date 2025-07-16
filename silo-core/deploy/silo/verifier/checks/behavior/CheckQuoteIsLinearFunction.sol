f98e657f2176ce9a565ca0001bc7bbef3cbb5824
Neo Racer <0xNeoRacer@protonmail.com>
2025-07-16 12:53:54 +0200

    .

--- silo-core/deploy/silo/verifier/checks/behavior/CheckQuoteIsLinearFunction.sol
+++ silo-core/deploy/silo/verifier/checks/behavior/CheckQuoteIsLinearFunction.sol
@@ -1,11 +1,13 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.28;
+import {console2} from "forge-std/console2.sol";
 
 import {ICheck} from "silo-core/deploy/silo/verifier/checks/ICheck.sol";
 import {ISiloOracle} from "silo-core/contracts/interfaces/ISiloOracle.sol";
-import {Strings} from "openzeppelin5/utils/Strings.sol";
 
+import {Strings} from "openzeppelin5/utils/Strings.sol";
 import {Utils} from "silo-core/deploy/silo/verifier/Utils.sol";
+import {PriceFormatter} from "../../../../lib/PriceFormatter.sol";
 
 contract CheckQuoteIsLinearFunction is ICheck {
     ISiloOracle internal oracle;
@@ -34,7 +36,10 @@ contract CheckQuoteIsLinearFunction is ICheck {
     }
 
     function errorMessage() external view override returns (string memory message) {
-        message = string.concat("property does not hold, breaks at amount ", Strings.toString(breaksAtAmount));
+        message = string.concat(
+            "property does not hold, breaks at amount ",
+            PriceFormatter.formatNumberInE(breaksAtAmount)
+        );
     }
 
     function execute() external override returns (bool result) {
@@ -59,7 +64,25 @@ contract CheckQuoteIsLinearFunction is ICheck {
             uint256 currentQuote;
             (success, currentQuote) = Utils.quote(oracle, token, amountToQuote);
 
+            /*
+            if we just div by 10, lower price might have some rounding applied eg.
+            previousQuote 98495855921412629816832503143859_353
+             currentQuote 98495855921412629816832503143859_34 < we can expect rounding down from 35 -> 34
+
+             previousQuote 9849591762903238198737336703_9006
+              currentQuote 9849591762903238198737336703_899
+
+             to handle this case, we need to divide both prices by additional number, to combat any expected rounding
+            */
             if (currentQuote != previousQuote / 10) {
+                console2.log("previousQuote", previousQuote);
+                console2.log(" currentQuote", currentQuote);
+
+                (, currentQuote) = Utils.quote(oracle, token, 1e18);
+                console2.log("quote(1e18)", currentQuote);
+                (, currentQuote) = Utils.quote(oracle, token, 1e17);
+                console2.log("quote(1e17)", currentQuote);
+
                 breaksAtAmount = amountToQuote;
                 return false;
             }
