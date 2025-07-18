44d3d9f3553847eb839b917857f9f0c00e6dbd2e
Neo Racer <0xNeoRacer@protonmail.com>
2025-07-18 13:11:18 +0200

    fmt

--- silo-core/test/echidna-leverage/utils/ActorLeverage.sol
+++ silo-core/test/echidna-leverage/utils/ActorLeverage.sol
@@ -13,21 +13,21 @@ contract ActorLeverage is Actor {
     constructor(address[] memory _tokens, address[] memory _contracts) payable Actor(_tokens, _contracts) {
         for (uint256 i = 0; i < _tokens.length; i++) {
             for (uint256 j = 0; j < _contracts.length; j++) {
-                try LeverageRouter(_contracts[j]).predictUserLeverageContract(address(this)) returns (address userLeverage) {
+                try LeverageRouter(_contracts[j]).predictUserLeverageContract(address(this)) returns (
+                    address userLeverage
+                ) {
                     IERC20(_tokens[i]).approve(userLeverage, type(uint256).max);
                     // revoke approval for router, leverage only needs approval on cloned contract
                     IERC20(_tokens[i]).approve(_contracts[j], 0);
 
                     try IERC20R(_tokens[i]).setReceiveApproval(userLeverage, type(uint256).max) {
-                    // nothing to do
-                } catch {
-                    // it might be not debt share token, ignore fail
-                }
+                        // nothing to do
+                    } catch {
+                        // it might be not debt share token, ignore fail
+                    }
                 } catch {
                     // this is not LeverageRouter, ignore fail
                 }
-
-                
             }
         }
     }
