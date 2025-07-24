7141a53a4b96f1229d9f1d48ab79a0ec2e9d3e69
Sergey <20372407+maksimov1@users.noreply.github.com>
2025-07-24 16:29:31 +0700

    silo-core: Two watchlists in Hypernative (#1502)
    (cherry picked from commit 7d356e9f5c7d11e2e9d1d53061cb739f9a8e5880)

--- silo-core/scripts/hypernative/printSilos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-CHAIN=$1
-
-# Extract lines from the file that belong to the given chain block
-# Print only those lines, grep for 0x, then extract the address (2nd field after colon) by sed
-
-sed -n "/\"$CHAIN\"[[:space:]]*:/,/}/p" "silo-core/deploy/silo/_siloDeployments.json" | \
-grep 0x | \
-sed -E 's/.*: *"([^"]+)".*/\1/'
--- silo-core/scripts/hypernative/updateWatchlists.sh
+++ silo-core/scripts/hypernative/updateWatchlists.sh
@@ -10,8 +10,8 @@
 #    forge script silo-core/scripts/PrintSiloAddresses.s.sol \
 #    --ffi --rpc-url $RPC_SONIC | grep 0x | ./silo-core/scripts/hypernative.sh sonic
 
-if [ -z "$1" ]; then
-    echo "Usage: $0 <chain-name>"
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <chain-name>" >&2
     exit 1
 fi
 
@@ -40,25 +40,33 @@ done
 
 echo "Amount of addresses to submit is ${#ADDRESSES[@]}"
 
-RESPONSE=$(curl -X 'PATCH' \
-    "$HYPERNATIVE_WATCHLIST" \
-    -H 'accept: application/json' \
-    -H 'Content-Type: application/json' \
-    -H "x-client-id: $HYPERNATIVE_CLIENT_ID" \
-    -H "x-client-secret: $HYPERNATIVE_CLIENT_SECRET" \
-    -d '{
-    "name": "All Silo0 and Silo1 addresses",
-    "description": "",
-    "assets": [
-    '"$JSON_ASSETS"'
-    ],
-    "mode": "add"
-}' 2>/dev/null)
+for WATCHLIST_TYPE in "pause" "sandbox"; do
+    echo "Sending request to update ${WATCHLIST_TYPE} watchlist..."
 
-if echo "$RESPONSE" | grep -q '"success"[[:space:]]*:[[:space:]]*true'; then
-    echo "Success from Hypernative response for $CHAIN_NAME"
-    exit 0
-else
-    echo "Error: PATCH failed or did not return \"success\":true in response" >&2
-    exit 1
-fi
+    if [ "$WATCHLIST_TYPE" = "pause" ]; then
+        URL="$HYPERNATIVE_WATCHLIST_PAUSE"
+    else
+        URL="$HYPERNATIVE_WATCHLIST_SANDBOX"
+    fi
+
+    RESPONSE=$(curl -X 'PATCH' \
+        "$URL" \
+        -H 'accept: application/json' \
+        -H 'Content-Type: application/json' \
+        -H "x-client-id: $HYPERNATIVE_CLIENT_ID" \
+        -H "x-client-secret: $HYPERNATIVE_CLIENT_SECRET" \
+        -d '{
+        "description": "",
+        "assets": [
+        '"$JSON_ASSETS"'
+        ],
+        "mode": "add"
+    }' 2>/dev/null)
+
+    if echo "$RESPONSE" | grep -q '"success"[[:space:]]*:[[:space:]]*true'; then
+        echo "Success from Hypernative ${WATCHLIST_TYPE} watchlist update response for $CHAIN_NAME"
+    else
+        echo "Error: Hypernative ${WATCHLIST_TYPE} watchlist update did not return \"success\":true in response" >&2
+        exit 1
+    fi
+done
