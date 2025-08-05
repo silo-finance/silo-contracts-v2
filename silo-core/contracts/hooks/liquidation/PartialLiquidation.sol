60901024cb139fea943b57932028a373cc0e3113
Neo Racer <0xNeoRacer@protonmail.com>
2025-08-05 21:10:28 +0200

    revert unnecessary code

--- silo-core/contracts/hooks/liquidation/PartialLiquidation.sol
+++ silo-core/contracts/hooks/liquidation/PartialLiquidation.sol
@@ -5,7 +5,6 @@ import {IERC20} from "openzeppelin5/interfaces/IERC20.sol";
 import {SafeERC20} from "openzeppelin5/token/ERC20/utils/SafeERC20.sol";
 
 import {ISilo} from "silo-core/contracts/interfaces/ISilo.sol";
-import {IFirmHook} from "silo-core/contracts/interfaces/IFirmHook.sol";
 import {IShareToken} from "silo-core/contracts/interfaces/IShareToken.sol";
 import {IPartialLiquidation} from "silo-core/contracts/interfaces/IPartialLiquidation.sol";
 import {ISiloConfig} from "silo-core/contracts/interfaces/ISiloConfig.sol";
@@ -34,10 +33,6 @@ abstract contract PartialLiquidation is TransientReentrancy, BaseHookReceiver, I
         uint256 withdrawAssetsFromCollateral;
         uint256 withdrawAssetsFromProtected;
         bytes4 customError;
-    }
-
-    function maturityDate() public view virtual returns (uint256) {
-        return IFirmHook(address(this)).maturityDate();
     }
 
     /// @inheritdoc IPartialLiquidation
