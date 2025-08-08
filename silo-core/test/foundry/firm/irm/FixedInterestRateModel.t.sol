60e8aa30b166ad401f3adba27599c582c31ca87c
Sergey Maksimov <sergey@silo.finance>
2025-08-08 20:43:14 +0700

    factory

--- silo-core/test/foundry/firm/irm/FixedInterestRateModel.t.sol
+++ silo-core/test/foundry/firm/irm/FixedInterestRateModel.t.sol
@@ -9,6 +9,7 @@ import {FixedInterestRateModelConfig} from "silo-core/contracts/interestRateMode
 import {FixedInterestRateModelFactory} from "silo-core/contracts/interestRateModel/firm/FixedInterestRateModelFactory.sol";
 import {IFixedInterestRateModel} from "silo-core/contracts/interestRateModel/firm/interfaces/IFixedInterestRateModel.sol";
 import {IFixedInterestRateModelConfig} from "silo-core/contracts/interestRateModel/firm/interfaces/IFixedInterestRateModelConfig.sol";
+import {IFixedInterestRateModelFactory} from "silo-core/contracts/interestRateModel/firm/interfaces/IFixedInterestRateModelFactory.sol";
 
 contract FixedInterestRateModelTest is Test {
     address constant FIRM_VAULT = address(111);
@@ -36,18 +37,40 @@ contract FixedInterestRateModelTest is Test {
         );
     }
 
-    function test_FixedInterestRateModel_predictAddress() public {
-        address predicted = factory.predictFixedInterestRateModelAddress(address(this), bytes32(0));
-
-        irm = factory.create(
-            config,
-            bytes32(0)
-        );
-
-        assertEq(address(irm), predicted);
+    function test_FixedInterestRateModelFactory_irmImplementation() public view {
+        assertTrue(address(factory.IRM_IMPLEMENTATION()) != address(0));
     }
 
-    function test_FixedInterestRateModel_initialize() public {
+    function test_FixedInterestRateModelFactory_predictAddress() public {
+        address predicted = factory.predictFixedInterestRateModelAddress(address(this), bytes32(0));
+        irm = factory.create(config, bytes32(0));
+        address next = factory.predictFixedInterestRateModelAddress(address(this), bytes32(0));
+        assertEq(address(irm), predicted);
+        assertTrue(address(irm) != next);
+    }
+
+    function test_FixedInterestRateModelFactory_createdInFactory() public view {
+        assertTrue(factory.createdInFactory(address(irm)));
+        assertTrue(!factory.createdInFactory(address(SHARE_TOKEN)));
+    }
+
+    function test_FixedInterestRateModelFactory_create_invalidMaturityTimestamp() public {
+        config.maturityTimestamp = block.timestamp - 1;
+        vm.expectRevert(IFixedInterestRateModelFactory.InvalidMaturityTimestamp.selector);
+        factory.create(config, bytes32(0));
+    }
+
+    function test_FixedInterestRateModelFactory_create_configIsEqual() public view {
+        IFixedInterestRateModel.Config memory configFromDeployed = irm.getConfig();
+
+        assertEq(configFromDeployed.apr, config.apr);
+        assertEq(configFromDeployed.maturityTimestamp, config.maturityTimestamp);
+        assertEq(configFromDeployed.firmVault, config.firmVault);
+        assertEq(address(configFromDeployed.shareToken), address(config.shareToken));
+        assertEq(configFromDeployed.silo, config.silo);
+    }
+
+    function test_FixedInterestRateModel_initialized() public {
         IFixedInterestRateModelConfig configAddress = FixedInterestRateModel(address(irm)).irmConfig();
         vm.expectRevert(Initializable.InvalidInitialization.selector);
         irm.initialize(address(configAddress));
