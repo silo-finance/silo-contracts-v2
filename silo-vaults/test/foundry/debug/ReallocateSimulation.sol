8a6157c132cb234c61b524fabac6ec03ad990210
Neo Racer <0xNeoRacer@protonmail.com>
2025-08-11 20:24:06 +0200

    v1

--- silo-vaults/test/foundry/debug/ReallocateSimulation.sol
+++ silo-vaults/test/foundry/debug/ReallocateSimulation.sol
@@ -5,6 +5,7 @@ import {Test} from "forge-std/Test.sol";
 import {console2} from "forge-std/console2.sol";
 
 import {IERC4626, IERC20} from "openzeppelin5/interfaces/IERC4626.sol";
+import {Strings} from "openzeppelin5/utils/Strings.sol";
 import {ISilo} from "silo-core/contracts/interfaces/ISilo.sol";
 import {MarketAllocation, ISiloVault} from "../../../contracts/interfaces/ISiloVault.sol";
 
@@ -18,7 +19,7 @@ contract ReallocateSimulationTest is Test {
      FOUNDRY_PROFILE=vaults_tests forge test --ffi --mt test_20250811_reallocate_simulation -vvv
     */
     function test_20250811_reallocate_simulation() public {
-        vm.createSelectFork(vm.envString("RPC_SONIC"), 42530830);
+        vm.createSelectFork(vm.envString("RPC_SONIC"), 42545060);
 
         ISiloVault vault = ISiloVault(0xDED4aC8645619334186f28B8798e07ca354CFa0e);
 
@@ -44,6 +45,9 @@ contract ReallocateSimulationTest is Test {
 
         uint256 amountToCoverDebt = vaultAssets;
 
+        // DEBUG:
+        amountToCoverDebt = 500e18;
+
         // INative(address(wS)).deposit{value: amountToCoverDebt}();
 
         vm.prank(multisig);
@@ -58,26 +62,24 @@ contract ReallocateSimulationTest is Test {
         console2.log("max withdraw by vault", maxWithdraw / 1e18);
         console2.log("preview withdraw", previewWithdraw / 1e18);
 
-        uint256 debtToRepay = amountToCoverDebt - maxWithdraw + 10e18;
+        uint256 debtToRepay = 45e18; // amountToCoverDebt - maxWithdraw + 10e18;
         console2.log("debt to repay", debtToRepay / 1e18);
 
         uint256 sharesToRepay = fromSilo.convertToShares(debtToRepay);
+        uint256 liquidity = ISilo(address(fromSilo)).getLiquidity();
         console2.log("shares to repay", sharesToRepay);
-        console2.log("liquidity", ISilo(address(fromSilo)).getLiquidity() / 1e18);
-
-        // +1 token just to be sure tx will not revert
-        uint256 allocation0 = previewWithdraw - maxWithdraw + 1e18; 
+        console2.log("liquidity", liquidity / 1e18);
 
         allocations[0].market = fromSilo;
-        allocations[0].assets = debtToRepay;
+        allocations[0].assets =  vaultAssets - liquidity + 1e18; // +1 to minimise risk of failing tx
 
         allocations[1].market = toSilo;
         allocations[1].assets = type(uint256).max; // deposit all
 
         console2.log("allocation[0].market", address(fromSilo));
-        console2.log("allocation[0].assets", allocation0);
+        console2.log("allocation[0].assets", allocations[0].assets);
         console2.log("allocation[1].market", address(toSilo));
-        console2.log("allocation[1].assets", type(uint256).max);
+        console2.log("allocation[1].assets", allocations[1].assets);
 
         _printVaultBalance(fromSilo, address(vault));
 
@@ -85,6 +87,11 @@ contract ReallocateSimulationTest is Test {
         vault.reallocate(allocations);
 
         _printVaultBalance(fromSilo, address(vault));
+
+        console2.log("\n-------------------------------- multisig tx:\n"); 
+        console2.log("approve and deposit amount: ", amountToCoverDebt);
+        console2.log("tokens: ", amountToCoverDebt / 1e18);
+        _printAllocation(allocations);
     }
 
     function _printVaultBalance(IERC4626 _silo, address _vault) internal view returns (uint256 vaultAssets) {
@@ -93,5 +100,25 @@ contract ReallocateSimulationTest is Test {
 
         // console2.log("vault shares", vaultShares / 1e18);
         console2.log("vault assets", vaultAssets / 1e18);
+    }
+
+    function _printAllocation(MarketAllocation[] memory _allocation) internal view {
+        // print in a way that can be copy pasted to the Safe multisig
+        string memory allocations = "[";
+
+        for (uint256 i; i < _allocation.length; ++i) {
+            allocations = string.concat(
+                allocations,
+                "[",
+                Strings.toHexString(address(_allocation[i].market)),
+                ", ",
+                Strings.toString(_allocation[i].assets),
+                "]",
+                i < _allocation.length - 1 ? "," : ""
+            );
+        }
+
+        allocations = string.concat(allocations, "]");
+        console2.log("reallocation data:\n", allocations);
     }
 }
