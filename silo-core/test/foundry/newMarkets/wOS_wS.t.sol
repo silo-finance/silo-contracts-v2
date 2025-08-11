a9c8dbbf6ed7a30ecefbd7dfd9afd8d0e9fd3692
Sergey Maksimov <sergey@silo.finance>
2025-08-11 17:47:58 +0700

    latest price

--- silo-core/test/foundry/newMarkets/wOS_wS.t.sol
+++ silo-core/test/foundry/newMarkets/wOS_wS.t.sol
@@ -4,5 +4,5 @@ pragma solidity 0.8.28;
 import {NewMarketTest} from "silo-core/test/foundry/newMarkets/common/NewMarket.sol";
 
 contract wOS_wS_Test is NewMarketTest {
-    constructor() NewMarketTest(BlockChain.SONIC, 42497244, 0x10AD9dD6f1250D496f1Cb65b7cB3C33A3605e1DC, 3185, 3089) {}
+    constructor() NewMarketTest(BlockChain.SONIC, 42498314, 0x10AD9dD6f1250D496f1Cb65b7cB3C33A3605e1DC, 3216, 3125) {}
 }
