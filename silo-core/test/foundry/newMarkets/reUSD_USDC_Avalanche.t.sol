426abd29aaf22424760a5d9e0c2aea5b4559df50
Sergey Maksimov <sergey@silo.finance>
2025-08-15 18:39:15 +0700

    redeploy with new IRM

--- silo-core/test/foundry/newMarkets/reUSD_USDC_Avalanche.t.sol
+++ silo-core/test/foundry/newMarkets/reUSD_USDC_Avalanche.t.sol
@@ -4,5 +4,5 @@ pragma solidity 0.8.28;
 import {NewMarketTest} from "silo-core/test/foundry/newMarkets/common/NewMarket.sol";
 
 contract reUSD_USDC_Avalanche_Test is NewMarketTest {
-    constructor() NewMarketTest(BlockChain.AVALANCHE, 67064078, 0x98f0ac5930E09F996650e4aC0Fc8D7578a0Caaab, 101, 100) {}
+    constructor() NewMarketTest(BlockChain.AVALANCHE, 67107126, 0xCcEB4356F5958e0E7a69Dd5c7A8B2aB730951819, 101, 100) {}
 }
