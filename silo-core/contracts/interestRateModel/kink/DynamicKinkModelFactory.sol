94fff6d3a03754b0bc14729db12e1ff63f3316c5
Neo Racer <0xNeoRacer@protonmail.com>
2025-08-18 10:38:36 +0200

    fix math for generator

--- silo-core/contracts/interestRateModel/kink/DynamicKinkModelFactory.sol
+++ silo-core/contracts/interestRateModel/kink/DynamicKinkModelFactory.sol
@@ -58,40 +58,36 @@ contract DynamicKinkModelFactory is Create2Factory, IDynamicKinkModelFactory {
         IDynamicKinkModel.UserFriendlyConfigInt memory defaultInt = _castConfig(_default);
 
         // 0 <= ulow < u1 < u2 < ucrit < DP
-        require(defaultInt.ulow.isInBelow(0, defaultInt.u1), IDynamicKinkModel.InvalidUlow());
         require(defaultInt.u1.isInside(defaultInt.ulow, defaultInt.u2), IDynamicKinkModel.InvalidU1());
         require(defaultInt.u2.isInside(defaultInt.u1, defaultInt.ucrit), IDynamicKinkModel.InvalidU2());
         require(defaultInt.ucrit.isInside(defaultInt.u2, DP), IDynamicKinkModel.InvalidUcrit());
 
-        // original: 0 <= rmin < rcritMin <= rcritMax <= r100
-        // proposed: 0 <= rmin < rcritMin < rritMax <= r100 TODO
+        // 0 <= rmin < rcritMin <= rcritMax <= r100
 
-        require(defaultInt.rmin.isInBelow(0, defaultInt.rcritMin), IDynamicKinkModel.InvalidRmin());
-        require(defaultInt.rcritMin.isInside(defaultInt.rmin, defaultInt.rcritMax), IDynamicKinkModel.InvalidRcritMin());
+        require(defaultInt.rcritMin.isInAbove(defaultInt.rmin, defaultInt.rcritMax), IDynamicKinkModel.InvalidRcritMin());
 
         require(
             defaultInt.rcritMax.isBetween(defaultInt.rcritMin, defaultInt.r100),
             IDynamicKinkModel.InvalidRcritMax()
         );
 
-        int256 rCheckHi = (defaultInt.r100 - defaultInt.rmin) / (defaultInt.rcritMax - defaultInt.rmin);
-        int256 rCheckLo = (DP - defaultInt.ucrit) / (defaultInt.ucrit - defaultInt.ulow);
+        int256 rCheckHi = (defaultInt.r100 - defaultInt.rmin) * DP / (defaultInt.rcritMax - defaultInt.rmin);
+        int256 rCheckLo = (DP - defaultInt.ulow) * DP / (defaultInt.ucrit - defaultInt.ulow);
         require(rCheckHi >= rCheckLo, IDynamicKinkModel.InvalidDefaultConfig());
 
         int256 s = 365 days;
 
         // 0 < tMin <= tcrit <= t2 < 100y  
-        require(defaultInt.tMin.isInAbove(0, defaultInt.tcrit), IDynamicKinkModel.InvalidTMin());
         require(defaultInt.tcrit.isBetween(defaultInt.tMin, defaultInt.t2), IDynamicKinkModel.InvalidTPlus());
         require(defaultInt.t2.isInBelow(defaultInt.tcrit, 100 * s), IDynamicKinkModel.InvalidT2());
 
         // 0 < tlow <= t1 < 100y
-        require(defaultInt.tlow.isInAbove(0, defaultInt.t1), IDynamicKinkModel.InvalidTMinus());
+        require(defaultInt.tlow != 0, IDynamicKinkModel.InvalidTMinus());
         require(defaultInt.t1.isInBelow(defaultInt.tlow, 100 * s), IDynamicKinkModel.InvalidT1());
 
         config.rmin = defaultInt.rmin / s;
-        config.kmin = SafeCast.toInt96((defaultInt.rcritMin - defaultInt.rmin) / (defaultInt.ucrit - defaultInt.ulow) / s);
-        config.kmax = SafeCast.toInt96((defaultInt.rcritMax - defaultInt.rmin) / (defaultInt.ucrit - defaultInt.ulow) / s);
+        config.kmin = SafeCast.toInt96((defaultInt.rcritMin - defaultInt.rmin) * DP / (defaultInt.ucrit - defaultInt.ulow) / s);
+        config.kmax = SafeCast.toInt96((defaultInt.rcritMax - defaultInt.rmin) * DP / (defaultInt.ucrit - defaultInt.ulow) / s);
 
         console2.log("config.kmax, defaultInt.ucrit", config.kmax);
         console2.log(defaultInt.ucrit);
@@ -99,15 +95,16 @@ contract DynamicKinkModelFactory is Create2Factory, IDynamicKinkModelFactory {
         int256 divider = s * config.kmax * (DP - defaultInt.ucrit);
         require(divider != 0, IDynamicKinkModel.AlphaDividerZero()); // TODO: check if we can handle this in other way
 
-        config.alpha = (defaultInt.r100 - defaultInt.rmin - s * config.kmax * (DP - defaultInt.ulow)) / divider;
+        config.alpha = (defaultInt.r100 - defaultInt.rmin - s * config.kmax * (DP - defaultInt.ulow)) * DP / divider;
+        console2.log("config.alpha", config.alpha);
 
         config.c1 = (config.kmax - config.kmin) / defaultInt.t1;
         config.c2 = (config.kmax - config.kmin) / defaultInt.t2;
         
-        config.cminus = ((config.kmax - config.kmin) / defaultInt.tlow - config.c1)
+        config.cminus = ((config.kmax - config.kmin) / defaultInt.tlow - config.c1) * DP
             / (defaultInt.u1 - defaultInt.ulow);
         
-        config.cplus = ((config.kmax - config.kmin) / defaultInt.tcrit - config.c2)
+        config.cplus = ((config.kmax - config.kmin) / defaultInt.tcrit - config.c2) * DP
             / (defaultInt.ucrit - defaultInt.u2);
         
         config.dmax = (config.kmax - config.kmin) / defaultInt.tMin;
