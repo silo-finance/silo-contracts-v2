e50466580fdbe85d5e87fcf3d608acdd8f52c4a7
Neo Racer <0xNeoRacer@protonmail.com>
2025-08-20 18:46:00 +0200

    qa

--- silo-oracles/test/foundry/pendle/amm/FixedPricePTAMMOracle.t.sol
+++ silo-oracles/test/foundry/pendle/amm/FixedPricePTAMMOracle.t.sol
@@ -7,6 +7,7 @@ import {FixedPricePTAMMOracleFactory} from "silo-oracles/contracts/pendle/amm/Fi
 import {IFixedPricePTAMMOracleConfig} from "silo-oracles/contracts/interfaces/IFixedPricePTAMMOracleConfig.sol";
 import {IFixedPricePTAMMOracle} from "silo-oracles/contracts/interfaces/IFixedPricePTAMMOracle.sol";
 import {IPendleAMM} from "silo-oracles/contracts/interfaces/IPendleAMM.sol";
+import {IFixedPricePTAMMOracleFactory} from "silo-oracles/contracts/interfaces/IFixedPricePTAMMOracleFactory.sol";
 
 contract FixedPricePTAMMOracleTest is Test {
     FixedPricePTAMMOracleFactory factory;
@@ -76,5 +77,73 @@ contract FixedPricePTAMMOracleTest is Test {
         address oracle2 = address(factory.create(_config, bytes32(0)));
 
         assertEq(oracle1, oracle2, "Oracle addresses should be the same if we reuse the same config");
+    }
+
+    /*
+    FOUNDRY_PROFILE=oracles forge test --mt test_ptamm_reorg --ffi -vv
+    */
+    function test_ptamm_reorg(IFixedPricePTAMMOracleConfig.DeploymentConfig memory _config) public {
+        address eoa1 = makeAddr("eoa1");
+        address eoa2 = makeAddr("eoa2");
+
+        uint256 snapshot = vm.snapshotState();
+
+        vm.prank(eoa1);
+        address oracle1 = address(factory.create(_config, bytes32(0)));
+
+        vm.revertToState(snapshot);
+
+        vm.prank(eoa2);
+        address oracle2 = address(factory.create(_config, bytes32(0)));
+
+        assertNotEq(oracle1, oracle2, "Oracle addresses should be different if we reorg");
+    }
+
+    /*
+    FOUNDRY_PROFILE=oracles forge test --mt test_ptamm_verifyConfig_pass_fuzz --ffi -vv
+    */
+    function test_ptamm_verifyConfig_pass_fuzz(IFixedPricePTAMMOracleConfig.DeploymentConfig memory _config) 
+        public 
+        view 
+    {
+        vm.assume(_config.baseToken != address(0));
+        vm.assume(_config.quoteToken != address(0));
+        vm.assume(_config.quoteToken != _config.baseToken);
+
+        factory.verifyConfig(_config);
+    }
+
+    /*
+    FOUNDRY_PROFILE=oracles forge test --mt test_ptamm_verifyConfig_fail --ffi -vv
+    */
+    function test_ptamm_verifyConfig_fail() public {
+        IFixedPricePTAMMOracleConfig.DeploymentConfig memory config;
+
+        vm.expectRevert(abi.encodeWithSelector(IFixedPricePTAMMOracleFactory.AddressZero.selector));
+        factory.verifyConfig(config);
+
+        config.baseToken = address(1);
+        vm.expectRevert(abi.encodeWithSelector(IFixedPricePTAMMOracleFactory.AddressZero.selector));
+        factory.verifyConfig(config);
+
+        config.quoteToken = address(0);
+        vm.expectRevert(abi.encodeWithSelector(IFixedPricePTAMMOracleFactory.AddressZero.selector));
+        factory.verifyConfig(config);
+
+        config.quoteToken = address(1);
+        vm.expectRevert(abi.encodeWithSelector(IFixedPricePTAMMOracleFactory.TokensAreTheSame.selector));
+        factory.verifyConfig(config);
+
+        config.quoteToken = address(2);
+        factory.verifyConfig(config); // pass
+    }
+
+    /*
+    FOUNDRY_PROFILE=oracles forge test --mt test_ptamm_hashConfig --ffi -vv
+    */
+    function test_ptamm_hashConfig_fuzz(IFixedPricePTAMMOracleConfig.DeploymentConfig memory _config) public view {
+        bytes32 configId = factory.hashConfig(_config);
+
+        assertEq(configId, keccak256(abi.encode(_config)), "Config hash should match");
     }
 }
