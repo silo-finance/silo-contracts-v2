889c78d5fd5be539027c91ab533d6fe6104537db
IhorSF <102213749+IhorSF@users.noreply.github.com>
2025-08-27 13:53:10 +0400

    silo-core: syrupUSDC/USDC market arbitrum (#1597)

--- silo-core/test/foundry/newMarkets/wOS_wS.t.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: Unlicense
-pragma solidity 0.8.28;
-
-import {NewMarketTest} from "silo-core/test/foundry/newMarkets/common/NewMarket.sol";
-
-contract wOS_wS_Test is NewMarketTest {
-    constructor() NewMarketTest(BlockChain.SONIC, 42498314, 0x10AD9dD6f1250D496f1Cb65b7cB3C33A3605e1DC, 3216, 3125) {}
-}
