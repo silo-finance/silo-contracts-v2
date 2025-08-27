7d34b347e6891d5abd8d2fbb28e1c7ba5a17dfef
Neo Racer <0xNeoRacer@protonmail.com>
2025-08-27 09:28:51 +0200

    amm refactor

--- silo-oracles/deploy/pendle/deployment-helpers/PTAmmOracleDeployCommon.s.sol
+++ silo-oracles/deploy/pendle/deployment-helpers/PTAmmOracleDeployCommon.s.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.28;
 
 import {console2} from "forge-std/console2.sol";
 
-import {CommonDeploy} from "../CommonDeploy.sol";
+import {CommonDeploy} from "../../CommonDeploy.sol";
 import {ChainsLib} from "silo-foundry-utils/lib/ChainsLib.sol";
 
 import {SiloOraclesFactoriesContracts, SiloOraclesFactoriesDeployments} from "silo-oracles/deploy/SiloOraclesFactoriesContracts.sol";
@@ -13,13 +13,11 @@ import {IFixedPricePTAMMOracleConfig} from "silo-oracles/contracts/interfaces/IF
 import {IFixedPricePTAMMOracle} from "silo-oracles/contracts/interfaces/IFixedPricePTAMMOracle.sol";
 import {IPendleAMM} from "silo-oracles/contracts/interfaces/IPendleAMM.sol";
 
-/**
-FOUNDRY_PROFILE=oracles \
-    forge script silo-oracles/deploy/pendle/Helper_PTAmmOracleDeploy.s.sol \
-    --ffi --rpc-url $RPC_AVALANCHE --broadcast --verify
- */
-contract Helper_PTAmmOracleDeploy is CommonDeploy {
-    function run() public {
+
+abstract contract PTAmmOracleDeployCommon is CommonDeploy {
+    function _deployPTAmmOracle(IFixedPricePTAMMOracleConfig.DeploymentConfig memory _config) internal {
+        require(_config.hardcoddedQuoteToken != address(0), "hardcoddedQuoteToken is not set");
+
         uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
 
         string memory chainAlias = ChainsLib.chainAlias();
@@ -30,23 +28,20 @@ contract Helper_PTAmmOracleDeploy is CommonDeploy {
         ));
 
         console2.log("factory: ", address(factory));
+        require(address(factory) != address(0), "factory is not deployed");
 
-        IFixedPricePTAMMOracleConfig.DeploymentConfig memory config = IFixedPricePTAMMOracleConfig.DeploymentConfig({
-            amm: IPendleAMM(0x4d717868F4Bd14ac8B29Bb6361901e30Ae05e340),
-            ptToken: 0xB4205a645c7e920BD8504181B1D7f2c5C955C3e7,
-            ptUnderlyingQuoteToken: 0x5d3a1Ff2b6BAb83b63cd9AD0787074081a52ef34,
-            hardcoddedQuoteToken: 0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E // USDC
-        });
-
-        address existingOracle = factory.resolveExistingOracle(factory.hashConfig(config));
+        address existingOracle = factory.resolveExistingOracle(factory.hashConfig(_config));
 
         console2.log("existing oracle: ", existingOracle);
         if (existingOracle != address(0)) return;
 
         vm.startBroadcast(deployerPrivateKey);
-        IFixedPricePTAMMOracle oracle = factory.create(config, bytes32(0));
+        IFixedPricePTAMMOracle oracle = factory.create(_config, bytes32(0));
         vm.stopBroadcast();
 
         console2.log("deployed oracle: ", address(oracle));
+
+        uint256 samplePrice = oracle.quote(1e18, _config.ptToken);
+        console2.log("sample price for 1e18 PT: ", samplePrice);
     }
 }
