438d9905aa9828f2e2bfd057d1d19f1b920c6a5a
Neo Racer <0xNeoRacer@protonmail.com>
2025-09-24 13:23:45 +0200

    cherrypick b7ed7bdf2a52e1bcd92664f783597175f98c95f0 Kink deployer
    + fixes

--- silo-core/deploy/SiloDeployerDeploy.s.sol
+++ silo-core/deploy/SiloDeployerDeploy.s.sol
@@ -1,14 +1,17 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.28;
 
+import {console2} from "forge-std/console2.sol";
 import {ChainsLib} from "silo-foundry-utils/lib/ChainsLib.sol";
 
 import {CommonDeploy} from "./_CommonDeploy.sol";
 import {SiloCoreContracts, SiloCoreDeployments} from "silo-core/common/SiloCoreContracts.sol";
 import {SiloDeployer} from "silo-core/contracts/SiloDeployer.sol";
 import {IInterestRateModelV2Factory} from "silo-core/contracts/interfaces/IInterestRateModelV2Factory.sol";
+import {IDynamicKinkModelFactory} from "silo-core/contracts/interfaces/IDynamicKinkModelFactory.sol";
 import {ISiloFactory} from "silo-core/contracts/interfaces/ISiloFactory.sol";
 import {ISiloDeployer} from "silo-core/contracts/interfaces/ISiloDeployer.sol";
+
 import {Silo} from "silo-core/contracts/Silo.sol";
 import {ShareProtectedCollateralToken} from "silo-core/contracts/utils/ShareProtectedCollateralToken.sol";
 import {ShareDebtToken} from "silo-core/contracts/utils/ShareDebtToken.sol";
@@ -16,7 +19,7 @@ import {ShareDebtToken} from "silo-core/contracts/utils/ShareDebtToken.sol";
 /**
     FOUNDRY_PROFILE=core \
         forge script silo-core/deploy/SiloDeployerDeploy.s.sol \
-        --ffi --rpc-url $RPC_INK --broadcast --verify
+        --ffi --rpc-url $RPC_SONIC --broadcast --verify
 
     Resume verification:
     FOUNDRY_PROFILE=core \
@@ -70,22 +73,51 @@ contract SiloDeployerDeploy is CommonDeploy {
         string memory chainAlias = ChainsLib.chainAlias();
         uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
 
-        ISiloFactory siloFactory = ISiloFactory(SiloCoreDeployments.get(SiloCoreContracts.SILO_FACTORY, chainAlias));
+        console2.log("[SiloDeployerDeploy] chainAlias", chainAlias);
+
+        address siloFactory = SiloCoreDeployments.get(SiloCoreContracts.SILO_FACTORY, chainAlias);
+
+        require(siloFactory != address(0), string.concat(SiloCoreContracts.SILO_FACTORY, " not deployed"));
+        console2.log("siloFactory", siloFactory);
+
+        if (keccak256(abi.encodePacked(chainAlias)) == keccak256(abi.encodePacked(ChainsLib.ANVIL_ALIAS))) {
+            _deployNewSiloImplementation(ISiloFactory(siloFactory));
+        }
 
         address irmConfigFactory = SiloCoreDeployments.get(
             SiloCoreContracts.INTEREST_RATE_MODEL_V2_FACTORY,
             chainAlias
         );
 
-        vm.startBroadcast(deployerPrivateKey);
+        require(irmConfigFactory != address(0), string.concat(SiloCoreContracts.INTEREST_RATE_MODEL_V2_FACTORY, " not deployed"));
+        console2.log("irmConfigFactory", irmConfigFactory);
 
-        address siloImpl = address(new Silo(siloFactory));
-        address shareProtectedCollateralTokenImpl = address(new ShareProtectedCollateralToken());
-        address shareDebtTokenImpl = address(new ShareDebtToken());
+        address dkinkIRMConfigFactory = SiloCoreDeployments.get(
+            SiloCoreContracts.DYNAMIC_KINK_MODEL_FACTORY,
+            chainAlias
+        );
+
+        require(dkinkIRMConfigFactory != address(0), string.concat(SiloCoreContracts.DYNAMIC_KINK_MODEL_FACTORY, " not deployed"));
+        console2.log("dkinkIRMConfigFactory", dkinkIRMConfigFactory);
+
+        address siloImpl = SiloCoreDeployments.get(SiloCoreContracts.SILO, chainAlias);
+        require(siloImpl != address(0), string.concat(SiloCoreContracts.SILO, " not deployed"));
+        console2.log("siloImpl", siloImpl);
+        
+        address shareProtectedCollateralTokenImpl = SiloCoreDeployments.get(SiloCoreContracts.SHARE_PROTECTED_COLLATERAL_TOKEN, chainAlias);
+        require(shareProtectedCollateralTokenImpl != address(0), string.concat(SiloCoreContracts.SHARE_PROTECTED_COLLATERAL_TOKEN, " not deployed"));
+        console2.log("shareProtectedCollateralTokenImpl", shareProtectedCollateralTokenImpl);
+
+        address shareDebtTokenImpl = SiloCoreDeployments.get(SiloCoreContracts.SHARE_DEBT_TOKEN, chainAlias);
+        require(shareDebtTokenImpl != address(0), string.concat(SiloCoreContracts.SHARE_DEBT_TOKEN, " not deployed"));
+        console2.log("shareDebtTokenImpl", shareDebtTokenImpl);
+        
+        vm.startBroadcast(deployerPrivateKey);
 
         siloDeployer = ISiloDeployer(address(new SiloDeployer(
             IInterestRateModelV2Factory(irmConfigFactory),
-            siloFactory,
+            IDynamicKinkModelFactory(dkinkIRMConfigFactory),
+            ISiloFactory(siloFactory),
             siloImpl,
             shareProtectedCollateralTokenImpl,
             shareDebtTokenImpl
@@ -94,6 +126,17 @@ contract SiloDeployerDeploy is CommonDeploy {
         vm.stopBroadcast();
 
         _registerDeployment(address(siloDeployer), SiloCoreContracts.SILO_DEPLOYER);
+
+        console2.log("[SiloDeployerDeploy] done, siloDeployer", address(siloDeployer));
+    }
+
+    function _deployNewSiloImplementation(ISiloFactory _siloFactory) internal returns (address) {
+        console2.log("\n[SiloDeployerDeploy] deploying new SiloImplementation\n");
+
+        address siloImpl = address(new Silo(_siloFactory));
+        address shareProtectedCollateralTokenImpl = address(new ShareProtectedCollateralToken());
+        address shareDebtTokenImpl = address(new ShareDebtToken());
+
         _registerDeployment(address(siloImpl), SiloCoreContracts.SILO);
 
         _registerDeployment(
