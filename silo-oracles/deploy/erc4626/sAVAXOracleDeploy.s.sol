9b1fd6eccefb3ff6be29b475d690bb3685036508
Neo Racer <0xNeoRacer@protonmail.com>
2025-09-25 13:25:02 +0200

    deployment for new oracle

--- silo-oracles/deploy/erc4626/sAVAXOracleDeploy.s.sol
+++ silo-oracles/deploy/erc4626/sAVAXOracleDeploy.s.sol
@@ -1,15 +1,13 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.28;
 
-import {IERC4626} from "openzeppelin5/interfaces/IERC4626.sol";
 import {AddrLib} from "silo-foundry-utils/lib/AddrLib.sol";
 import {console2} from "forge-std/console2.sol";
 
 import {IERC20Metadata} from "openzeppelin5/token/ERC20/extensions/IERC20Metadata.sol";
 
+import {sAVAXOracle} from "silo-oracles/contracts/custom/sAVAX/sAVAXOracle.sol";
 import {CommonDeploy} from "../CommonDeploy.sol";
-import {SiloOraclesFactoriesContracts} from "../SiloOraclesFactoriesContracts.sol";
-import {ERC4626OracleFactory} from "silo-oracles/contracts/erc4626/ERC4626OracleFactory.sol";
 import {ISiloOracle} from "silo-core/contracts/interfaces/ISiloOracle.sol";
 import {OraclesDeployments} from "../OraclesDeployments.sol";
 
@@ -37,7 +35,9 @@ contract sAVAXOracleDeploy is CommonDeploy {
     }
 
     function _qa(ISiloOracle oracle) internal view {
-        console2.log("fetch price for: %s/%s", IERC20Metadata(sAVAXOracle(vault).S_AVAX()).symbol(), IERC20Metadata(oracle.quoteToken()).symbol());
+        address vault = address(sAVAXOracle(address(oracle)).S_AVAX());
+
+        console2.log("fetch price for: %s/%s", IERC20Metadata(vault).symbol(), IERC20Metadata(oracle.quoteToken()).symbol());
         printQuote(oracle, address(vault), uint256(10 ** IERC20Metadata(vault).decimals()));
     }
 }
