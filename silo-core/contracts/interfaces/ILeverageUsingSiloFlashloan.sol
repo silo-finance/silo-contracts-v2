4399fcf4cb06fafe5e249a583a1502036955d254
Neo Racer <0xNeoRacer@protonmail.com>
2025-10-14 09:43:23 +0200

    LeverageUsingSiloFlashloan

--- silo-core/contracts/interfaces/ILeverageUsingSiloFlashloan.sol
+++ silo-core/contracts/interfaces/ILeverageUsingSiloFlashloan.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.0;
 
+// solhint-disable ordering
+
 import {ISilo, IERC3156FlashLender} from "./ISilo.sol";
 import {IGeneralSwapModule} from "./IGeneralSwapModule.sol";
 
