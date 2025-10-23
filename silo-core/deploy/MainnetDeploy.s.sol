315bfe27f95ec667d7425b884472a764ce70aa38
Neo Racer <0xNeoRacer@protonmail.com>
2025-10-23 12:02:10 +0200

    main files deployment

--- silo-core/deploy/MainnetDeploy.s.sol
+++ silo-core/deploy/MainnetDeploy.s.sol
@@ -24,7 +24,7 @@ import {DKinkIRMFactoryDeploy} from "silo-core/deploy/DKinkIRMFactoryDeploy.s.so
 /*
     FOUNDRY_PROFILE=core \
         forge script silo-core/deploy/MainnetDeploy.s.sol \
-        --ffi --rpc-url $RPC_INJECTIVE --verify --broadcast --slow
+        --ffi --rpc-url $RPC_INJECTIVE --broadcast --slow --verify
 
     Resume verification:
     FOUNDRY_PROFILE=core \
@@ -64,7 +64,7 @@ contract MainnetDeploy is CommonDeploy {
         siloHookV1Deploy.run();
         siloHookV2Deploy.run();
         siloDeployerDeploy.run();
-        // liquidationHelperDeploy.run();
+        // liquidationHelperDeploy.run(); // TODO once we have exchange on Injective
         siloLensDeploy.run();
         towerDeploy.run();
         siloRouterV2Deploy.run();
