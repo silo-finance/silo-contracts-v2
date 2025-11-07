11dfb8199d847b06841eb952388167c831a65c08
Neo Racer <0xNeoRacer@protonmail.com>
2025-11-07 09:30:08 +0100

    1K simulation

--- silo-vaults/test/foundry/debug/VaultBorrow.sol
+++ silo-vaults/test/foundry/debug/VaultBorrow.sol
@@ -27,6 +27,10 @@ contract VaultBorrow is Test {
     ISilo internal constant SILO = ISilo(0xf0543D476e7906374863091034fe679a7bE8Ee20);
     IERC20Metadata internal SILO_ASSET;
 
+    address depositor = address(0x999);
+    ISilo collateralSilo = ISilo(0xACb7432a4BB15402CE2afe0A7C9D5b738604F6F9);
+    IERC20Metadata collateralAsset;
+
     IVaultIncentivesModule internal incentivesModule;
 
     IERC20 internal wAvax = IERC20(0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7);
@@ -37,6 +41,44 @@ contract VaultBorrow is Test {
 
         incentivesModule = VAULT.INCENTIVES_MODULE();
         SILO_ASSET = IERC20Metadata(SILO.asset());
+
+        collateralAsset = IERC20Metadata(collateralSilo.asset());
+    }
+
+    /*
+    FOUNDRY_PROFILE=vaults_tests forge test --ffi --mt test_depostor_posibilities -vvv
+    */
+    function test_depostor_posibilities() public {
+        uint256 amount = 1000e6;
+        deal(address(collateralAsset), depositor, amount);
+        assertEq(collateralAsset.balanceOf(depositor), amount, "depositor should have $1K");
+
+        vm.startPrank(depositor);
+        collateralAsset.approve(address(collateralSilo), amount);
+        collateralSilo.deposit(amount, depositor, ISilo.CollateralType.Collateral);
+        vm.stopPrank();
+
+        uint256 decimals = SILO_ASSET.decimals();
+        string memory symbol = SILO_ASSET.symbol();
+
+        uint256 maxBorrow = SILO.maxBorrow(depositor);
+        emit log_named_decimal_uint(string.concat("maxBorrow immediately after $1K (not protected) collateral, ", symbol), maxBorrow, decimals);
+
+        vm.warp(block.timestamp + 30 days);
+
+        maxBorrow = SILO.maxBorrow(depositor);
+        emit log_named_decimal_uint(string.concat("maxBorrow after 30 days, ", symbol), maxBorrow, decimals);
+
+        vm.warp(block.timestamp + 30 days);
+
+        maxBorrow = SILO.maxBorrow(depositor);
+        emit log_named_decimal_uint(string.concat("maxBorrow after 60 days, ", symbol), maxBorrow, decimals);
+
+        vm.warp(block.timestamp + 30 days);
+
+        maxBorrow = SILO.maxBorrow(depositor);
+        emit log_named_decimal_uint(string.concat("maxBorrow after 90 days, ", symbol), maxBorrow, decimals);
+
     }
 
     /*
@@ -62,7 +104,7 @@ contract VaultBorrow is Test {
 
         uint256 maxBorrow = SILO.maxBorrow(address(VAULT));
         emit log_named_decimal_uint("VAULT maxBorrow", maxBorrow, assetDecimals);
-        console2.log("SILO liquidity", SILO.getLiquidity());
+        emit log_named_decimal_uint("SILO liquidity", SILO.getLiquidity(), assetDecimals);
 
 
         BorrowFromSilo newLogic = new BorrowFromSilo();
@@ -95,6 +137,9 @@ contract VaultBorrow is Test {
 
         _borrowViaVault(14 days);
         _borrowViaVault(30 days);
+        _borrowViaVault(60 days);
+        _borrowViaVault(90 days);
+        _borrowViaVault(120 days);
 
         _qaVaultOperations();
 
@@ -148,6 +193,7 @@ contract VaultBorrow is Test {
 
         uint256 assetBalanceAfter = SILO_ASSET.balanceOf(address(VAULT));
         emit log_named_decimal_uint("[_borrowViaVault] ASSET balance after", assetBalanceAfter, assetDecimals);
+        console2.log("after ", _warp / 1 days, " days");
         emit log_named_decimal_uint(string.concat("[_borrowViaVault] ", symbol, " balance"), assetBalanceAfter - assetBalanceBefore, assetDecimals);
     }
 }
