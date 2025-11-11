11b29c03f6a137617b4dd023ceb489886847e7c1
Neo Racer <0xNeoRacer@protonmail.com>
2025-11-11 20:32:58 +0100

    qa

--- silo-core/test/foundry/liquidation/defaulting/DefaultingLiquidation.i.sol
+++ silo-core/test/foundry/liquidation/defaulting/DefaultingLiquidation.i.sol
@@ -4,21 +4,26 @@ pragma solidity ^0.8.28;
 import {Test} from "forge-std/Test.sol";
 import {console2} from "forge-std/console2.sol";
 
+import {Math} from "openzeppelin5/utils/math/Math.sol";
+
 import {ISiloConfig} from "silo-core/contracts/interfaces/ISiloConfig.sol";
 import {ISilo} from "silo-core/contracts/interfaces/ISilo.sol";
 import {ISiloOracle} from "silo-core/contracts/interfaces/ISiloOracle.sol";
 import {IShareToken} from "silo-core/contracts/interfaces/IShareToken.sol";
 import {IPartialLiquidation} from "silo-core/contracts/interfaces/IPartialLiquidation.sol";
-import {IERC20Errors} from "openzeppelin5/interfaces/draft-IERC6093.sol";
+import {IPartialLiquidationByDefaulting} from "silo-core/contracts/interfaces/IPartialLiquidationByDefaulting.sol";
 
 import {SiloLittleHelper} from "../../_common/SiloLittleHelper.sol";
 import {SiloConfigOverride, SiloFixture} from "../../_common/fixtures/SiloFixture.sol";
 import {MintableToken} from "silo-core/test/foundry/_common/MintableToken.sol";
+import {SiloConfigsNames} from "silo-core/deploy/silo/SiloDeployments.sol";
+import {SiloLensLib} from "silo-core/contracts/lib/SiloLensLib.sol";
+
+import {DummyOracle} from "silo-core/test/foundry/_common/DummyOracle.sol";
 
 
 // import {SiloLittleHelper} from "../../../_common/SiloLittleHelper.sol";
 /*
-1 wei collateral fuzzing tests: goal is to make sure defaulting never reverts if oracle price > 0
 
 when 0 collateral, defaulting should NOT revert
 
@@ -35,34 +40,36 @@ should work for both collaterals (collateral and protected) in same way
 
 
 TODO test if we revert with TooHigh error on repay because of delegate call
+
+anything todo with decimals?
 */
 
 contract DefaultingLiquidationTest is SiloLittleHelper, Test {
+    using SiloLensLib for ISilo;
+
     ISiloConfig siloConfig;
 
     address borrower = makeAddr("borrower");
+    address depositor = makeAddr("depositor");
 
-    function setUp() public virtual {
-        siloConfig = _setUpLocalFixture(SiloConfigsNames.SILO_LOCAL_NO_ORACLE_DEFAULTING);
-    }
+    DummyOracle oracle;
 
-
-    address oracle = makeAddr("Oracle");
+    IPartialLiquidationByDefaulting defaulting;
 
     function setUp() public {
-        token0 = new MintableToken(8);
-        token1 = new MintableToken(10);
+        token0 = new MintableToken(18);
+        token1 = new MintableToken(18);
+
+        oracle = new DummyOracle(1e18, address(token1)); // 1:1 price
 
         token0.setOnDemand(true);
         token1.setOnDemand(true);
 
-        vm.mockCall(oracle, abi.encodeWithSelector(ISiloOracle.quoteToken.selector), abi.encode(address(token1)));
-
         SiloConfigOverride memory overrides;
         overrides.token0 = address(token0);
         overrides.token1 = address(token1);
-        overrides.solvencyOracle0 = oracle;
-        overrides.maxLtvOracle0 = oracle;
+        overrides.solvencyOracle0 = address(oracle);
+        overrides.maxLtvOracle0 = address(oracle);
         overrides.configName = SiloConfigsNames.SILO_LOCAL_NO_ORACLE_DEFAULTING;
 
         SiloFixture siloFixture = new SiloFixture();
@@ -71,19 +78,56 @@ contract DefaultingLiquidationTest is SiloLittleHelper, Test {
         (siloConfig, silo0, silo1,,, hook) = siloFixture.deploy_local(overrides);
 
         partialLiquidation = IPartialLiquidation(hook);
+        defaulting = IPartialLiquidationByDefaulting(hook);
     }
 
     /*
     FOUNDRY_PROFILE=core_test forge test -vv --ffi --mt test_defaulting_setup
     */
     function test_defaulting_setup() public {
-        _createPosition(1, 1, false);
+        _createPosition(10, 10, false);
     }
+
+    /*
+    FOUNDRY_PROFILE=core_test forge test --ffi --mt test_defaulting_neverReverts_1weiCollateral -vv
+
+    1 wei collateral fuzzing tests: goal is to make sure defaulting never reverts if oracle price > 0
+
+    */
+    function test_defaulting_neverReverts_1weiCollateral() public {
+        oracle.setPrice(2e18);
+        _createPosition({_collateral: 0, _protected: 10, _maxOut: true});
+        oracle.setPrice(1e18);
+
+        _printLtv(borrower);
+        vm.assume(_defaultingPossible(borrower));
+
+        defaulting.liquidationCallByDefaulting(borrower);
+
+        // assertEq(silo0.getLtv(borrower), 1e18, "expected LTV for test");
+
+    }
+
+    /*
+    if _defaultingPossible() we never revert otherwise we do revert
+    */
+
+    /*
+    bad debt scenario: everybody can exit with the same loss
+    */
+
+    /*
+    if no bad debt, both liquidations are the same
+    */
+
+    /*
+    fee is corectly splitted 
+    */
 
 
     function _mockQuote(uint256 _amountIn, uint256 _price) public {
         vm.mockCall(
-            oracle, abi.encodeWithSelector(ISiloOracle.quote.selector, _amountIn, address(token0)), abi.encode(_price)
+            address(oracle), abi.encodeWithSelector(ISiloOracle.quote.selector, _amountIn, address(token0)), abi.encode(_price)
         );
     }
 
@@ -105,13 +149,31 @@ contract DefaultingLiquidationTest is SiloLittleHelper, Test {
     }
 
     function _createPosition(uint256 _collateral, uint256 _protected, bool _maxOut) internal {
-        if (_collateral != 0) _depositCollateral(_collateral, borrower, _sameAssetPosition());
-        if (_protected != 0) _depositCollateral(_protected, borrower, _sameAssetPosition(), ISilo.CollateralType.Protected);
+        bool sameAssetPosition = _sameAssetPosition();
 
-        uint256 maxBorrow = silo1.maxBorrow(borrower);
+        uint256 forBorrow = Math.max(_collateral, _protected);
+
+        if (sameAssetPosition) _deposit(forBorrow, depositor);
+        else _depositForBorrow(forBorrow, depositor);
+
+        if (_collateral != 0) _deposit(_collateral, borrower);
+        if (_protected != 0) _deposit(_protected, borrower, ISilo.CollateralType.Protected);
+
+        _printBalances(silo0, borrower);
+        _printBalances(silo1, borrower);
+
+        uint256 maxBorrow = sameAssetPosition ? silo0.maxBorrowSameAsset(borrower) : silo1.maxBorrow(borrower);
+        console2.log("maxBorrow", maxBorrow);
         assertGt(maxBorrow, 0, "maxBorrow should be > 0");
 
-        _borrow(maxBorrow, borrower);
+        if (sameAssetPosition) {
+            vm.prank(borrower);
+            silo0.borrowSameAsset(maxBorrow, borrower, borrower);
+        } else {
+            _borrow(maxBorrow, borrower);
+        }
+
+        _printLtv(borrower);
 
         if (_maxOut) {
             uint256 maxWithdraw = silo0.maxWithdraw(borrower);
@@ -119,10 +181,30 @@ contract DefaultingLiquidationTest is SiloLittleHelper, Test {
 
             maxWithdraw = silo0.maxWithdraw(borrower, ISilo.CollateralType.Protected);
             if (maxWithdraw != 0) _withdraw(maxWithdraw, borrower, ISilo.CollateralType.Protected);
+            _printLtv(borrower);
         }
     }
 
     function _sameAssetPosition() internal pure virtual returns (bool) {
         return false;
+    }
+
+    function _printBalances(ISilo _silo, address _user) internal view {
+        (address protectedShareToken, address collateralShareToken, address debtShareToken) = _silo.config().getShareTokens(address(_silo));
+        string memory siloName = string.concat("silo", address(_silo) == address(silo0) ? "0" : "1");
+
+        console2.log(siloName, "balance", IShareToken(collateralShareToken).balanceOf(_user));
+        console2.log(siloName, "protected balance", IShareToken(protectedShareToken).balanceOf(_user));
+        console2.log(siloName, "debt balance", IShareToken(debtShareToken).balanceOf(_user));
+    }
+
+    function _printLtv(address _user) internal {
+        emit log_named_decimal_uint("LTV [%]", silo0.getLtv(_user), 16);
+    }
+
+    function _defaultingPossible(address _user) internal view returns (bool) {
+        uint256 margin = defaulting.LT_MARGIN_FOR_DEFAULTING();
+        uint256 lt = silo0.config().getConfig(address(silo0)).lt;
+        return silo0.getLtv(_user) >= lt + margin;
     }
 }
