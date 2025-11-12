cd531b56fbe5625cfff3ec73c4d3e18cbc743d60
Neo Racer <0xNeoRacer@protonmail.com>
2025-11-12 18:53:18 +0100

    test_defaulting_neverReverts_fuzz

--- silo-core/test/foundry/liquidation/defaulting/DefaultingLiquidationCommon.sol
+++ silo-core/test/foundry/liquidation/defaulting/DefaultingLiquidationCommon.sol
@@ -60,7 +60,7 @@ abstract contract DefaultingLiquidationCommon is SiloLittleHelper, Test {
     address borrower = makeAddr("borrower");
     address depositor = makeAddr("depositor");
 
-    DummyOracle oracle;
+    DummyOracle oracle0;
 
     IPartialLiquidationByDefaulting defaulting;
     ISiloIncentivesController gauge;
@@ -69,7 +69,7 @@ abstract contract DefaultingLiquidationCommon is SiloLittleHelper, Test {
         token0 = new MintableToken(18);
         token1 = new MintableToken(18);
 
-        oracle = new DummyOracle(1e18, address(token1)); // 1:1 price
+        oracle0 = new DummyOracle(1e18, address(token1)); // 1:1 price
 
         token0.setOnDemand(true);
         token1.setOnDemand(true);
@@ -77,8 +77,8 @@ abstract contract DefaultingLiquidationCommon is SiloLittleHelper, Test {
         SiloConfigOverride memory overrides;
         overrides.token0 = address(token0);
         overrides.token1 = address(token1);
-        overrides.solvencyOracle0 = address(oracle);
-        overrides.maxLtvOracle0 = address(oracle);
+        overrides.solvencyOracle0 = address(oracle0);
+        overrides.maxLtvOracle0 = address(oracle0);
         overrides.configName = _useConfigName();
 
         SiloFixture siloFixture = new SiloFixture();
@@ -91,22 +91,28 @@ abstract contract DefaultingLiquidationCommon is SiloLittleHelper, Test {
     }
 
     /*
-    FOUNDRY_PROFILE=core_test forge test -vv --ffi --mt test_defaulting_setup
+    FOUNDRY_PROFILE=core_test forge test --ffi --mt test_defaulting_setup -vv
     */
     function test_defaulting_setup() public {
-        _createPosition(10, 10, false);
+        _setCollateralPrice(3e18);
+        assertTrue(_createPosition({_collateral: 0, _protected: 2, _maxOut: true}));
     }
 
     /*
-    FOUNDRY_PROFILE=core_test forge test --ffi --mt test_defaulting_neverReverts_1weiCollateral -vv
-
-    1 wei collateral fuzzing tests: goal is to make sure defaulting never reverts if oracle price > 0
-
+    FOUNDRY_PROFILE=core_test forge test --ffi --mt test_defaulting_neverReverts_fuzz -vv --mc DefaultingLiquidationTwo1Test
     */
-    function test_defaulting_neverReverts_1weiCollateral() public {
-        oracle.setPrice(2e18);
-        _createPosition({_collateral: 0, _protected: 10, _maxOut: true});
-        oracle.setPrice(1e18);
+    function test_defaulting_neverReverts_fuzz(uint32 _collateral, uint32 _protected) public {
+        _setCollateralPrice(1000e18);
+        // minimal collateral to create position is 2
+        bool success = _createPosition({_collateral: _collateral, _protected: _protected, _maxOut: true});
+        vm.assume(success);
+
+        // this will help with interest 
+        _removeLiquidity(); 
+
+        _setCollateralPrice(1e18); // drop price 1000x
+
+        vm.warp(block.timestamp + 10000 days);
 
         _printLtv(borrower);
         vm.assume(_defaultingPossible(borrower));
@@ -117,7 +123,7 @@ abstract contract DefaultingLiquidationCommon is SiloLittleHelper, Test {
 
         _printLtv(borrower);
 
-        // assertEq(silo0.getLtv(borrower), 1e18, "expected LTV for test");
+        assertEq(silo0.getLtv(borrower), 0, "position should be removed");
     }
 
     /*
@@ -142,33 +148,45 @@ abstract contract DefaultingLiquidationCommon is SiloLittleHelper, Test {
 
     function _mockQuote(uint256 _amountIn, uint256 _price) public {
         vm.mockCall(
-            address(oracle),
+            address(oracle0),
             abi.encodeWithSelector(ISiloOracle.quote.selector, _amountIn, address(token0)),
             abi.encode(_price)
         );
     }
 
-    function _depositAndBurn(uint256 _amount, uint256 _burn, ISilo.CollateralType _collateralType) public {
-        if (_amount == 0) return;
+    // function _depositAndBurn(uint256 _amount, uint256 _burn, ISilo.CollateralType _collateralType) public {
+    //     if (_amount == 0) return;
 
-        uint256 shares = _deposit(_amount, address(this), _collateralType);
-        vm.assume(shares >= _burn);
+    //     uint256 shares = _deposit(_amount, address(this), _collateralType);
+    //     vm.assume(shares >= _burn);
 
-        if (_burn != 0) {
-            (address protectedShareToken, address collateralShareToken,) =
-                silo0.config().getShareTokens(address(silo0));
-            address token =
-                _collateralType == ISilo.CollateralType.Protected ? protectedShareToken : collateralShareToken;
+    //     if (_burn != 0) {
+    //         (address protectedShareToken, address collateralShareToken,) =
+    //             silo0.config().getShareTokens(address(silo0));
+    //         address token =
+    //             _collateralType == ISilo.CollateralType.Protected ? protectedShareToken : collateralShareToken;
 
-            vm.prank(address(silo0));
-            IShareToken(token).burn(address(this), address(this), _burn);
-        }
+    //         vm.prank(address(silo0));
+    //         IShareToken(token).burn(address(this), address(this), _burn);
+    //     }
+    // }
+
+    function _removeLiquidity() internal {
+        vm.startPrank(depositor);
+        uint256 amount = silo0.maxWithdraw(depositor);
+        if (amount != 0) silo0.withdraw(amount, depositor, depositor);
+
+        amount = silo1.maxWithdraw(depositor);
+        if (amount != 0) silo1.withdraw(amount, depositor, depositor);
+        vm.stopPrank();
     }
 
-    function _createPosition(uint256 _collateral, uint256 _protected, bool _maxOut) internal {
+    function _createPosition(uint256 _collateral, uint256 _protected, bool _maxOut) internal returns (bool success){
         (ISilo collateralSilo, ISilo debtSilo) = _getSilos();
 
         uint256 forBorrow = Math.max(_collateral, _protected);
+        if (forBorrow == 0) return false;
+        
         vm.prank(depositor);
         debtSilo.deposit(forBorrow, depositor);
 
@@ -182,18 +200,25 @@ abstract contract DefaultingLiquidationCommon is SiloLittleHelper, Test {
 
         uint256 maxBorrow = _maxBorrow(borrower);
         console2.log("maxBorrow", maxBorrow);
-        assertGt(maxBorrow, 0, "maxBorrow should be > 0");
+        console2.log("liquidity0", silo0.getLiquidity());
+        console2.log("liquidity1", silo1.getLiquidity());
+        success = maxBorrow > 0;
+
+        if (!success) return false;
 
         _executeBorrow(borrower, maxBorrow);
 
         _printLtv(borrower);
 
         if (_maxOut) {
+            vm.startPrank(borrower);
             uint256 maxWithdraw = collateralSilo.maxWithdraw(borrower);
             if (maxWithdraw != 0) collateralSilo.withdraw(maxWithdraw, borrower, borrower);
 
             maxWithdraw = collateralSilo.maxWithdraw(borrower, ISilo.CollateralType.Protected);
             if (maxWithdraw != 0) collateralSilo.withdraw(maxWithdraw, borrower, borrower, ISilo.CollateralType.Protected);
+            vm.stopPrank();
+
             _printLtv(borrower);
         }
     }
@@ -212,21 +237,42 @@ abstract contract DefaultingLiquidationCommon is SiloLittleHelper, Test {
         emit log_named_decimal_uint("LTV [%]", silo0.getLtv(_user), 16);
     }
 
-    function _defaultingPossible(address _user) internal view returns (bool) {
+    function _defaultingPossible(address _user) internal returns (bool possible) {
         uint256 margin = defaulting.LT_MARGIN_FOR_DEFAULTING();
-        uint256 lt = silo0.config().getConfig(address(silo0)).lt;
+        (ISilo collateralSilo,) = _getSilos();
+        uint256 lt = collateralSilo.config().getConfig(address(collateralSilo)).lt;
+        uint256 ltv = collateralSilo.getLtv(_user);
 
-        return silo0.getLtv(_user) >= lt + margin;
+        possible = ltv >= lt + margin;
+        emit log_named_decimal_uint("    lt", lt, 16);
+        emit log_named_decimal_uint("margin", margin, 16);
+        emit log_named_decimal_uint("   ltv", ltv, 16);
+        console2.log("defaulting possible: ", possible ? "yes" : "no");
     }
 
     function _createIncentiveController() internal {
         // TODO test if revert for silo0
-        gauge = new SiloIncentivesController(address(this), address(partialLiquidation), address(silo0));
+        (ISilo collateralSilo,) = _getSilos();
+        gauge = new SiloIncentivesController(address(this), address(partialLiquidation), address(collateralSilo));
 
         address owner = Ownable(address(defaulting)).owner();
         vm.prank(owner);
-        IGaugeHookReceiver(address(defaulting)).setGauge(gauge, IShareToken(address(silo0)));
+        IGaugeHookReceiver(address(defaulting)).setGauge(gauge, IShareToken(address(collateralSilo)));
         console2.log("gauge configured");
+    }
+
+    /// @param _price 1e18 will make collateral:debt 1:1, 2e18 will make collateral to be 2x more valuable than debt
+    function _setCollateralPrice(uint256 _price) internal {
+        (ISilo collateralSilo,) = _getSilos();
+        if (address(collateralSilo) == address(silo0)) oracle0.setPrice(_price);
+        else oracle0.setPrice(1e36 / _price); 
+
+        emit log_named_decimal_uint("value of token 0", oracle0.quote(10 ** token0.decimals(), address(token0)), token1.decimals());
+    }
+
+    function _siloLp() internal view returns (string memory lp) {
+        (ISilo collateralSilo,) = _getSilos();
+        lp = address(collateralSilo) == address(silo0) ? "0" : "1";
     }
 
     // CONFIGURATION
