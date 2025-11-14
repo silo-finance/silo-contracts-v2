4b67fab1913c416f26336322bc63895b2063c7aa
Neo Racer <0xNeoRacer@protonmail.com>
2025-11-14 12:44:17 +0100

    import

--- silo-core/contracts/interfaces/IPartialLiquidationByDefaulting.sol
+++ silo-core/contracts/interfaces/IPartialLiquidationByDefaulting.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0;
 
+import {ISilo} from "./ISilo.sol";
+
 interface IPartialLiquidationByDefaulting {
     struct CallParams {
         uint256 collateralSharesTotal;
