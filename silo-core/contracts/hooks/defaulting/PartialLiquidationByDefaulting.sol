4329311a7c9c35100ea90146964907806f9ce452
Neo Racer <0xNeoRacer@protonmail.com>
2025-11-17 11:36:18 +0100

    remove unused variable

--- silo-core/contracts/hooks/defaulting/PartialLiquidationByDefaulting.sol
+++ silo-core/contracts/hooks/defaulting/PartialLiquidationByDefaulting.sol
@@ -114,7 +114,6 @@ abstract contract PartialLiquidationByDefaulting is IPartialLiquidationByDefault
         _liquidateByDistributingCollateral({
             _borrower: _borrower,
             _collateralSilo: collateralConfig.silo,
-            _debtSilo: debtConfig.silo,
             _shareToken: collateralConfig.collateralShareToken,
             _withdrawSharesForLenders: params.collateralSharesForLenders,
             _withdrawSharesForKeeper: params.collateralSharesForKeeper
@@ -123,7 +122,6 @@ abstract contract PartialLiquidationByDefaulting is IPartialLiquidationByDefault
         _liquidateByDistributingCollateral({
             _borrower: _borrower,
             _collateralSilo: collateralConfig.silo,
-            _debtSilo: debtConfig.silo,
             _shareToken: collateralConfig.protectedShareToken,
             _withdrawSharesForLenders: params.protectedSharesForLenders,
             _withdrawSharesForKeeper: params.protectedSharesForKeeper
@@ -250,7 +248,6 @@ abstract contract PartialLiquidationByDefaulting is IPartialLiquidationByDefault
     function _liquidateByDistributingCollateral(
         address _borrower,
         address _collateralSilo,
-        address _debtSilo,
         address _shareToken,
         uint256 _withdrawSharesForLenders,
         uint256 _withdrawSharesForKeeper
