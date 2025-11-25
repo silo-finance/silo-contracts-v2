4dec70b67477b5f6c949a302afeac552d0c0ce27
Neo Racer <0xNeoRacer@protonmail.com>
2025-11-25 23:55:23 +0100

    Revert "echidna"
    This reverts commit 10736996e9171a5ad044872a1e2b60f402ce99d6.

---
+++ silo-core/test/echidna-leverage/CryticToFoundryLeverage.t.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+// Libraries
+import {Actor} from "silo-core/test/invariants/utils/Actor.sol";
+
+// Contracts
+import {SetupLeverage} from "./SetupLeverage.t.sol";
+import {InvariantsLeverage} from "./InvariantsLeverage.t.sol";
+
+// solhint-disable function-max-lines, func-name-mixedcase
+
+/*
+ * Test suite that converts from  "fuzz tests" to foundry "unit tests"
+ * The objective is to go from random values to hardcoded values that can be analyzed more easily
+ */
+contract CryticToFoundryLeverage is InvariantsLeverage, SetupLeverage {
+    uint256 public constant DEFAULT_TIMESTAMP = 337812;
+
+    CryticToFoundryLeverage public LeverageTester = this;
+
+    function setUp() public {
+        // Deploy protocol contracts
+        _setUp();
+
+        // Deploy actors
+        _setUpActors();
+
+        // Initialize handler contracts
+        _setUpHandlers();
+
+        vm.warp(DEFAULT_TIMESTAMP);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    //                                 FAILING INVARIANTS REPLAY                                 //
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    //                              FAILING POSTCONDITIONS REPLAY                                //
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+
+    /*
+    FOUNDRY_PROFILE=echidna_leverage forge test -vv --ffi --mt test_EchidnaLeverage_leverage
+    */
+    function test_EchidnaLeverage_leverage() public {
+        LeverageTester.deposit(1, 0, 0, 0);
+        LeverageTester.openLeveragePosition(4506857007, 0, RandomGenerator(1, 0, 0));
+    }
+
+    /*
+    FOUNDRY_PROFILE=echidna_leverage forge test -vv --ffi --mt test_EchidnaLeverage_onFlashLoan_0
+    */
+    function test_EchidnaLeverage_onFlashLoan_0() public {
+        LeverageTester.onFlashLoan(
+            address(0x0),
+            144878998102916798939665310881083899372024861808743479,
+            1068209701505743703662069164166715788602248289963999918073026641719,
+            "",
+            RandomGenerator(0, 0, 0)
+        );
+    }
+}
---
+++ silo-core/test/echidna-leverage/utils/mocks/TestWETH.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+import {TestERC20} from "./TestERC20.sol";
+
+contract TestWETH is TestERC20 {
+    constructor(string memory name, string memory symbol, uint8 decimals) TestERC20(name, symbol, decimals) {}
+
+    function deposit() external payable {
+        _mint(msg.sender, msg.value);
+    }
+
+    function burn(address _from, uint256 _amount) external virtual {
+        _burn(_from, _amount);
+    }
+
+    function withdraw(uint256 _amount) external virtual {
+        _burn(msg.sender, _amount);
+        payable(msg.sender).transfer(_amount);
+    }
+}
