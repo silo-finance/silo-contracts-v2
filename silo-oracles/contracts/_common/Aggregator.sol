75d10c3cf64dc8e17328a16058917a1d531c74ca
Neo Racer <0xNeoRacer@protonmail.com>
2026-01-27 12:26:57 +0100

    common aggregator interface

---
+++ silo-oracles/contracts/_common/Aggregator.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity 0.8.28;
+
+import {AggregatorV3Interface} from "chainlink/v0.8/interfaces/AggregatorV3Interface.sol";
+
+abstract contract Aggregator is AggregatorV3Interface {
+    function latestRoundData()
+        external
+        view
+        virtual
+        returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)
+    {
+        return (0, 0, 0, 0, 0);
+    }
+
+    /// @notice all Silo oracles should return price in 18 decimals
+    function decimals() external view virtual returns (uint8) {
+        return 18;
+    }
+
+    function description() external view virtual returns (string memory) {
+        return "Silo Oracle";
+    }
+
+    function version() external view returns (uint256) {
+        return 1;
+    }
+
+    /// @notice not supported
+    function getRoundData(uint80 _roundId)
+        external
+        view
+        returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)
+    {
+        return (0, 0, 0, 0, 0);
+    }
+
+    /// @notice this function follows the Chainlink V3 interface but only answer is used, all other return values are zero
+    function latestRoundData()
+        external
+        view
+        virtual
+        returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)
+    {
+        (address token, uint256 decimals) = baseToken();
+        answer = quote(10 ** decimals, token);
+
+        startedAt = block.timestamp;
+        updatedAt = block.timestamp;
+    }
+
+    function quote(uint256 _baseAmount, address _baseToken) public view virtual returns (uint256 quoteAmount);
+    
+    function baseToken() public view virtual returns (address token, uint256 decimals);
+}
