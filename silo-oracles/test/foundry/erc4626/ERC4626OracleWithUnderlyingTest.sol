ce9cb5db78741261b60a973e764bb8a0a4bf86f5
Neo Racer <0xNeoRacer@protonmail.com>
2026-02-09 15:01:38 +0100

    rpc

--- silo-oracles/test/foundry/erc4626/ERC4626OracleWithUnderlyingTest.sol
+++ silo-oracles/test/foundry/erc4626/ERC4626OracleWithUnderlyingTest.sol
@@ -4,6 +4,9 @@ pragma solidity ^0.8.28;
 import {Test} from "forge-std/Test.sol";
 import {AddrLib} from "silo-foundry-utils/lib/AddrLib.sol";
 
+import {ERC4626Mock} from "openzeppelin5/mocks/token/ERC4626Mock.sol";
+import {ERC20Mock} from "openzeppelin5/mocks/token/ERC20Mock.sol";
+
 import {ERC4626OracleWithUnderlyingFactoryDeploy} from
     "../../../deploy/erc4626/ERC4626OracleWithUnderlyingFactoryDeploy.s.sol";
 import {ERC4626OracleWithUnderlyingDeploy} from "../../../deploy/erc4626/ERC4626OracleWithUnderlyingDeploy.s.sol";
@@ -12,6 +15,7 @@ import {ERC4626OracleWithUnderlyingFactory} from
 import {ERC4626OracleWithUnderlying} from "silo-oracles/contracts/erc4626/ERC4626OracleWithUnderlying.sol";
 import {SiloOraclesFactoriesContracts} from "silo-oracles/deploy/SiloOraclesFactoriesContracts.sol";
 import {IERC4626OracleWithUnderlying} from "silo-oracles/contracts/interfaces/IERC4626OracleWithUnderlying.sol";
+import {ISiloOracle} from "silo-core/contracts/interfaces/ISiloOracle.sol";
 
 /*
     FOUNDRY_PROFILE=oracles forge test --mc ERC4626OracleWithUnderlyingTest --ffi -vv
@@ -19,9 +23,12 @@ import {IERC4626OracleWithUnderlying} from "silo-oracles/contracts/interfaces/IE
 contract ERC4626OracleWithUnderlyingTest is Test {
     ERC4626OracleWithUnderlying oracle;
     address wstUSR;
+    address chainlink;
+    address underlying;
 
     function setUp() public {
-        vm.createSelectFork(vm.envString("RPC_MAINNET"), 22724590); // forking Jun 17 2025
+        underlying = address(new ERC20Mock());
+        wstUSR = address(new ERC4626Mock(underlying));
 
         AddrLib.init();
 
@@ -33,11 +40,21 @@ contract ERC4626OracleWithUnderlyingTest is Test {
         AddrLib.setAddress(SiloOraclesFactoriesContracts.ERC4626_ORACLE_UNDERLYING_FACTORY, address(factory));
 
         ERC4626OracleWithUnderlyingDeploy deployer = new ERC4626OracleWithUnderlyingDeploy();
+        chainlink = makeAddr("CHAINLINK_USR_USD");
         deployer.setUseConfig("wstUSR", "CHAINLINK_USR_USD");
 
-        oracle = deployer.run();
+        AddrLib.setAddress("wstUSR", wstUSR);
+        AddrLib.setAddress("CHAINLINK_USR_USD", chainlink);
 
-        wstUSR = AddrLib.getAddress("wstUSR");
+        vm.mockCall(chainlink, abi.encodeWithSelector(ISiloOracle.quoteToken.selector), abi.encode(underlying));
+
+        vm.mockCall(
+            chainlink,
+            abi.encodeWithSelector(ISiloOracle.quote.selector, 1e18, address(underlying)),
+            abi.encode(1.088293604071978577e18)
+        );
+
+        oracle = deployer.run();
     }
 
     /*
@@ -79,6 +96,12 @@ contract ERC4626OracleWithUnderlyingTest is Test {
     FOUNDRY_PROFILE=oracles forge test --mt test_wrappedVault_ZeroQuote --ffi -vv
      */
     function test_wrappedVault_ZeroQuote() public {
+        vm.mockCallRevert(
+            chainlink,
+            abi.encodeWithSelector(ISiloOracle.quote.selector, 0, address(underlying)),
+            abi.encodeWithSelector(IERC4626OracleWithUnderlying.ZeroQuote.selector)
+        );
+
         vm.expectRevert(IERC4626OracleWithUnderlying.ZeroQuote.selector);
         oracle.quote(0, wstUSR);
     }
